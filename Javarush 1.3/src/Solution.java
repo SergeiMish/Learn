5404d2a3b1a3ab31a9107ba8d0cfd67ad7f1b473
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-05 11:19:06 +0500

    Javarush 1.3

---
+++ Javarush 1.3/src/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Аварийное питание включено");
+        System.out.println("Ручное управление включено");
+
+    }
+}
