21488076cc9530d17a0dc2dd43d29239e4c639e4
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-05 12:05:35 +0500

    Javarush 1.3 2

---
+++ Javarush 1.3 2/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        String name = "Mary";
+        int age = 34;
+        String city = "Ufa";
+
+
+        //напишите тут ваш код
+    }
+}
