b1042f6a7a151da71d378241fcc3555311f67c76
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-07 22:39:59 +0500

    Task 3

---
+++ Task 3/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public class Vector {
+        // Три приватных поля
+        private double x;
+        private double y;
+        private double z;
+
+        // С тремя параметрами
+        public Vector(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // Длина вектора. Корень из суммы квадратов
+        public double length() {
+            return Math.sqrt(x * x + y * y + z * z);
+        }
+
+        // метод, вычисляющий скалярное произведение
+        public double scalarProduct(Vector vector) {
+            return x * vector.x + y * vector.y + z * vector.z;
+        }
+    }
+}
