bcb70dba414516e3503cde3e5ba314a709233841
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-08 17:39:42 +0500

    task 7

---
+++ task 7/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static int bruteForce(double[] array, double key) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key)
+                return i;
+        }
+        return -1;
+    }
+}
