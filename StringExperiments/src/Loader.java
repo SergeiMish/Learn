b769e4be4927ccd2f3d3e9854f4a27ef57c32ee6
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-11 15:16:02 +0500

    task 13

--- StringExperiments/src/Loader.java
+++ StringExperiments/src/Loader.java
@@ -2,21 +2,10 @@
 public class Loader {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String str = text.trim();
-        String digits = "";
-        int sum = 0;
-        str += " ";
-        for (int i = 0; i < str.length(); i++) {
-            char chrs = str.charAt(i);
-            if (Character.isDigit(chrs))
-                digits += chrs;
-            else {
-                if (!digits.equals(""))
-                    sum += Integer.parseInt(digits);
-                digits = "";
-            }
-
+        String[] strings = text.replaceAll("\\D+", " ").trim().split("\\s+");
+        int result = 0;
+        for (String str : strings) {
+            result += Integer.parseInt(str);
         }
-        System.out.println(sum);
-    }
-}
+        System.out.println(result);
+    }}
