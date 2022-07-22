e573fcbebb2faee316556e6291ce0ed7297019e4
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-22 15:45:51 +0500

    task 31

---
+++ task 31/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0; i<=25; i++){
+        if (i%3==0) {continue;}
+        System.out.println(i);
+        }
+
+    }
+}
