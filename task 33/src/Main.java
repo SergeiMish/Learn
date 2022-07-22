5186c69786484ff99995448e98671eedad00a27f
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-22 18:18:24 +0500

    task 33

---
+++ task 33/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 1;
+        int a = 1;
+        while (n != 11) {
+            while (a != 11) {
+                System.out.print(n * a + " ");
+                a++;
+            }
+            System.out.println();
+            n++;
+            a = 1;
+        }
+    }
+}
