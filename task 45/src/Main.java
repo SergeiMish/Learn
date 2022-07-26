71f05a954fedf98bdfb6479ebc12292277b4c717
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2022-07-26 15:56:54 +0500

    task 45

---
+++ task 45/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder cool = new StringBuilder("Кто то");
+        cool.append(" там").append(" шалит");
+        System.out.println(cool.toString());
+
+    }
+}
