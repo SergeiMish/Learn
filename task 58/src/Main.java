0f178f8e6891d247d15b4cabf876681a8203cc90
SergeiMish <mishalevskii@yandex.ru>
2022-07-30 17:00:48 +0500

    task 58/2

--- task 58/src/Main.java
+++ task 58/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите число");
         Scanner scanner = new Scanner(System.in);
-        number = scanner.nextInt();
+        int number = scanner.nextInt();
+        System.out.println(number + " + " + number*11 + " + " + number*111);
 
     }
 }
