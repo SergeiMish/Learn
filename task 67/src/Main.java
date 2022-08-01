aacb4c24422cdd9ab2aeddd4ddf7b9c0c93a740c
SergeiMish <mishalevskii@yandex.ru>
2022-08-01 23:18:21 +0500

    task 67.2

--- task 67/src/Main.java
+++ task 67/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Введите значение: ");
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
-        for(char c : number.toCharArray())
-            if(Character.isDigit(c)) System.out.println("Целочисленное значение: " +c);
+        int result = Integer.parseInt(number);
+                System.out.println("Целочисленное значение: " + result);
     }
 }
