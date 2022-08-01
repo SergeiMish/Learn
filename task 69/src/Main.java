8bdb534efa6386d6e3a5b5e97d4e86f5fd4e3b86
SergeiMish <mishalevskii@yandex.ru>
2022-08-02 00:04:33 +0500

    task 69

---
+++ task 69/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите первое число: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число: ");
+        int b = scanner.nextInt();
+        System.out.println("Введите третье число: ");
+        int c = scanner.nextInt();
+        System.out.println("Значение " +numbers(a, b, c));
+    }
+    public static boolean numbers(int ax, int bx, int cx){
+        return ((ax + bx) == cx && bx>ax && cx>bx);
+
+    }
+}
