efbbace99146f617e376c1cd04bd841d81830180
SergeiMish <mishalevskii@yandex.ru>
2022-08-01 22:33:13 +0500

    task 64

---
+++ task 64/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        if ((x % 2) == 0){
+        System.out.println("1");}
+        else{
+            if ((x % 1) == 0);
+            System.out.println("0");
+        }
+
+    }
+}
