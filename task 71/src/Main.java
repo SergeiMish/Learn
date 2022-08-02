f3516135f1f2fe06ea83ae76f11bec57c6baf865
SergeiMish <mishalevskii@yandex.ru>
2022-08-02 18:45:34 +0500

    task 71.2

--- task 71/src/Main.java
+++ task 71/src/Main.java
@@ -7,7 +7,6 @@ public class Main {
         int number = scanner.nextInt();
         for (int i=1; i<10; i++){
             if (number>0){
-                int x = number / i;
                 System.out.print(i);
             }
         }
