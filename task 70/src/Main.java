afee09b3af121157432434e77feacf0288a08d75
SergeiMish <mishalevskii@yandex.ru>
2022-08-02 15:40:02 +0500

    task 70.2

--- task 70/src/Main.java
+++ task 70/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         System.out.println("Введите секунды");
         Scanner scanner = new Scanner(System.in);
         int sec = scanner.nextInt();
-        
+        int hour = sec/3600;
+        int mm = ((sec - (hour*3600)) /60);
+        int ss = (((sec - (hour*3600)) - mm) / 60);
+        System.out.println(hour + " " + mm + " " + ss);
     }
 }
