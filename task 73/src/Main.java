8ce0be6b753dac7ca86d94b7ef491f7d0e507f7f
SergeiMish <mishalevskii@yandex.ru>
2022-08-03 14:27:26 +0500

    task 73 Дженерики (параметризация)

---
+++ task 73/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List animals = new ArrayList();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Frog");
+        String animal = (String) animals.get(2);
+        System.out.println(animal);
+        List<String> animals2 = new ArrayList<>();
+        animals2.add("Морж");
+
+    }
+}
