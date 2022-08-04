07f0a54292bd6a5083ada0c1f63f7ead40200127
SergeiMish <mishalevskii@yandex.ru>
2022-08-04 13:43:40 +0500

    task 76.1 Считывание с файла.

---
+++ task 76/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = separator + "C:" + separator + "Users" + separator + "HP M"+ separator + "Desktop"+ separator +"Текстовый документ.txt";
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
+//"C:\Users\HP M\Desktop\Текстовый документ.txt"
