77abfd901ad6ca7bc57d491ff85c03fc250ffbb1
SergeiMish <mishalevskii@yandex.ru>
2022-08-06 19:24:58 +0500

    task 82.2

--- task 82/src/Main.java
+++ task 82/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String words = scanner.nextLine();
+
         System.out.println(words.toUpperCase(Locale.ROOT));
     }
 }
