cd499d6b32e6f6836913152b5c35a3a23f3eaa63
SergeiMish <mishalevskii@yandex.ru>
2022-08-07 21:34:47 +0500

    task 86.2

--- task 86/src/Main.java
+++ task 86/src/Main.java
@@ -10,5 +10,22 @@ public class Main {
         binary1 = in.nextLong();
         System.out.print("Input second binary number: ");
         binary2 = in.nextLong();
-
+        while (binary1 != 0 || binary2 != 0)
+        {
+            sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
+            remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
+            binary1 = binary1 / 10;
+            binary2 = binary2 / 10;
+        }
+        if (remainder != 0) {
+            sum[i++] = remainder;
+        }
+        --i;
+        System.out.print("Sum of two binary numbers: ");
+        while (i >= 0) {
+            System.out.print(sum[i--]);
+        }
+        System.out.print("\n");
+    }
+}
 
