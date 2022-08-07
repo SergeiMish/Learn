b3dd45b243113a004faf209dc5855b8c86e79e9e
SergeiMish <mishalevskii@yandex.ru>
2022-08-07 21:34:31 +0500

    task 86.1

---
+++ task 86/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        long binary1, binary2;
+        int i = 0, remainder = 0;
+        int[] sum = new int[20];
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first binary number: ");
+        binary1 = in.nextLong();
+        System.out.print("Input second binary number: ");
+        binary2 = in.nextLong();
+
+
