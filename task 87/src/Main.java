944642acc2a5638c85f301804e74ad3f3dc682ff
SergeiMish <mishalevskii@yandex.ru>
2022-08-07 21:37:06 +0500

    task 87.2

--- task 87/src/Main.java
+++ task 87/src/Main.java
@@ -26,7 +26,28 @@ public class Main {
         }
         System.out.print("Product of two binary numbers: " + multiply+"\n");
     }
-
+    static int binaryproduct(int binary1, int binary2)
+    {
+        int i = 0, remainder = 0;
+        int[] sum = new int[20];
+        int binary_prod_result = 0;
+        while (binary1 != 0 || binary2 != 0)
+        {
+            sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+            remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
+            binary1 = binary1 / 10;
+            binary2 = binary2 / 10;
+        }
+        if (remainder != 0)
+        {
+            sum[i++] = remainder;
+        }
+        --i;
+        while (i >= 0)
+        {
+            binary_prod_result = binary_prod_result * 10 + sum[i--];
+        }
+        return binary_prod_result;
     }
     }
 
