69781f735dd9a94cf60701e8da93a47e74497a76
SergeiMish <mishalevskii@yandex.ru>
2022-08-08 16:55:18 +0500

    task 92.1 Сериализация

---
+++ task 92/src/ReadObjects.java
@@ -0,0 +1,5 @@
+public class ReadObjects {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ task 92/src/WriteObjects.java
@@ -0,0 +1,8 @@
+public class WriteObjects {
+    public static void main(String[] args) {
+        Person person1 = new Person(5,"Том");
+        Person person2 = new Person(8,"Стивен");
+
+
+    }
+}
