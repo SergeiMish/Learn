33e95a6d1d964ca31adb74ee3091db2a72311a93
SergeiMish <mishalevskii@yandex.ru>
2022-08-11 16:02:51 +0500

    task 98.2 Enum

--- task 98/src/Animal.java
+++ task 98/src/Animal.java
@@ -1,7 +1,11 @@
 public enum Animal {
-    DOG, CAT, MOUSE;
+    DOG("Собака"), CAT("Кошка"), MOUSE("Мышь");
     private String traslation;
     Animal(String traslation){
         this.traslation = traslation;
+    }
+
+    public String getTraslation() {
+        return traslation;
     }
 }
--- task 98/src/Main.java
+++ task 98/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Animal animal = Animal.MOUSE;
+        System.out.println(animal.getTraslation());
+        Animal animal2 = Animal.valueOf("MOUSE");
+        System.out.println(animal2.getTraslation());
 
     }
 }
