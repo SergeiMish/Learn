a8350e8b2501311b67a061f1b6c6780dccc5e1bf
SergeiMish <mishalevskii@yandex.ru>
2022-08-11 16:33:13 +0500

    task 99.2 Рекурсия

--- task 99/src/Test.java
+++ task 99/src/Test.java
@@ -1,10 +1,17 @@
 public class Test {
     public static void main(String[] args) {
-        counter(5);
+        System.out.println(fac(6));
     }
-    public static void counter(int n){
-        if (n == 0) return;
-        System.out.println(n);
-        counter(n-1);
+    public static int fac(int n){
+        if (n == 1)
+            return 1;
+        return n * fac(n-1);
     }
+//        counter(5);
+//    }
+//    public static void counter(int n){
+//        if (n == 0) return;
+//        System.out.println(n);
+//        counter(n-1);
+//    }
 }
