a1cb408300084033243057728c8afad175d2195b
SergeiMish <mishalevskii@yandex.ru>
2022-08-11 12:29:41 +0500

    task 97.1 Сериализация 6

--- task 97/src/Person.java
+++ task 97/src/Person.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
     private int id;
-    private String name;
+    private transient String name;
 
     public Person(int id, String name){
         this.id = id;
