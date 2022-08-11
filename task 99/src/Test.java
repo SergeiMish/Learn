54df527d8b059c2132c4564496a4453700b5825a
SergeiMish <mishalevskii@yandex.ru>
2022-08-11 16:11:12 +0500

    task 99 Рекурсия

---
+++ task 99/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        someMethod();
+    }
+    public static void someMethod(){
+        System.out.println("Hello World");
+        someMethod();
+    }
+}
