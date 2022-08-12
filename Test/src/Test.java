20dcc30fe740da019f5b0a643003e426443f9a28
SergeiMish <mishalevskii@yandex.ru>
2022-08-12 13:13:26 +0500

    task 100.4 Динамический массив

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,22 +1,8 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>(100);
 
-        for (int i = 0; i < 101; i++){
-            arrayList.add(i);
-
-        }
-        System.out.println(arrayList);
-        System.out.println(arrayList.get(10));
-        System.out.println(arrayList.get(100));
-        for (int i = 0; i < arrayList.size(); i++){
-            System.out.println(arrayList.get(i));
-        }
-        for (Integer x: arrayList)
-            System.out.println(x);
-        arrayList.remove(9);
     }
 }
