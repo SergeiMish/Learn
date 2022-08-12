895d9b4fc1a05229a12dde76128e61c0602bae2a
SergeiMish <mishalevskii@yandex.ru>
2022-08-12 16:51:53 +0500

    task 101.2 Многопоточность

--- task 101/src/Test.java
+++ task 101/src/Test.java
@@ -2,13 +2,14 @@ public class Test {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         myThread.start();
-        System.out.println("Я в первом потоке");
+        MyThread myThread2 = new MyThread();
+        myThread2.start();
 
     }
 }
 class MyThread extends Thread{
     public void run(){
-        for (int i = 0; i < 100; i++)
-            System.out.println("Я в потоке");
+        for (int i = 0; i < 1000; i++)
+            System.out.println("Я в потоке " + i);
     }
 }
