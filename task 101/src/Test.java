85027383a75124a4943464b02187fbac70c5e846
SergeiMish <mishalevskii@yandex.ru>
2022-08-12 17:33:57 +0500

    task 101.6 Многопоточность

--- task 101/src/Test.java
+++ task 101/src/Test.java
@@ -4,8 +4,10 @@ public class Test {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         myThread.start();
+
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
+
         myThread.shutDown();
     }
     }
@@ -15,10 +17,9 @@ public class Test {
         while (running){
         System.out.println("Hello");
         try {
-            MyThread.sleep(1000);
+            MyThread.sleep(100);
         } catch (InterruptedException e) {
             System.out.println("Поток прерван!");
-            ;
         }
         }
     }
