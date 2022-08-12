abca480274577f893b61bf617a6c5d39475a0eeb
SergeiMish <mishalevskii@yandex.ru>
2022-08-12 16:57:57 +0500

    task 101.3 Многопоточность

--- task 101/src/Test.java
+++ task 101/src/Test.java
@@ -1,15 +1,17 @@
 public class Test {
-    public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        myThread.start();
-        MyThread myThread2 = new MyThread();
-        myThread2.start();
-
+    public static void main(String[] args) throws InterruptedException {
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//        MyThread myThread2 = new MyThread();
+//        myThread2.start();
+        System.out.println("Я сплю");
+        Thread.sleep(3000);
+        System.out.println("Я проснулся");
     }
 }
-class MyThread extends Thread{
-    public void run(){
-        for (int i = 0; i < 1000; i++)
-            System.out.println("Я в потоке " + i);
-    }
-}
+//class MyThread extends Thread{
+//    public void run(){
+//        for (int i = 0; i < 1000; i++)
+//            System.out.println("Я в потоке " + i);
+//    }
+//}
