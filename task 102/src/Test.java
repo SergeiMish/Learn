148d503aaa4de8a5d8e6f6018ffbfef6de8619cb
SergeiMish <mishalevskii@yandex.ru>
2022-08-13 15:05:21 +0500

    task 102.3

--- task 102/src/Test.java
+++ task 102/src/Test.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        int[] num_array1 = {50, -20, 0, 30, 40, 60, 12};
-        int[] num_array2 = {45, 20, 10, 20, 30, 50, 11};
-
-        System.out.println(num_array1[0] == num_array2[0] || num_array1[num_array1.length-1] == num_array2[num_array2.length-1]);
+        {
+            int[] array1 = {50, -20, 0};
+            int[] array2 = {5, -50, 10};
+            System.out.println("Array1: " + Arrays.toString(array1));
+            System.out.println("Array2: " + Arrays.toString(array2));
+            int[] array_new = {array1[0], array2[2]};
+            System.out.println("New Array: " + Arrays.toString(array_new));
+        }
     }
-    }
+}
