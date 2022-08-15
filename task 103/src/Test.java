c24178a73f62980910095b9ab494cb5373988ec6
SergeiMish <mishalevskii@yandex.ru>
2022-08-15 18:02:43 +0500

    task 103

--- task 103/src/Test.java
+++ task 103/src/Test.java
@@ -4,9 +4,9 @@ public class Test {
         public static void main(String[] args)
         {
             int[] array_nums = {20, 30, 40};
-            System.out.println("Original Array: "+ Arrays.toString(array_nums));
+            System.out.println("Оригинальный массив: "+ Arrays.toString(array_nums));
             int[] new_array_nums = {array_nums[1], array_nums[2], array_nums[0]};
-            System.out.println("Rotated Array: "+Arrays.toString(new_array_nums));
+            System.out.println("Перевернутый массив: "+Arrays.toString(new_array_nums));
         }
     }
 
