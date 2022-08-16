ba4a689cbb0521782c84da1fd9d45f83a069f1ab
SergeiMish <mishalevskii@yandex.ru>
2022-08-16 14:41:33 +0500

    task 105 Алгоритмы. Числа фибоначчи

---
+++ task 105/src/Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+    }
+
+        private static long fibNaive(int n){
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+
+        }
+    }
