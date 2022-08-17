7f1549b4ce389c4432e5487ff575b7b1ddc8098f
SergeiMish <mishalevskii@yandex.ru>
2022-08-17 19:46:24 +0500

    task 108 Текущая среда и свойства. Система безопасности.

--- tasj 108/src/Test.java
+++ tasj 108/src/Test.java
@@ -1,8 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Текущая системная среда");
+        System.out.println("Текущая системная среда: ");
         System.out.println(System.getenv());
-        System.out.println("Текущие системные свойства");
+        System.out.println("Текущие системные свойства: ");
         System.out.println(System.getProperties());
+        System.out.println("Система безопасности: ");
+        System.out.println(System.getSecurityManager());
     }
 }
