75ca9d6f9ec338571922489698037a82b602f91e
SergeiMish <mishalevskii@yandex.ru>
2022-08-17 19:28:01 +0500

    task 106 Умножение 2-х массивов

---
+++ task 106/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int [] num1 = {5, 3, 34, -2,};
+        int [] num2 = {-3, 4, 5, 10};
+
+    }
+    public void summ(){
+
+    }
+}
