666051d6f683412d38a6954a59f6496af27e0446
SergeiMish <mishalevskii@yandex.ru>
2022-08-17 19:44:36 +0500

    task 108 Текущая среда и свойства.

---
+++ tasj 108/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Текущая системная среда");
+        System.out.println(System.getenv());
+        System.out.println("Текущие системные свойства");
+        System.out.println(System.getProperties());
+    }
+}
