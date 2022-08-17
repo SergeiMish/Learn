98a899896c7784326f84f5566f8d71f2b93e6538
SergeiMish <mishalevskii@yandex.ru>
2022-08-17 20:01:23 +0500

    task 110

---
+++ task 110/src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+    //int[] nums = {10, 10, 2, 4, 9};
+    int[] nums = {10, 10, 2, 4, 20, 20};
+    int ctr_even = 0, ctr_odd = 0;
+	System.out.println("Original Array: "+Arrays.toString(nums));
+
+    boolean found1010 = false;
+    boolean found2020 = false;
+
