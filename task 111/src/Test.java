c35f73b306855d096d6a3b54ff08fb40293a6e2b
SergeiMish <mishalevskii@yandex.ru>
2022-08-18 17:19:26 +0500

    task 111.1 Жадные алгоритмы

--- task 111/src/Test.java
+++ task 111/src/Test.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         int [] digits = {3, 1, 7, 9, 9, 5};
-
-
+        System.out.println(maxNumbOfDig(digits));
     }
-    public static String maxNumbOfDig(){
-
+    public static String maxNumbOfDig(int[] digits){
+        Arrays.sort(digits);
+        String result = "";
+        for (int i = digits.length; i <= 0; i--)
+            result += digits[i];
+        return result;
     }
 }
