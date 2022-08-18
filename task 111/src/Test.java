33c76baac187486843134811f2fec44023574d36
SergeiMish <mishalevskii@yandex.ru>
2022-08-18 18:54:03 +0500

    task 111.4 Жадные алгоритмы

--- task 111/src/Test.java
+++ task 111/src/Test.java
@@ -4,14 +4,15 @@ import java.util.Comparator;
 
 public class Test {
     public static void main(String[] args) {
-        int [] digits = {3, 1, 7, 9, 9, 5};
-        System.out.println(maxNumbOfDig(digits));
+//        int [] digits = {3, 1, 7, 9, 9, 5};
+        int[] stations = {0, 200, 375, 500, 750, 950};
+        System.out.println(minStops(stations, 400));
     }
-    public static String maxNumbOfDig(int[] digits){
-                 return String.join("", Arrays.stream(digits).boxed()
-                         .sorted(Comparator.reverseOrder())
-                       .map(String::valueOf)
-                       .toArray(String[]::new));
+//    public static String maxNumbOfDig(int[] digits){
+//                 return String.join("", Arrays.stream(digits).boxed()
+//                         .sorted(Comparator.reverseOrder())
+//                       .map(String::valueOf)
+//                       .toArray(String[]::new));
 
 //        Arrays.sort(digits);
 //
@@ -21,5 +22,7 @@ public class Test {
 //            result += digits[i];
 //
 //        return result;
+    public static int minStops(int[] station, int capacity){
+
     }
 }
