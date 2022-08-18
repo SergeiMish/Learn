6a0b39400b684943cb490794b6033a31ee84c093
SergeiMish <mishalevskii@yandex.ru>
2022-08-18 19:26:59 +0500

    task 111.10 Жадные алгоритмы

--- task 111/src/Test.java
+++ task 111/src/Test.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class Test {
     public static void main(String[] args) {
 //        int [] digits = {3, 1, 7, 9, 9, 5};
-        int[] stations = {0, 200, 375, 500, 750, 950};
+        int[] stations = {0, 200, 375, 550, 750, 950};
         System.out.println(minStops(stations, 400));
     }
 //    public static String maxNumbOfDig(int[] digits){
@@ -22,16 +22,20 @@ public class Test {
 //            result += digits[i];
 //
 //        return result;
-    public static int minStops(int[] station, int capacity){
+    public static int minStops(int[] stations, int capacity){
         int result = 0;
         int currentStop = 0;
-        while (currentStop < station.length -1){
+
+        while (currentStop < stations.length -1){
             int nextStop = currentStop;
-            while (nextStop < station.length -1 && station[nextStop + 1] - station[currentStop ] <=capacity)
+
+            while (nextStop < stations.length -1 &&
+                    stations[nextStop + 1] - stations[currentStop] <= capacity)
                 nextStop++;
+
             if (currentStop == nextStop)
                 return -1;
-            if (currentStop < station.length -1)
+            if (nextStop < stations.length -1)
                 result++;
             currentStop = nextStop;
             }
