8a6cef6f868cdeb07255b307eb383d451ba88538
SergeiMish <mishalevskii@yandex.ru>
2022-08-19 18:09:51 +0500

    task 113.1

---
+++ task 113/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args)
+    {
+        int[] array_nums = {10, 70, 80, 50, 20, 13, 50};
+        boolean testd = false;
+        int result=0;
+        int x = 10;
+        int y = 20;
+
+        for(int i = 0; i < array_nums.length; i++) {
+            if(array_nums[i] == x)
+                testd = true;
+
+            if(testd && array_nums[i] == y)
+}
