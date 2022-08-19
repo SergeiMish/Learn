3ade1edaab486788039cb0401b42fcf2cd08c52c
SergeiMish <mishalevskii@yandex.ru>
2022-08-19 18:11:32 +0500

    task 113.4

--- task 113/src/Test.java
+++ task 113/src/Test.java
@@ -1,25 +1,15 @@
 public class Test {
     public static void main(String[] args)
     {
-        int[] array_nums = {10, 70, 80, 50, 20, 13, 50};
+        int[] array_nums = {10, 20, 10, 50, 20, 13, 50};
+        //int[] array_nums = {10, 10, 50, 50, 20, 13, 50};
         boolean testd = false;
-        int result=0;
+        int result = 0;
         int x = 10;
-        int y = 20;
 
-        for(int i = 0; i < array_nums.length; i++) {
-            if(array_nums[i] == x)
-                testd = true;
-
-            if(testd && array_nums[i] == y)
+        for(int i = 0; i < array_nums.length - 1; i++) {
+            if(array_nums[i] == x && array_nums[i+1] == x)
             {
                 System.out.printf( String.valueOf(true));
-                result = 1		 ;
+                result = 1 ;
             }
-        }
-        if (result==0)
-        {  System.out.printf( String.valueOf(false));
-        }
-        System.out.printf("\n");
-    }
-}
