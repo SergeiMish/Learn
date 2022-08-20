f11d9368e2d24a28b70c4e086383f5f2a366d5ee
SergeiMish <mishalevskii@yandex.ru>
2022-08-20 11:32:39 +0500

    task 116.1 обьеденение массивов и сортировка Maven

--- untitled3/src/test/java/Main.java
+++ untitled3/src/test/java/Main.java
@@ -2,7 +2,7 @@ import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.Arrays;
 
-public class test {
+public class Main {
     public static void main(String[] args) {
         int[] numb1 = {1, 2, 3, 4,};
         int[] numb2 = {3, 5, 3, 55};
