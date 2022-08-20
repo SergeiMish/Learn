77aab40379f1d11c64aa935b447bf91075cd3cbb
SergeiMish <mishalevskii@yandex.ru>
2022-08-20 08:30:47 +0500

    task 115

---
+++ task 115/src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Test{
+    public static void main(String[] args) {
+        int[] number1  = {1, 2, 3, 4};
+        int[] number2 = {2, 5, 7, 8};
+        int[] allDigits = new int[number1.length + number2.length];
+        System.out.println(Arrays.toString(allDigits));
+
+    }
+}
