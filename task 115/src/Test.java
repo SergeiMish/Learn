6d281316623492c409e788a7222525b437ce53e9
SergeiMish <mishalevskii@yandex.ru>
2022-08-20 08:39:29 +0500

    task 115.1

--- task 115/src/Test.java
+++ task 115/src/Test.java
@@ -2,10 +2,14 @@ import java.util.Arrays;
 
 public class Test{
     public static void main(String[] args) {
+    }
+
+    public int[] arr(){
         int[] number1  = {1, 2, 3, 4};
         int[] number2 = {2, 5, 7, 8};
-        int[] allDigits = new int[number1.length + number2.length];
-        System.out.println(Arrays.toString(allDigits));
-
+        int[] result = Arrays.copyOf(number1, number1.length + number2.length);
+        System.arraycopy(number2, 0, result, number1.length, number2.length);
+        return result;
+        System.out.println(Arrays.toString(result[]));
     }
 }
