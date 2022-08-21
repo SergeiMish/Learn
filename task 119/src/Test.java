dc5bd02584b3991d6e8bf1977eaae986745f9c55
SergeiMish <mishalevskii@yandex.ru>
2022-08-21 22:41:54 +0500

    task 119.1

--- task 119/src/Test.java
+++ task 119/src/Test.java
@@ -1,3 +1,5 @@
+import static java.util.Collections.reverse;
+
 public class Test {
     public static void main(String[] arg)
     {
@@ -7,4 +9,12 @@ public class Test {
         int len = A.length;
         offset %= len;
         reverse(A, 0, len - offset - 1);
+        reverse(A, len - offset, len - 1);
+        reverse(A, 0, len - 1);
+        System.out.println("\n"+Arrays.toString(A));
+    }
+    private static void reverse(char[] str, int start, int end) {
+        while (start < end) {
+            char temp = str[start];
+            str[start] = str[end];
 }
