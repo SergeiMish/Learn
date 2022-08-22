b87dcc897079474e20f1350d8704d6ff06b5fcff
SergeiMish <mishalevskii@yandex.ru>
2022-08-22 07:42:54 +0500

    task 120.6

--- task 120/src/Test.java
+++ task 120/src/Test.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 import static java.lang.Math.sqrt;
 
 public class Test {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int num;
         Scanner in = new Scanner(System.in);
         System.out.print("Input a positive integer: ");
         int n = in.nextInt();
-        System.out.printf("Square root of %d is: ",n);
-        System.out.println(sqrt(n)); }
+        System.out.printf("Square root of %d is: ", n);
+        System.out.println(sqrt(n));
+    }
 
     private static int sqrt(int num) {
         if (num == 0 || num == 1) {
@@ -31,3 +31,4 @@ public class Test {
         }
         return a;
     }
+}
