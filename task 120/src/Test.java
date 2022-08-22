6f016de1fc303ffd8f8d35d30332dc8d1bf40a07
SergeiMish <mishalevskii@yandex.ru>
2022-08-22 07:39:27 +0500

    task 120

---
+++ task 120/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.printf("\n%d: fizz buzz",i);
