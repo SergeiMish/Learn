88888479b65009deea7457611c9b85130e57d2c5
SergeiMish <mishalevskii@yandex.ru>
2022-08-25 22:00:42 +0500

    task 121

---
+++ task 121/src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Test {int array1[]=new int[8];
+    array1[0]=1;
+    array1[1]=2;
+    array1[2]=3;
+    array1[3]=4;
+    int[] array2 = {2,5,7,8};
+        System.out.println("\nArray1: "+Arrays.toString(array1));
+	    System.out.println("\nArray2: "+Arrays.toString(array2));
+}
