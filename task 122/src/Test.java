786dc67ccb707fc71f398d4d16229989888c0826
SergeiMish <mishalevskii@yandex.ru>
2022-08-28 22:57:45 +0500

    task 122.1

--- task 122/src/Test.java
+++ task 122/src/Test.java
@@ -4,5 +4,11 @@ public class Test {
         int[] array_nums = {10, 20, 10, 20, 40, 13, 20};
 
         int count = 0, result =0;
-
+        if(array_nums.length >= 1 && array_nums[0] == 20)
+            count++;
+        for(int i = 1; i < array_nums.length; i++) {
+            if(array_nums[i - 1] == 20 && array_nums[i] == 20)
+            {
+                System.out.printf( String.valueOf(false));
+                result = 1;
     }
