0204f16efa1ba46d3f47feb9561b6ed349bf921c
SergeiMish <mishalevskii@yandex.ru>
2022-09-01 15:56:01 +0500

    task 124

---
+++ task 124/src/Test.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args)
+    {
+        int[] array_nums1 = {10, 11, 10, 20, 43, 20, 50};
+        int[] array_nums2 = {10, 13, 11, 20, 44, 30, 50};
+        System.out.println("Array1: "+Arrays.toString(array_nums1));
+        System.out.println("Array2: "+ Arrays.toString(array_nums2));
+    }
