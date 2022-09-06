3d15226178cdc8d005569496ff3377fceede630a
SergeiMish <mishalevskii@yandex.ru>
2022-09-06 17:21:35 +0500

    task 125.4

--- task 125/src/Test.java
+++ task 125/src/Test.java
@@ -8,4 +8,12 @@ public class Test {
 	   System.out.print("Input a positive integer: ");
     int n = in.nextInt();
        if (n < 1) {
+           System.out.print(Boolean.toString(false));
+        test = 1;
+    }
+		if ((n & (n - 1)) != 0) {
+        System.out.print(Boolean.toString(false));
+        test = 1;
+    }
+		if (test==0)
 }
