b864ae12186e716daa39b6fc5454b01fc2c8b245
SergeiMish <mishalevskii@yandex.ru>
2022-09-06 17:20:19 +0500

    task 125.2

--- task 125/src/Test.java
+++ task 125/src/Test.java
@@ -2,22 +2,9 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args)
-{
-    int[] array_nums = {10, 11, 10, 30, 45, 20, 33, 53};
-    int result = 0;
-    System.out.println("Original Array: "+ Arrays.toString(array_nums));
-
-    int ctr1 = 0;
-    int ctr2 = 0;
-
-    for(int i = 0; i < array_nums.length; i++) {
-        if(array_nums[i] == 10)
-            ctr1++;
-
-        if(array_nums[i] == 20)
-            ctr2++;
-    }
-    System.out.printf(String.valueOf(ctr1 > ctr2));
-    System.out.printf("\n");
-}
+    int test = 0;
+    Scanner in = new Scanner(System.in);
+	   System.out.print("Input a positive integer: ");
+    int n = in.nextInt();
+       if (n < 1) {
 }
