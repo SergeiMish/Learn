837a9b04a6640b613e0eb2a8d56313a8833464a8
SergeiMish <mishalevskii@yandex.ru>
2022-09-07 17:03:06 +0500

    task 126.3 Задача о рюкзаке (Жаднрые алгоритмы)

--- task 126/src/Test.java
+++ task 126/src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
-
+    public static void main(String[] args) {
+        final Item item1 = new Item(4, 20);
+        final Item item2 = new Item(3, 18);
+        final Item item3 = new Item(2, 14);
+        final Item[] items = {item1, item2, item3}; // Рюкзак.
+    }
 }
 
 class Item{
