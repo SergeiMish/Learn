a1517ece24942cde84d2f0a02ec0b929a4abd3a6
SergeiMish <mishalevskii@yandex.ru>
2022-09-07 16:56:56 +0500

    task 126.1 Задача о рюкзаке (Жаднрые алгоритмы)

--- task 126/src/Test.java
+++ task 126/src/Test.java
@@ -5,4 +5,20 @@ public class Test {
 class Item{
     private int weigth;
     private int value;
+
+    Item(int weigth, int value) {
+        this.weigth = weigth;
+        this.value = value;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public String toString(){
+        return "{w: " + weigth + "v: " + value + "}";
+    }
 }
