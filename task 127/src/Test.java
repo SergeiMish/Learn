bc4b813655d27e2712705cb1da608b6c029433aa
SergeiMish <mishalevskii@yandex.ru>
2022-09-08 19:15:20 +0500

    task 127.4 Бинарный поиск

--- task 127/src/Test.java
+++ task 127/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-
+        System.out.println(binarySearch(new int[] {1,5,6,7,8,3,}, 8));
     }
 
     public static int binarySearch (int[] a, int key) {
@@ -9,16 +9,15 @@ public class Test {
         while (low <= high){
             int middle = low + (high - low) / 2;
 
-            if (key < middle) {
+            if (key < a[middle]) {
                 high = middle - 1;
-            } else if (key > middle) {
+            } else if (key > a[middle]) {
                 low = middle + 1;
             }
             else{
                 return middle;
             }
         }
-        }
-
+        return -1;
     }
 }
