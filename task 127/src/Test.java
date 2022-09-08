fb68eec6492848e2922daae047d729d1259ab0d7
SergeiMish <mishalevskii@yandex.ru>
2022-09-08 18:57:42 +0500

    task 127.2 Бинарный поиск

--- task 127/src/Test.java
+++ task 127/src/Test.java
@@ -6,7 +6,9 @@ public class Test {
     public static int binarySearch (int[] a, int key) {
         int low = 0;
         int high = a.length -1;
-
+        while (low <= high){
+            int middle = low + (high - low) / 2;
+        }
 
     }
 }
