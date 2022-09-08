b365c08fd1bd230ad976fa6965d42047911de872
SergeiMish <mishalevskii@yandex.ru>
2022-09-08 19:03:47 +0500

    task 127.3 Бинарный поиск

--- task 127/src/Test.java
+++ task 127/src/Test.java
@@ -8,6 +8,16 @@ public class Test {
         int high = a.length -1;
         while (low <= high){
             int middle = low + (high - low) / 2;
+
+            if (key < middle) {
+                high = middle - 1;
+            } else if (key > middle) {
+                low = middle + 1;
+            }
+            else{
+                return middle;
+            }
+        }
         }
 
     }
