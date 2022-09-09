8ccb4b7657b6d88dbb96a1c8e369aa45903ec201
SergeiMish <mishalevskii@yandex.ru>
2022-09-09 14:43:12 +0500

    task 128.6 SpringApp

--- springappTest/src/main/java/org/example/TestSpring.java
+++ springappTest/src/main/java/org/example/TestSpring.java
@@ -7,5 +7,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+       TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+        context.close();
+
     }
 }
