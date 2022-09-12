cc791a3896c796cbca21c34bf80345b7930695ea
SergeiMish <mishalevskii@yandex.ru>
2022-09-12 22:51:51 +0500

    task 128.8 SpringApp

--- springappTest/src/main/java/org/example/TestSpring.java
+++ springappTest/src/main/java/org/example/TestSpring.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
