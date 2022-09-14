c7832e3e4e3b595aa6c2e7053885a1fda83cdf55
SergeiMish <mishalevskii@yandex.ru>
2022-09-14 14:32:29 +0500

    task 128.14 SpringApp IC

--- springappTest/src/main/java/org/example/MusicPlayer.java
+++ springappTest/src/main/java/org/example/MusicPlayer.java
@@ -6,6 +6,9 @@ public class MusicPlayer {
     public MusicPlayer (Music music){
         this.music = music;
     }
+    public void setMusic(Music music){
+        this.music = music;
+    }
 
     public void playMusic(){
         System.out.println("Playing: " + music.getSong());
