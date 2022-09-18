d61359f244fc9d1d3a8c684650d52f7954defb9f
SergeiMish <mishalevskii@yandex.ru>
2022-09-18 19:27:38 +0500

    task 129.1

--- task 129/src/Test.java
+++ task 129/src/Test.java
@@ -6,4 +6,8 @@ public class Test {
 
         int length2 = str2.length();
         String result = "";
+        result += (str1.length() >= 1) ? str1.charAt(0) : '#';
+        result += (length2 >= 1) ? str2.charAt(length2-1) : '#';
+        System.out.println(result);
+    }
 }
