e6725e6cb82ff6495030e2ddb0d086ab6f9b08c6
SergeiMish <mishalevskii@yandex.ru>
2022-09-18 19:27:12 +0500

    task 129

---
+++ task 129/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args)
+    {
+        String str1 ="Python";
+        String str2 = "";
+
+        int length2 = str2.length();
+        String result = "";
+}
