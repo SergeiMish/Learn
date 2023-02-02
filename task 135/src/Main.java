bd640ea047d7572f672d962b3c9f197bfdaa90b1
SergeiMish <mishalevskii@yandex.ru>
2023-02-02 08:04:53 +0500

    task 135.2

--- task 135/src/Main.java
+++ task 135/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       double [][] numbers = new double[10][10];
-       double i =  Math.random();
-        System.out.println(numbers[(int) i][(int) i]);
+
     }
 }
