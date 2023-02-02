f381346234a5f6418f2a14c0718701fea1530f0c
SergeiMish <mishalevskii@yandex.ru>
2023-02-02 08:08:39 +0500

    task 135.3

--- task 135/src/Main.java
+++ task 135/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int [][] number = {{1,5,6},
+                           {3,8,2},
+                           {4,5,1}};
+        for (int[]numbers : number){
+            System.out.println(Arrays.toString(numbers));
+        }
 
     }
 }
