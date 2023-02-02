bb45c78f3a152375a59b03140ca3b66b87fdc22a
SergeiMish <mishalevskii@yandex.ru>
2023-02-02 21:41:13 +0500

    task 135.7

--- task 135/src/Main.java
+++ task 135/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
          double x = Math.random();
          int k = (int) Math.round(x * 10);
          int [] nubmers = new int[10];
-         for (int i = 0; i<k; i++){
+         for (int i = 0; i<nubmers.length; i++){
              nubmers[i]= k+1;
              System.out.println(nubmers[k]);
          }
