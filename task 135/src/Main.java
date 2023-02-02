81f844f54077cbf8212f7eea52a38bc756d109be
SergeiMish <mishalevskii@yandex.ru>
2023-02-02 20:19:51 +0500

    task 135.6

--- task 135/src/Main.java
+++ task 135/src/Main.java
@@ -1,15 +1,10 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
          double x = Math.random();
          int k = (int) Math.round(x * 10);
-         int [][] nubmers = new int[10][10];
-        nubmers[k][k] = nubmers[k][0] * nubmers[0][k];
-         for (int i = 0; i<nubmers.length; i++){
-             for (int j = 0; j < nubmers.length; i++){
-                 System.out.print(nubmers[i][j] + " ");
-             }
-         }
+         int [] nubmers = new int[10];
+         nubmers[0] = k;
+        System.out.println(nubmers[0]);
     }
 }
