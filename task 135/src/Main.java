98babdc3f34557fcf44de2ccd27270b4201a98d1
SergeiMish <mishalevskii@yandex.ru>
2023-02-02 21:53:55 +0500

    task 135.8

--- task 135/src/Main.java
+++ task 135/src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -8,8 +7,6 @@ public class Main {
         Random rran = new Random();
         numbers = rran.ints().limit(numbers.length).toArray();
         System.out.println("Рандомные числа: ");
-        for (int i = 0; i< numbers.length; i++){
             System.out.println(Arrays.toString(numbers));
-        }
     }
 }
