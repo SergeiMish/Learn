216a21c70e163ea9f287b9829f2b236d8d72d76e
SergeiMish <mishalevskii@yandex.ru>
2023-02-04 16:13:02 +0500

    task 136.2

--- task 136/src/Main.java
+++ task 136/src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
       Moto moto1 = new Moto();
       moto1.name = "Harley Davidson";
+      Moto moto2= new Moto();
+      moto2.name = "Yamaxa";
       moto1.ride();
+      moto2.ride();
 
 
     }
@@ -10,7 +13,9 @@ public class Main {
         String name;
         int age;
         void ride(){
-            System.out.println("Я еду на " + name);
+            for (int i = 0; i<3; i++){
+                System.out.println("Я еду на " + name);
+            }
         }
     }
 }
