4332fd7fbb5fda640653c2cae8b980bd361878bd
SergeiMish <mishalevskii@yandex.ru>
2023-02-06 07:51:50 +0500

    task 136.4

--- task 136/src/Main.java
+++ task 136/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
       Moto moto1 = new Moto();
-      moto1.name = "Harley Davidson";
+      moto1.setName("Harley Davidson");
       Moto moto2= new Moto();
       moto2.name = "Yamaxa";
       moto1.gasolineConsumption();
+        System.out.println("Мотоцикл " + moto1.name + ", с расходом " + moto1.gasolineConsumption());
 
 
 
@@ -17,6 +18,9 @@ public class Main {
         int gasolineConsumption(){
             int hp = 100/middleConsumption;
             return hp;
+        }
+        void setName(String motoName){
+            motoName = name;
         }
         void ride(){
             for (int i = 0; i<3; i++){
