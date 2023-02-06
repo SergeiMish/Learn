d9f0e4e57c8988fb5e4da77fd644bf2d5a943820
SergeiMish <mishalevskii@yandex.ru>
2023-02-06 20:59:28 +0500

    task 136.7

--- task 136/src/Main.java
+++ task 136/src/Main.java
@@ -1,35 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-      Moto moto1 = new Moto();
-      moto1.setName("Harley Davidson");
-      Moto moto2= new Moto();
-      moto2.name = "Yamaxa";
-      moto1.gasolineConsumption();
-      moto1.gasOnTop();
-
-
-
-
+        Moto moto1 = new Moto();
+        moto1.name = ("Harley Davidson");
+        Moto moto2 = new Moto();
+        moto2.name = "Yamaxa";
     }
-    static class Moto{
-        String name;
-        int age;
-        int middleConsumption =15;
-        int gasolineConsumption(){
-            int hp = 100/middleConsumption;
-            return hp;
+
+    class Moto {
+        private String name;
+        private int age;
+
+        public void setName(String name) {
+            this.name = name;
         }
-        void setName(String motoName){
-            name = motoName;
-        }
-        void ride(){
-            for (int i = 0; i<3; i++){
-                System.out.println("Я еду на " + name);
-            }
-        }
-        void gasOnTop(){
-            System.out.println("Мотоцикл " + name + ", с расходом " + gasolineConsumption());
-            }
+
+        public void setAge(int age) {
+            this.age = age;
         }
     }
+
+    void goRide() {
+        System.out.println("Я еду на " + name + " и ему всего " + age);
+    }
+
+}
 
