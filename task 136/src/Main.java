186c2f741176ec5fc1ffdf311f8f8f1c74aba991
SergeiMish <mishalevskii@yandex.ru>
2023-02-08 22:39:28 +0500

    task 136.15

--- task 136/src/Main.java
+++ task 136/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    StringBuilder sb = new StringBuilder("Hello");
-    sb.append("5");
-    sb.append("Опять");
+        StringBuilder sb = new StringBuilder("Hello ");
+        sb.append(5);
+        sb.append(" Опять");
         System.out.println(sb.toString());
     }
 }
