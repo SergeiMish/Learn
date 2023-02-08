d150cdd6d399cecf774ff0898d742630bf570f9f
SergeiMish <mishalevskii@yandex.ru>
2023-02-08 22:29:00 +0500

    task 136.14

--- task 136/src/Main.java
+++ task 136/src/Main.java
@@ -1,25 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Human h1 = new Human("Мерлин", 180);
-        Human h2 = new Human("Борис",20);
-        Human h3 = new Human("Лариса", 26);
-        h1.countOfPeople();
-        h3.countOfPeople();
+    StringBuilder sb = new StringBuilder("Hello");
+    sb.append("5");
+    sb.append("Опять");
+        System.out.println(sb.toString());
     }
 }
-    class Human {
-        private String name;
-        private int age;
-
-        public static int countPeople;
-
-        public Human(String name, int age){
-        this.name = name;
-        this.age = age;
-        countPeople++;
-
-        }
-        public void countOfPeople(){
-            System.out.println("Количество людей в чате: " + countPeople);
-        }
-    }
