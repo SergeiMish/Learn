3fb6d0bde24fd7232c54a26fde7500ecb722c1d9
SergeiMish <mishalevskii@yandex.ru>
2023-02-09 21:18:32 +0500

    task 137.1

---
+++ task 137/src/Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+    public void eat(){
+        System.out.println("Я ем");
+            }
+    public void sleep(){
+        System.out.println("Я сплю");
+    }
+}
---
+++ task 137/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.sleep();
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
