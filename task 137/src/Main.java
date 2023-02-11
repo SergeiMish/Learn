8ab104d83d3681bf04270dd4f80102fc4680e157
SergeiMish <mishalevskii@yandex.ru>
2023-02-11 12:12:44 +0500

    task 137.8

--- task 137/src/Main.java
+++ task 137/src/Main.java
@@ -1,5 +1,6 @@
 import Interfeces.Animal;
 import Interfeces.Human;
+import Interfeces.Info;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +8,10 @@ public class Main {
         Human human = new Human("Марго");
         animal.showInfo();
         human.showInfo();
+        outPutInfo(animal);
+        outPutInfo(human);
+    }
+    public static void outPutInfo(Info info){
+        info.showInfo();
     }
 }
