e74a5cc5fdc989cd47ed271ad029db226dd027fe
SergeiMish <mishalevskii@yandex.ru>
2023-02-12 21:24:52 +0500

    task 139.3

--- task 138/src/Dog.java
+++ task 138/src/Dog.java
@@ -1,3 +1,10 @@
 public class Dog extends Animal{
+    @Override
+    public void eat(){
 
+    }
+ public void bark(){
+     System.out.println("ГавГав");
+
+ }
 }
--- task 138/src/Test.java
+++ task 138/src/Test.java
@@ -2,8 +2,6 @@ public class Test {
     public static void main(String[] args) {
 
         Animal animal = new Animal();
-        Dog dog = new Dog();
-        dog.eat();
         animal.eat();
 
     }
