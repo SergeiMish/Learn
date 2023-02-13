1b03521600c3060e7b57174a605e044ff4a5fbe9
SergeiMish <mishalevskii@yandex.ru>
2023-02-13 19:28:20 +0500

    task 139.6

--- task 138/src/Test.java
+++ task 138/src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-          Integer x = 123;
-          Integer y = new Integer(123);
+          Dog dog = new Dog();
+          Animal animal = dog;
 
     }
 }
