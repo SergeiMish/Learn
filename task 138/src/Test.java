4823071a533c26348b6acc2104b83501ce7419d8
SergeiMish <mishalevskii@yandex.ru>
2023-02-14 08:04:48 +0500

    task 139.15

--- task 138/src/Test.java
+++ task 138/src/Test.java
@@ -1,9 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Test {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.eat();
     }
+    Animal animal2 = new Animal(){
+        @Override
+        public void eat(){
+            System.out.println("Второе животное есть");
+        }
+    };
 }
