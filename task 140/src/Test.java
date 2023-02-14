43ec7132007994f783c16d91dcdf41a8d69428e2
SergeiMish <mishalevskii@yandex.ru>
2023-02-14 22:10:58 +0500

    Task 140.4

--- task 140/src/Test.java
+++ task 140/src/Test.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,27 +10,31 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         String separator = File.separator;
         String path = separator + "C:" + separator + "Users" + separator + "HP M" + separator + "Desktop" + separator + "Текстовый документ.txt";
         File file = new File(path);
         String names;
 
         List<String> fio = new ArrayList<>();
+        file = (File) Files.readAllLines(path);
 
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()){
-            names = scanner.nextLine();
-            fio.add(names);
-
-        }
         System.out.println(fio.toString());
 
-        for (String allFio : fio){
-            System.out.println(allFio);
-        }
 
-        scanner.close();
+//        Scanner scanner = new Scanner(file);
+//        while (scanner.hasNextLine()){
+//            names = scanner.nextLine();
+//            fio.add(names);
+//
+//        }
+//        System.out.println(fio.toString());
+//
+//        for (String allFio : fio){
+//            System.out.println(allFio);
+//        }
+//
+//        scanner.close();
     }
 }
 //"C:\Users\HP M\Desktop\Текстовый документ.txt"
