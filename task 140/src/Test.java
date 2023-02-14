51c033d0154cf08a45ee912f9cc97058b7e8f98d
SergeiMish <mishalevskii@yandex.ru>
2023-02-14 21:59:04 +0500

    Task 140.3

--- task 140/src/Test.java
+++ task 140/src/Test.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,13 +11,23 @@ public class Test {
         String separator = File.separator;
         String path = separator + "C:" + separator + "Users" + separator + "HP M" + separator + "Desktop" + separator + "Текстовый документ.txt";
         File file = new File(path);
+        String names;
+
+        List<String> fio = new ArrayList<>();
 
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()){
-            System.out.println(scanner.nextLine());
-            scanner.close();
+            names = scanner.nextLine();
+            fio.add(names);
+
+        }
+        System.out.println(fio.toString());
+
+        for (String allFio : fio){
+            System.out.println(allFio);
         }
 
+        scanner.close();
     }
 }
 //"C:\Users\HP M\Desktop\Текстовый документ.txt"
