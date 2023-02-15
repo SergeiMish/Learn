a88d0fb9bd64c689dc974a07b3b7195f3c6da513
SergeiMish <mishalevskii@yandex.ru>
2023-02-15 07:59:49 +0500

    Task 140.6

--- task 140/src/Test.java
+++ task 140/src/Test.java
@@ -1,12 +1,6 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Test {
@@ -14,29 +8,22 @@ public class Test {
         String separator = File.separator;
         String path = separator + "C:" + separator + "Users" + separator + "HP M" + separator + "Desktop" + separator + "Текстовый документ.txt";
         File file = new File(path);
-        String names;
-
-//        List<Integer> fio = new ArrayList<>();
-//        file = (File) Files.readAllLines(path);
-//
-//        System.out.println(fio.toString());
 
 
         Scanner scanner = new Scanner(file);
-        String line  = scanner.nextLine();
-
-        }
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
 
         int[] numbers = new int[5];
         int counter = 0;
 
 
-        for (Integer allFio : fio){
-            numbers[counter++] = Integer.parseInt(String.valueOf(allFio));
+        for (String number : numbersString) {
+            numbers[counter++] = Integer.parseInt(number);
 
             System.out.println(Arrays.toString(numbers));
-//        }
-//
-//        scanner.close();
+
+            scanner.close();
+        }
     }
 }
