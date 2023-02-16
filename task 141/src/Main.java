5722b2d877895a85d0c27e7b0a5084f913cab6fe
SergeiMish <mishalevskii@yandex.ru>
2023-02-16 07:58:35 +0500

    task 141.1

---
+++ task 141/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("asdlasd");
+        Scanner scanner = new Scanner(file);
+
+
+    }
+}
