a1255078e04f3d36e3746e9b10a48bf9f42f8cec
SergeiMish <mishalevskii@yandex.ru>
2023-02-16 19:56:19 +0500

    task 141.7

--- task 141/src/Main.java
+++ task 141/src/Main.java
@@ -1,13 +1,20 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("Введите 0");
         Scanner scanner = new Scanner(System.in);
         while (true){
            int x = Integer.parseInt(scanner.nextLine());
            if (x != 0){
-               System.out.println("Введите 0");
-           }
+               try {
+                   throw new IOException();
+               }catch (IOException e) {
+                   System.out.println("Вы ввели не 0");
+               }
+           }else System.out.println("Молодец, возьми с полки пирожок");
+
         }
 
     }
