e74035022488747f626e9b5feb9028549ab05d28
SergeiMish <mishalevskii@yandex.ru>
2023-02-19 14:18:39 +0500

    task 141.11

--- task 141/src/Main.java
+++ task 141/src/Main.java
@@ -1,15 +1,19 @@
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ScannerException {
-        System.out.println("Введите 0");
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            int x = Integer.parseInt(scanner.nextLine());
-            if (x != 0) {
-                throw new ScannerException();
-            }
+    public static void main(String[] args){
+        try {
+            run();
+        } catch (ParseException | IOException e) {
+            throw new RuntimeException(e);
         }
+
     }
+
+    public static void run() throws ParseException, IOException {
+
+    }
+
 }
