09f1edd3900b8e945ef060f255f9a02e117d74f5
SergeiMish <mishalevskii@yandex.ru>
2023-02-19 13:51:11 +0500

    task 141.9

--- task 141/src/Main.java
+++ task 141/src/Main.java
@@ -2,18 +2,14 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ScannerException {
         System.out.println("Введите 0");
         Scanner scanner = new Scanner(System.in);
-        while (true){
-           int x = Integer.parseInt(scanner.nextLine());
-           if (x != 0){
-               try {
-                   throw new IOException();
-               }catch (IOException e) {
-                   System.out.println("Вы ввели не 0");
-               }
-           }else System.out.println("Молодец, возьми с полки пирожок");
+        while (true) {
+            int x = Integer.parseInt(scanner.nextLine());
+            if (x != 0) {
+                throw new ScannerException();
+            }
         }
     }
 }
---
+++ task 141/src/ScannerException.java
@@ -0,0 +1,4 @@
+public class ScannerException extends Exception{
+
+
+}
