e139a310f0e19242d39d2d07d64b7c673d36cc42
SergeiMish <mishalevskii@yandex.ru>
2023-02-21 21:41:50 +0500

    task 145.5

--- task 145/src/WriteObject.java
+++ task 145/src/WriteObject.java
@@ -7,18 +7,23 @@ public class WriteObject {
     public static void main(String[] args) {
         Person person1 = new Person("Петр",25);
         Person person2 = new Person("Мария", 22);
+        Person[] people = {
+        new Person("Петр",25),new Person("Мария", 22), new Person("Эдуард", 40) };
 
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-            oos.writeObject(person1);
-            oos.writeObject(person2);
+            oos.writeInt(people.length);
 
-            oos.close();
+            for (Person person : people){
+                oos.writeObject(person);
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 }
