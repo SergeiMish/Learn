8c45649815118911d0d67aed9125f31a07d1156c
SergeiMish <mishalevskii@yandex.ru>
2023-02-21 21:34:09 +0500

    task 145.4

--- task 145/src/Person.java
+++ task 145/src/Person.java
@@ -1,20 +1,18 @@
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable {
             private int id;
             private String name;
-
             public Person(String name, int id){
                 this.name = name;
                 this.id = id;
             }
-
             public int getId() {
                 return id;
             }
-
             public String getName() {
                 return name;
             }
-
             public String toString(){
                 return id + ":" + name;
             }
--- task 145/src/WriteObject.java
+++ task 145/src/WriteObject.java
@@ -1,5 +1,7 @@
-import java.io.FileNotFoundException;
+
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class WriteObject {
     public static void main(String[] args) {
@@ -8,7 +10,14 @@ public class WriteObject {
 
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
-        } catch (FileNotFoundException e) {
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(person1);
+            oos.writeObject(person2);
+
+            oos.close();
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
