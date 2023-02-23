850decfd9fc9fa002d0ad22c152f01df9f8c68c3
SergeiMish <mishalevskii@yandex.ru>
2023-02-23 11:02:42 +0500

    task 146.0

---
+++ task 146/src/test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+
+        Animal animal = Animal.CAT;
+        switch (animal){
+            case CAT:
+                System.out.println("Its a cat");
+                break;
+            case DOG:
+                  System.out.println("Its a dog");
+                  break;
+        }
+    }
+}
