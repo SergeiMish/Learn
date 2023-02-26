6f61e209aac68504172f3784bf9d9baf6b362df5
SergeiMish <mishalevskii@yandex.ru>
2023-02-26 19:23:22 +0500

    task 147.3

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -1,20 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-        public boolean canBeIncreasing ( int[] nums){
-            int count = 0;
-            int p = 0;
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] > nums[i + 1] || nums[i] == nums[i + 1]) {
-                    count++;
-                    p = i;
-                }
-            }
-            if (count > 1) return false;
-            else if (count == 1) {
-                if (p == 0 || p == nums.length - 2) return true;
-                if (nums[p + 1] > nums[p - 1] || nums[p + 2] > nums[p]) return true;
-                else return false;
-            }
-            return true;
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            arrayList.add(i);
+
+            System.out.println(arrayList.toString());
         }
     }
+}
