9b5af958dd401bdf8348fe86bc12106a5e80eacd
SergeiMish <mishalevskii@yandex.ru>
2023-02-26 18:31:00 +0500

    task 147.1

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -1,26 +1,28 @@
-
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        FileReader fileReader = new FileReader("test");
-
-        int x;
-
-            while ((x = fileReader.read())!= -1){
-
-                System.out.println(x);
-
-
+            ArrayList<Integer> time = new ArrayList<>();
+            int result;
+            String str;
+            FileReader file = new FileReader("input.txt");
+            Scanner sc = new Scanner(file);
+            str = sc.nextLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+            while (st.hasMoreTokens()) {
+                time.add(Integer.valueOf(st.nextToken()));
+            }
+            result = (time.get(1) > time.get(0)) ? time.get(1) - time.get(0) : 12 + time.get(1) - time.get(0);
+            FileWriter fileOut = new FileWriter("output.txt");
+            fileOut.write(String.valueOf(result));
+            fileOut.close();
         }
-
-
-
     }
-}
+
