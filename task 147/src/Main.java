99aa1977dc70c4a83e3c9a6e44921aec73b7f54c
SergeiMish <mishalevskii@yandex.ru>
2023-02-27 20:15:28 +0500

    task 147.9

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -1,21 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
 
-        Scanner scan = new Scanner(new FileReader("test"));
-        ArrayList<String> data = new ArrayList<>() ;
-        while(scan.hasNextLine()){
-            data.add(scan.nextLine());
-        }
-        System.out.println(data + "\n");
+        System.out.println("Im gonna sleep");
+
+        Thread.sleep(3000);
+
+        System.out.println("Im sleep");
+
 
     }
 }
