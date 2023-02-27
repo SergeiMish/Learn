d1ee347bd90db5a63939809e1a9aee3933374897
SergeiMish <mishalevskii@yandex.ru>
2023-02-27 22:13:01 +0500

    task 147.14

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -1,34 +1,13 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        myThread.start();
-
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-
-        myThread.stopRun();
-
 
     }
 }
-
-    class MyThread extends Thread{
-
-        private volatile boolean stop = true;
-
-        public void run(){
-            while (stop){
-                System.out.println("Hello");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-        public void stopRun(){
-            this.stop = false;
-        }
+    interface EuroSocket{
+    void getPower();
     }
+
+    interface USSoket{
+    void getPower();
+    }
+    Class Radio(){
