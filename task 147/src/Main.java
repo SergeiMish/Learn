42a9ef571315914edc296d3ce3fc6afbaec71017
SergeiMish <mishalevskii@yandex.ru>
2023-02-28 08:15:53 +0500

    task 147.16

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -1,17 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
-    }
-}
-    interface EuroSocket{
-    void getPower();
-    }
-
-    interface USSoket{
-    void getPower();
-    }
-    class Radio{
-    public void listenMusic(EuroSocket euroSocket){
-    euroSocket.getPower();
+        int x = 44;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add(i);
+            System.out.println(numbers);
         }
     }
+}
