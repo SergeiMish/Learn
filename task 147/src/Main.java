bcc39a89c4b04fb6364fb267372180713ab7f69b
SergeiMish <mishalevskii@yandex.ru>
2023-02-28 19:48:53 +0500

    task 147.17

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -5,17 +5,28 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         int x = 44;
-        int [] numbers = new int[100];
+        int low;
+        int high;
+        int index = -1;
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            numbers[i]
+            numbers.add(i);
         }
-            System.out.println(numbers);
-
-        for (int j = 0; j < numbers.size(); j++){
-            if (j != x){
-                j = numbers.toArray().length;
+        System.out.println(numbers);
+    }
+    public static int binarySeatch {
+        while (high<=low) {
+            int mid = low + (high - low) / 2;
+            if (mid != x) {
+                low = mid + 1;
+            } else if (mid > x) {
+                high = mid - 1;
+            } else if (mid == x) {
+                index = mid;
+                break;
             }
         }
+        return index;
+            }
         }
 
-    }
