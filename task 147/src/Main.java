f94751261e1f989dbdac8b196cc399b5c45e5faf
SergeiMish <mishalevskii@yandex.ru>
2023-02-28 20:05:21 +0500

    task 147.18

--- task 147/src/Main.java
+++ task 147/src/Main.java
@@ -1,32 +1,32 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 44;
-        int low;
-        int high;
-        int index = -1;
+        int valueToFind = 44;
+        int[] values = {1, 1, 2, 3, 4, 10};
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             numbers.add(i);
         }
         System.out.println(numbers);
+        System.out.printf("Index = %d%n", binarySearch(values, valueToFind, 0, values.length - 1));
+
     }
-    public static int binarySeatch {
-        while (high<=low) {
+    private static int binarySearch(int[] sortedArray, int valueToFind, int low, int high) {
+        int index = -1;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (mid != x) {
+            if (sortedArray[mid] < valueToFind) {
                 low = mid + 1;
-            } else if (mid > x) {
+            } else if (sortedArray[mid] > valueToFind) {
                 high = mid - 1;
-            } else if (mid == x) {
+            } else if (sortedArray[mid] == valueToFind) {
                 index = mid;
                 break;
             }
         }
         return index;
+    }
             }
-        }
 
