0b683727cc6a17a7b64d7aa561e38541c145143d
SergeiMish <mishalevskii@yandex.ru>
2023-03-01 21:26:19 +0500

    Spring-app2.1 (7)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
@@ -1,4 +1,13 @@
 package ru.sergeimish.springapp2;
 
 public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music){
+        this.music = music;
+    }
+
+    public void playMusic(){
+        System.out.println("Сейчас играет " + music.getSong());
+    }
 }
