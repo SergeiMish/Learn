2dac3e4f7797480c6f9537d0a9483b9c7d886355
SergeiMish <mishalevskii@yandex.ru>
2023-03-01 19:34:05 +0500

    Spring-app2.1 (4)

---
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/TestSpring.java
@@ -0,0 +1,12 @@
+package ru.sergeimish.springapp2;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        context.getBean("testBean");
+    }
+}
