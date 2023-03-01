719ed5ba8430d6b8b63fa1fa9ff0b74d98b96d8e
SergeiMish <mishalevskii@yandex.ru>
2023-03-01 08:26:53 +0500

    task 148.1

--- task 148/src/Main.java
+++ task 148/src/Main.java
@@ -1,15 +1,17 @@
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         int[][] table = new int[10][10];
-
-        for (int i = 0; i < 10; i++){
-            int j= i++;
-            table [i][j] = table[i][j] * table[j][i];
-
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                table[i][j] = i * j;
+            }
         }
-        for (int[] numbers: table){
-            for (int elem : numbers) System.out.println(elem);
+            for (int[] numbers : table) {
+                System.out.println(Arrays.toString(numbers));
+            }
         }
     }
-}
