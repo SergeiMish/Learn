3a19c46a068118a0efc5df98846782b831416a57
SergeiMish <mishalevskii@yandex.ru>
2023-03-02 21:14:26 +0500

    Spring-app2.1 (14)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
@@ -1,6 +1,26 @@
 package ru.sergeimish.springapp2;
 
 public class MusicPlayer {
+
+    private String name;
+    public int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     private Music music;
 
     public MusicPlayer(Music music){
