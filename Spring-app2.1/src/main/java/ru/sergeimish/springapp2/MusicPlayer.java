89558a405ee10822c08012daa90a11825027075b
SergeiMish <mishalevskii@yandex.ru>
2023-03-03 08:13:52 +0500

    Spring-app2.1 (19)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class MusicPlayer {
 
-    List<Music>musicList = new ArrayList<>();
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     public int volume;
@@ -28,16 +28,28 @@ public class MusicPlayer {
 
     private Music music;
 
-    public MusicPlayer(Music music){
-        this.music = music;
-    }
-    public MusicPlayer(){}
-
-    public void setMusic(Music music){
+    public MusicPlayer(Music music) {
         this.music = music;
     }
 
-    public void playMusic(){
-        System.out.println("Now playing " + music.getSong());
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Now playing " + music.getSong());
+        }
+
+        public List<Music> getMusicList () {
+            return musicList;
+        }
+
+        public void setMusicList (List < Music > musicList) {
+            this.musicList = musicList;
+        }
     }
 }
