85858ce6ce861c51aaf2f218ac4a57922c6fa096
SergeiMish <mishalevskii@yandex.ru>
2023-03-05 20:58:38 +0500

    Spring-app2.1 (28)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/RockMusic.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/RockMusic.java
@@ -1,6 +1,12 @@
 package ru.sergeimish.springapp2;
 
 public class RockMusic implements Music{
+    public void doMyInit(){
+        System.out.println("Im initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("Im destroy your bean");
+    }
     @Override
     public String getSong() {
         return "Highway to hell";
