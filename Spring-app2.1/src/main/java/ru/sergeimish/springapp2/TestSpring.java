986d2d2434b0177bb281d95dff10820238ef3206
SergeiMish <mishalevskii@yandex.ru>
2023-03-05 21:01:44 +0500

    Spring-app2.1 (29)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/TestSpring.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/TestSpring.java
@@ -7,22 +7,26 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        RockMusic rockMusic = context.getBean("musicBean1",RockMusic.class);
+
+        System.out.println(rockMusic.getSong());
 //        Music music = context.getBean("musicBean", Music.class);
 //
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = musicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-        System.out.println(musicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        musicPlayer.setVolume(25);
-
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = musicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//        System.out.println(musicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        musicPlayer.setVolume(25);
+//
+//        System.out.println(musicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
