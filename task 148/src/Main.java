d32e2d97f10f2a6d8782e50391669ae594bfa1c7
SergeiMish <mishalevskii@yandex.ru>
2023-03-06 22:36:15 +0500

    task 148.2

--- task 148/src/Main.java
+++ task 148/src/Main.java
@@ -1,17 +1,14 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        int[][] table = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                table[i][j] = i * j;
-            }
+        List<Integer> table = new ArrayList<>();
+        for (int i=0; i <10; i++){
+            table.add(i);
         }
-            for (int[] numbers : table) {
-                System.out.println(Arrays.toString(numbers));
-            }
-        }
+        System.out.println(table.toString());
     }
+}
