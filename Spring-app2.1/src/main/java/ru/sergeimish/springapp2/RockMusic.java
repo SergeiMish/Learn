f62d99b22fffdeed950bee84f7c2486825e96b19
SergeiMish <mishalevskii@yandex.ru>
2023-03-07 07:59:54 +0500

    Spring-app2.1 (37)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/RockMusic.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/RockMusic.java
@@ -2,19 +2,19 @@ package ru.sergeimish.springapp2;
 
 import org.springframework.stereotype.Component;
 
-@Component("someRock")
+@Component()
 public class RockMusic implements Music{
-    private RockMusic(){}
-
-    public static RockMusic getRockMusic() {
-        return new RockMusic();
-    }
-    public void doMyInit(){
-        System.out.println("Im initialization");
-    }
-    public void doMyDestroy(){
-        System.out.println("Im destroy your bean");
-    }
+//    private RockMusic(){}
+//
+//    public static RockMusic getRockMusic() {
+//        return new RockMusic();
+//    }
+//    public void doMyInit(){
+//        System.out.println("Im initialization");
+//    }
+//    public void doMyDestroy(){
+//        System.out.println("Im destroy your bean");
+//    }
     @Override
     public String getSong() {
         return "Highway to hell";
