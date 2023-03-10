1d3e48d89fc92be44eef0504fb965a8ce7a486e4
SergeiMish <mishalevskii@yandex.ru>
2023-03-10 18:09:43 +0500

    Spring-app2.1 (42)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private Music music;
-    public MusicPlayer(Music music){ this.music = music;}
+    private ClassicalMusic classicalMusic;
+    public MusicPlayer(ClassicalMusic classicalMusic){ this.classicalMusic = classicalMusic;}
 
     public void playMusic(){
-        System.out.println("Now playing " + music.getSong());
+        System.out.println("Now playing " + classicalMusic.getSong());
 
         }
     }
