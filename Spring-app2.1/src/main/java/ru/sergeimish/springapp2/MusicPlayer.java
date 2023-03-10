11b6f918afc12077232a98391f663c677c593e2d
SergeiMish <mishalevskii@yandex.ru>
2023-03-10 18:08:05 +0500

    Spring-app2.1 (42)

--- Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
+++ Spring-app2.1/src/main/java/ru/sergeimish/springapp2/MusicPlayer.java
@@ -1,50 +1,14 @@
 package ru.sergeimish.springapp2;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-
-    private List<Music> musicList = new ArrayList<>();
-
-    private String name;
-    public int volume;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
     private Music music;
+    public MusicPlayer(Music music){ this.music = music;}
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+    public void playMusic(){
+        System.out.println("Now playing " + music.getSong());
 
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-    public void playMusicList() {
-        for (Music music : musicList) {
-            System.out.println("Now playing " + music.getSong());
         }
     }
-}
