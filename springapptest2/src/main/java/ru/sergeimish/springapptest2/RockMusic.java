29b055a96d043aa82a5611a4d5803a4c5b80483b
SergeiMish <mishalevskii@yandex.ru>
2023-03-11 10:37:55 +0500

    Spring-app2.1 (42)

--- springapptest2/src/main/java/ru/sergeimish/springapptest2/RockMusic.java
+++ springapptest2/src/main/java/ru/sergeimish/springapptest2/RockMusic.java
@@ -1,9 +1,18 @@
 package ru.sergeimish.springapptest2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("musicBean1")
 public class RockMusic {
+    @Autowired
+    private RockMusic rockMusic;
+
+    public MusicPlayer (RockMusic rockMusic){ this.rockMusic = rockMusic;}
+
+    public void playMusic(){
+        System.out.println("Now playing " + rockMusic.getSong(););
+    }
     public void getSong(){
         System.out.println("Kollisium");
     }
