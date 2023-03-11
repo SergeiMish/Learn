757a9f2877c432d704e8620090f6092241925534
SergeiMish <mishalevskii@yandex.ru>
2023-03-11 11:37:15 +0500

    springTest

--- springapptest2/src/main/java/ru/sergeimish/springapptest2/RockMusic.java
+++ springapptest2/src/main/java/ru/sergeimish/springapptest2/RockMusic.java
@@ -2,7 +2,6 @@ package ru.sergeimish.springapptest2;
 
 import org.springframework.stereotype.Component;
 
-@Component("musicBean1")
 public class RockMusic implements Music {
     public String getSong() {
         return "Kollizium";
