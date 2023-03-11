4a54c596efc879c5016e207b83edc7618b4e7d93
SergeiMish <mishalevskii@yandex.ru>
2023-03-11 11:42:50 +0500

    springTest

--- springapptest2/src/main/java/ru/sergeimish/springapptest2/MusicPlayer.java
+++ springapptest2/src/main/java/ru/sergeimish/springapptest2/MusicPlayer.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
-        @Autowired
+
         private Music music;
-        public MusicPlayer(Music music){ this.music = music;}
+    @Autowired
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer(Music music){ this.music = music;}
 
         public void playMusic(){
             System.out.println("Now playing " + music.getSong());
