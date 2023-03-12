98ec73344d5da8fbacff1a3acfa6c81c1e33ad30
SergeiMish <mishalevskii@yandex.ru>
2023-03-12 22:23:12 +0500

    springTest

--- springapptest2/src/main/java/ru/sergeimish/springapptest2/RockMusic.java
+++ springapptest2/src/main/java/ru/sergeimish/springapptest2/RockMusic.java
@@ -4,19 +4,20 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class RockMusic implements Music {
-    public void Rock() {
-        MusicStyle musicStyle = MusicStyle.ROCK;
+    public String Rock() {
+        MusicStyle musicRock = MusicStyle.ROCK;
         List<String> musicStile = new ArrayList<>();
         musicStile.add("Song of Silents");
         musicStile.add("Invaders must die");
         musicStile.add("Mutter");
-
+        Random random = new Random();
+        return musicStile.get(random.nextInt(musicStile.size()));
     }
-
     public String getSong() {
-        return "Kollizium";
+        return musicRock;
     }
 }
