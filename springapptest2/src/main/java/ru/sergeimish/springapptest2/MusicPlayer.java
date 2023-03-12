d55f543dbf9fa0187a36bfdc9b4c2ac76225fc24
SergeiMish <mishalevskii@yandex.ru>
2023-03-12 21:21:07 +0500

    springTest

--- springapptest2/src/main/java/ru/sergeimish/springapptest2/MusicPlayer.java
+++ springapptest2/src/main/java/ru/sergeimish/springapptest2/MusicPlayer.java
@@ -1,10 +1,12 @@
 package ru.sergeimish.springapptest2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
     @Autowired
+    @Qualifier("rockMusic")
     private Music music;
 
         public String playMusic(){
