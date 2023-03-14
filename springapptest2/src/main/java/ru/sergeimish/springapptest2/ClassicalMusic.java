2281b2339b9f2de56267430f619280384341ee53
SergeiMish <mishalevskii@yandex.ru>
2023-03-14 20:48:59 +0500

    springTest

--- springapptest2/src/main/java/ru/sergeimish/springapptest2/ClassicalMusic.java
+++ springapptest2/src/main/java/ru/sergeimish/springapptest2/ClassicalMusic.java
@@ -4,15 +4,20 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @Scope("prototype")
 public class ClassicalMusic implements Music {
-@PostConstruct
+    @PostConstruct
     public void doMyInit(){
         System.out.println("Do my init");
+    }
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Do my destroy");
     }
     private List<String> classicalSongs = new ArrayList<>();
 
