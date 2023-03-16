f6a1b412987cbe35d6963d2215969e74ec5c134c
SergeiMish <mishalevskii@yandex.ru>
2023-03-16 19:18:06 +0500

    task 148.5

--- task 148/src/Main.java
+++ task 148/src/Main.java
@@ -6,21 +6,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[]table = {1,2,4,5,6,7,9,};
-        int index = Integer.MAX_VALUE;
+        int[] table = {1, 2, 4, 5, 6, 7, 9,};
+        int index;
         int low = table[0];
         int high = table.length;
         int key = 5;
-        while (low <= high){
-            int mid = low + ((high - low)/2);
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
             if (table[mid] < key) {
                 low = mid + 1;
-            }else if (table[mid] > key){
+            } else if (table[mid] > key) {
                 high = mid - 1;
-            }else  if (table[mid] == key){
-            }index = mid;
+            } else if (table[mid] == key) {
+                index = mid;
+            }
+            System.out.println(index);
         }
-        System.out.println(index);
-        }
-
-}
+    }
+    }
