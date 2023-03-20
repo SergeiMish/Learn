dbdd476dab6f56487f0a47bc69f90506772c0e58
SergeiMish <mishalevskii@yandex.ru>
2023-03-20 21:48:36 +0500

    MVCtest2 3

--- MVCtest2/src/main/java/ru/MVCtest/config/SpringConfig.java
+++ MVCtest2/src/main/java/ru/MVCtest/config/SpringConfig.java
@@ -1,4 +1,9 @@
 package ru.MVCtest.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.MVCtest")
 public class SpringConfig {
 }
