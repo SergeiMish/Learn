5e6a47a7835bece33ba977bff921fbd0c4be2699
SergeiMish <mishalevskii@yandex.ru>
2023-03-20 21:49:56 +0500

    MVCtest2 3

--- MVCtest2/src/main/java/ru/MVCtest/config/SpringConfig.java
+++ MVCtest2/src/main/java/ru/MVCtest/config/SpringConfig.java
@@ -2,8 +2,10 @@ package ru.MVCtest.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("ru.MVCtest")
+@EnableWebMvc
 public class SpringConfig {
 }
