bd5a71ac3df8dbc1ccd11993d900b30543ef32f5
SergeiMish <mishalevskii@yandex.ru>
2023-03-21 21:39:37 +0500

    MVCtest2 4

--- MVCtest2/src/main/java/ru/MVCtest/config/controllers/FirstController.java
+++ MVCtest2/src/main/java/ru/MVCtest/config/controllers/FirstController.java
@@ -1,7 +1,13 @@
 package ru.MVCtest.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
+    @GetMapping
+    public String helloPage(){
+
+    }
+
 }
