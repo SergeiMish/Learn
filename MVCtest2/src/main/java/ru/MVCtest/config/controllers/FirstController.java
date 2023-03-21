ff02f1ed25a5ec0898cec22d8c98eade02800d89
SergeiMish <mishalevskii@yandex.ru>
2023-03-21 22:25:28 +0500

    MVCtest2 4

--- MVCtest2/src/main/java/ru/MVCtest/config/controllers/FirstController.java
+++ MVCtest2/src/main/java/ru/MVCtest/config/controllers/FirstController.java
@@ -2,16 +2,19 @@ package ru.MVCtest.config.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
+
     @GetMapping("/hello")
-    public String helloPage(){
-    return "first/hello";
-    }
-    @GetMapping("/goodbye")
-    public String goodByePage(){
-    return "first/goodbye";
+    public String helloPage() {
+        return "first/hello";
     }
 
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
 }
