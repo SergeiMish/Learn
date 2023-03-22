92ffce9335dee259a5735b8de147df2c41e8c518
SergeiMish <mishalevskii@yandex.ru>
2023-03-22 21:06:04 +0500

    MVCtest3 1

--- MVCtest3/src/main/java/ru/MVCtest3/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ MVCtest3/src/main/java/ru/MVCtest3/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,4 +1,23 @@
 package ru.MVCtest3.config;
 
-public class MySpringMvcDispatcherSerlvetIntitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Neil Alishev
+ */
+public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
