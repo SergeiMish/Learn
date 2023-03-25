1d81551e238c5d5eed8d541f735763ba990e241f
SergeiMish <mishalevskii@yandex.ru>
2023-03-25 15:16:39 +0500

    task 149

--- task 149/src/Main.java
+++ task 149/src/Main.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-       int [] array = {1, 2, 3, 4, 15, 17, 18, 22};
-       int IndexPoison = 2;
+       int [] array = {1, 2, 3, 4, 15, 17, 18, 22, 33};
+       int IndexPoison = 4;
        int newValue = 7;
 
         System.out.println("Original array " + Arrays.toString(array));
 
-        for (int i = array.length; i > IndexPoison; i--){
+        for (int i = array.length - 1; i > IndexPoison; i--){
             array[i] = array[i-1];}
         array[IndexPoison] = newValue;
         System.out.println("New Array " + Arrays.toString(array));
