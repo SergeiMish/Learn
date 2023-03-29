3defe522ec729b014368f657a0350640bca9a571
SergeiMish <mishalevskii@yandex.ru>
2023-03-29 21:54:50 +0500

    MVCtest2 5

--- MVCtest2/src/main/java/ru/MVCtest/config/controllers/FirstController.java
+++ MVCtest2/src/main/java/ru/MVCtest/config/controllers/FirstController.java
@@ -27,9 +27,9 @@ public class FirstController {
         return "first/goodbye";
     }
     @GetMapping("/calculator")
-    public String calculator (@RequestParam (required = false, name = "a") Integer a ,
-                              @RequestParam(required = false, name = "b") Integer b,
-                             @RequestParam (required = false, name = "action") String action, Model model) {
+    public String calculator (@RequestParam(required = false) int a,
+                              @RequestParam(required = false) int b,
+                             @RequestParam ("action") String action, Model model) {
 
         double result;
 
