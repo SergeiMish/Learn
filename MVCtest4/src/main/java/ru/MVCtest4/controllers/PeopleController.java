0e675d84807298e945399c586a3bb6cb50a350f7
SergeiMish <mishalevskii@yandex.ru>
2023-03-30 22:41:34 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -1,9 +1,15 @@
 package ru.MVCtest4.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+
+    @GetMapping
+    public String index(){
+        return null;
+    }
 }
