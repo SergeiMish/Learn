04b46e254068fa8052a4554eb550b2ae8f91f17a
SergeiMish <mishalevskii@yandex.ru>
2023-03-31 22:13:31 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDao.java
+++ MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDao.java
@@ -2,8 +2,15 @@ package ru.MVCtest4.dao;
 
 import ru.MVCtest4.models.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDao {
-    private List<Person> peoplel;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+    }
+
 }
