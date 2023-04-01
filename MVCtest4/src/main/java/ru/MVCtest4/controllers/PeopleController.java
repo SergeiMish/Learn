9819628079cb4828bc85fe19bbe33dee7cb56478
SergeiMish <mishalevskii@yandex.ru>
2023-04-01 20:03:02 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -5,12 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.MVCtest4.dao.PersonDao;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-
+    private PersonDao personDao;
 
     @GetMapping
     public String index(Model model){
