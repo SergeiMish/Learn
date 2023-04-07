a934004c3b1e0d20ce189b58e6cd70dcc5b8c3ad
SergeiMish <mishalevskii@yandex.ru>
2023-04-07 22:12:34 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -32,6 +32,7 @@ public class PeopleController {
     return "people/new";
     }
 
+
     @PostMapping
     public String create(@ModelAttribute("person") Person person) {
         personDao.save(person);
