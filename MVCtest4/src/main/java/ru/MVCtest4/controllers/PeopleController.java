5c0eb295dbbbbd2e7112544c09633033eaabf13a
SergeiMish <mishalevskii@yandex.ru>
2023-04-08 20:50:43 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -21,6 +21,7 @@ public class PeopleController {
             model.addAttribute("people", personDao.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
     public String show(@PathVariable ("id") int id, Model model){
         model.addAttribute("person", personDao.show(id));
