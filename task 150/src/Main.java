8f6ad48d4f154e2f642dbd17d7e6e53c507930f7
SergeiMish <mishalevskii@yandex.ru>
2023-04-11 21:27:47 +0500

    task 150

--- task 150/src/Main.java
+++ task 150/src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
-        public static int binarySearch ( int arr[], int elementToSearch){
+    public static int recursiveBinarySearch(int arr[], int firstElement, int lastElement, int elementToSearch) {
+        if (lastElement >= firstElement) {
+            int mid = firstElement + (lastElement - firstElement) / 2;
 
-            int firstIndex = 0;
-            int lastIndex = arr.length - 1;
-            while (firstIndex <= lastIndex) {
-                int middleIndex = (firstIndex + lastIndex) / 2;
-                if (arr[middleIndex] == elementToSearch) {
-                    return middleIndex;
-                } else if (arr[middleIndex] < elementToSearch)
-                    firstIndex = middleIndex + 1;
-                else if (arr[middleIndex] > elementToSearch)
-                    lastIndex = middleIndex - 1;
-            }
-            return -1;
-        }
-    }
+
