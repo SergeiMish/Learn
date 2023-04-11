fb63a4bd0090c03b326504fc7819f689482b648c
SergeiMish <mishalevskii@yandex.ru>
2023-04-11 21:25:27 +0500

    task 150

---
+++ task 150/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        public static int binarySearch(int arr[], int elementToSearch) {
+
+            int firstIndex = 0;
+            int lastIndex = arr.length - 1;
+            while(firstIndex <= lastIndex) {
+                int middleIndex = (firstIndex + lastIndex) / 2;
+    }
+}
