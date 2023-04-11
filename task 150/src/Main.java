5ae376fb35878ddbbf564efa3d7caa028cdaece1
SergeiMish <mishalevskii@yandex.ru>
2023-04-11 21:28:24 +0500

    task 150

--- task 150/src/Main.java
+++ task 150/src/Main.java
@@ -4,10 +4,11 @@ public class Main {
             int mid = firstElement + (lastElement - firstElement) / 2;
             if (arr[mid] == elementToSearch)
                 return mid;
-
             if (arr[mid] > elementToSearch)
                 return recursiveBinarySearch(arr, firstElement, mid - 1, elementToSearch);
 
             return recursiveBinarySearch(arr, mid + 1, lastElement, elementToSearch);
         }
+        return -1;
+    }}
 
