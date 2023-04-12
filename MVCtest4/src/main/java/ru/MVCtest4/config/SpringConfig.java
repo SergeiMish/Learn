b1ec4a92135673977e3832f8a5b1439d041e126a
SergeiMish <mishalevskii@yandex.ru>
2023-04-12 21:53:03 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/config/SpringConfig.java
+++ MVCtest4/src/main/java/ru/MVCtest4/config/SpringConfig.java
@@ -46,6 +46,8 @@ public class SpringConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setContentType("text/html; charset=UTF-8");
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
