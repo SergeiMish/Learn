23e2badfd18f7c664a8e60d234002e23dea3aca4
SergeiMish <mishalevskii@yandex.ru>
2023-04-19 21:25:49 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
+++ MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
@@ -1,10 +1,12 @@
 package ru.MVCtest4.models;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public class Person {
     private int id;
     @NotEmpty(message = "Name shout not be empty")
+    @Size(min = 2, max = 30, message = "Name shot be between 2 and 30 characters")
     private String name;
     private int age;
     private String email;
