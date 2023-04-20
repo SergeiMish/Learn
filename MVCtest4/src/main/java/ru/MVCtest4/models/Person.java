dd91f349019c9ebb14b0d505ca15c4a7114e5792
SergeiMish <mishalevskii@yandex.ru>
2023-04-20 23:00:12 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
+++ MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
@@ -9,9 +9,9 @@ public class Person {
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
-
     @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+
     private String email;
 
     public Person() {
