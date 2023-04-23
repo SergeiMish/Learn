0835c44e0c76ca667ce3e122fbe216400e1ec258
SergeiMish <mishalevskii@yandex.ru>
2023-04-23 22:42:08 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
+++ MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
@@ -24,10 +24,11 @@ public class Person {
     public int getId() {
         return id;
     }
-    
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
