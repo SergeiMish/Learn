1ffec2f20cdfdcfabc348cd5b8c84b0acdb84b00
SergeiMish <mishalevskii@yandex.ru>
2023-04-29 09:03:33 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -61,6 +61,7 @@ public class PeopleController {
         personDAO.update(id, person);
         return "redirect:/people";
     }
+
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
