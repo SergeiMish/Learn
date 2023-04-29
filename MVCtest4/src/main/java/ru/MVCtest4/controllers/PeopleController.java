d6fb283116b44fa53a5c3fa250a4fe97bb6ff888
SergeiMish <mishalevskii@yandex.ru>
2023-04-29 09:04:03 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -12,7 +12,6 @@ import ru.MVCtest4.models.Person;
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
-
     private final PersonDAO personDAO;
     @Autowired
     public PeopleController(PersonDAO personDAO) {
