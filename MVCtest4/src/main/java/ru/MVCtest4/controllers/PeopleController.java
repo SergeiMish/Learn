4d89d14dad61ea0828192916b139122f1457cdc4
SergeiMish <mishalevskii@yandex.ru>
2023-05-02 21:22:27 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -44,12 +44,13 @@ public class PeopleController {
         return "redirect:/people";
     }
 
+
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
-    
+
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person")@Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
