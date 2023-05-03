52f3e80d802aa5554bfbaf3d8938746db64716eb
SergeiMish <mishalevskii@yandex.ru>
2023-05-03 22:06:43 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
+++ MVCtest4/src/main/java/ru/MVCtest4/controllers/PeopleController.java
@@ -46,7 +46,6 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
-
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person")@Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
