aaa03886e32768a6c29893281de33030556e6b53
SergeiMish <mishalevskii@yandex.ru>
2023-05-08 23:05:14 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDAO.java
+++ MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDAO.java
@@ -12,7 +12,6 @@ import java.util.List;
 public class PersonDAO {
     private static int PEOPLE_COUNT;
 
-
     private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "postgres";
