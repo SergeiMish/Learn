1a89af37d61a3cc272145d17775868d2f2c63965
SergeiMish <mishalevskii@yandex.ru>
2023-05-24 22:14:03 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/config/SpringConfig.java
+++ MVCtest4/src/main/java/ru/MVCtest4/config/SpringConfig.java
@@ -37,6 +37,7 @@ public class SpringConfig implements WebMvcConfigurer {
 
 
 
+    
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
