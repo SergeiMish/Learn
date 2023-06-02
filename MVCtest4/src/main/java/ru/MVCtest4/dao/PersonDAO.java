71c8f9765888c978242c2b71342c14a7a1edf2a5
SergeiMish <mishalevskii@yandex.ru>
2023-06-02 10:08:33 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDAO.java
+++ MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDAO.java
@@ -18,7 +18,7 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-    return jdbcTemplate.query("")
+    return jdbcTemplate.query("SELECT * FROM Person", )
     }
     public Person show(int id) {
         Person person = null;
