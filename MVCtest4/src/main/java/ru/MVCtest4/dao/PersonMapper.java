9e4d77f495eb606a708bec802a00afbecd9ef524
SergeiMish <mishalevskii@yandex.ru>
2023-06-02 10:10:31 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/dao/PersonMapper.java
+++ MVCtest4/src/main/java/ru/MVCtest4/dao/PersonMapper.java
@@ -3,6 +3,13 @@ package ru.MVCtest4.dao;
 
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PersonMapper implements RowMapper {
 
+    @Override
+    public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return null;
+    }
 }
