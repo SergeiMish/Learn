1f0c0d4c5e784cf1e95e53d05604035cf01fdcab
SergeiMish <mishalevskii@yandex.ru>
2023-06-04 22:01:31 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/dao/PersonMapper.java
+++ MVCtest4/src/main/java/ru/MVCtest4/dao/PersonMapper.java
@@ -10,7 +10,8 @@ import java.sql.SQLException;
 public class PersonMapper implements RowMapper<Person> {
 
 
-    
+
+
     @Override
     public Person mapRow(ResultSet resultSet, int i) throws SQLException {
         Person person = new Person();
