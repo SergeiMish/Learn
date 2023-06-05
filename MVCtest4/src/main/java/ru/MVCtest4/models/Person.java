9fe5e2c424a07b9b184646530404b08b38a8e442
SergeiMish <mishalevskii@yandex.ru>
2023-06-05 22:25:36 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
+++ MVCtest4/src/main/java/ru/MVCtest4/models/Person.java
@@ -17,6 +17,7 @@ public class Person {
     private String email;
     public Person() {
     }
+
     public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
