d2505befe577e19bebd65ed1b2255e4aac0e32b3
SergeiMish <mishalevskii@yandex.ru>
2023-06-07 09:54:44 +0500

    MVCtest4

--- MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDAO.java
+++ MVCtest4/src/main/java/ru/MVCtest4/dao/PersonDAO.java
@@ -19,10 +19,10 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-    return jdbcTemplate.query("SELECT * FROM Person",new BeanPropertyRowMapper<>());
+    return jdbcTemplate.query("SELECT * FROM Person",new BeanPropertyRowMapper<>(Person.class ));
     }
     public Person show(int id) {
-    return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new PersonMapper())
+    return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
             .stream().findAny().orElse(null);
     }
     public void save(Person person) {
