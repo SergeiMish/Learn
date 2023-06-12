a761234381f10136524c862cc07818fe7a55231b
SergeiMish <mishalevskii@yandex.ru>
2023-06-12 19:44:02 +0500

    task 150

--- task 151/src/test.java
+++ task 151/src/test.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 public class test {
     public static void main(String[] args) {
         ArrayList<Integer> x = new ArrayList<>();
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 10; i++){
             x.add(i);
         }
+        x.remove(5);
         System.out.println(x);
     }
 }
