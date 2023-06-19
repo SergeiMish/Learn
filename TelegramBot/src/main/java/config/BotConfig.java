43903ac98e57b60f018b3f7e592d1334cabb6af4
SergeiMish <mishalevskii@yandex.ru>
2023-06-19 21:52:43 +0500

    TelegramBot

--- TelegramBot/src/main/java/config/BotConfig.java
+++ TelegramBot/src/main/java/config/BotConfig.java
@@ -1,6 +1,8 @@
 package config;
 
 import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Data
