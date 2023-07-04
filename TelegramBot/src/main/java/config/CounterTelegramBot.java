be4bfb93080429707672cbe03fd01774b9e50f1c
SergeiMish <mishalevskii@yandex.ru>
2023-07-04 23:14:56 +0500

    TelegramBot

--- TelegramBot/src/main/java/config/CounterTelegramBot.java
+++ TelegramBot/src/main/java/config/CounterTelegramBot.java
@@ -35,7 +35,6 @@ public class CounterTelegramBot extends TelegramLongPollingBot {
         }
     }
 
-
     private void startBot(long chatId, String userName) {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
