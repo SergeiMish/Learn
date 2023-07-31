17a187c9944558926cb48df1b67352406e2943f1
SergeiMish <mishalevskii@yandex.ru>
2023-07-31 22:14:53 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/controllers/GreetingController.java
+++ Project 2.0/src/main/java/com/Project2/controllers/GreetingController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-
     @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
