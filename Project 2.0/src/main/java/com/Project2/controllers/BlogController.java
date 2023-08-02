968c15cb51bcf7441254a22cdce2e00b1eff28e9
SergeiMish <mishalevskii@yandex.ru>
2023-08-02 21:02:12 +0500

    Project2

---
+++ Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
@@ -0,0 +1,13 @@
+package com.Project2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String Blog (Model model){
+        return "blog-main";
+    }
+}
