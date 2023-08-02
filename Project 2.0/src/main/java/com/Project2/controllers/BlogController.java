f3418313dbe5424af5a094a5a577ec607faaa94f
SergeiMish <mishalevskii@yandex.ru>
2023-08-02 21:17:17 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
+++ Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
@@ -1,5 +1,4 @@
 package com.Project2.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
