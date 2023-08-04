ba98144ae7eacfcf4f4eb09f3bc5a4822ec0652f
SergeiMish <mishalevskii@yandex.ru>
2023-08-04 23:47:47 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/controllers/GreetingController.java
+++ Project 2.0/src/main/java/com/Project2/controllers/GreetingController.java
@@ -10,6 +10,7 @@ public class GreetingController {
     @GetMapping("/")
 
 
+
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
