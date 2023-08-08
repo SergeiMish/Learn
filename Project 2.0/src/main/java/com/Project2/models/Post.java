93490a9f83202ba1c4070f283b57f893d676cb2c
SergeiMish <mishalevskii@yandex.ru>
2023-08-08 21:36:27 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/models/Post.java
+++ Project 2.0/src/main/java/com/Project2/models/Post.java
@@ -1,11 +1,14 @@
 package com.Project2.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Post {
     @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
 
 }
