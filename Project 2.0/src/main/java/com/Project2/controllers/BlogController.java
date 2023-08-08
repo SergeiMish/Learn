a575e256aecd65eadefded2c0c3aa78e275be2dc
SergeiMish <mishalevskii@yandex.ru>
2023-08-08 21:55:34 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
+++ Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
@@ -1,10 +1,15 @@
 package com.Project2.controllers;
+import com.Project2.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
     @GetMapping("/blog")
     public String Blog (Model model){
         return "blog-main";
