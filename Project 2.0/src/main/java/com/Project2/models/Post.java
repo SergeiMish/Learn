f5224e14200d07d7e6ff78307c42085ddc249b2f
SergeiMish <mishalevskii@yandex.ru>
2023-08-08 21:45:41 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/models/Post.java
+++ Project 2.0/src/main/java/com/Project2/models/Post.java
@@ -11,6 +11,22 @@ public class Post {
     @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     private String title, anons, full_text;
     private int views;
 }
