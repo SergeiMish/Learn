75cc727ecba994cb550fe3bce23a30881a101f1e
SergeiMish <mishalevskii@yandex.ru>
2023-08-08 21:53:25 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/repo/PostRepository.java
+++ Project 2.0/src/main/java/com/Project2/repo/PostRepository.java
@@ -1,6 +1,7 @@
 package com.Project2.repo;
 
+import com.Project2.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PostRepository extends CrudRepository {
+public interface PostRepository extends CrudRepository<Post, Long> {
 }
