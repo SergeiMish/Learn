49bef2fcc510e0c2cffb4b8d6e8467402273bee0
SergeiMish <mishalevskii@yandex.ru>
2023-09-07 17:47:54 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
+++ Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
@@ -60,14 +60,7 @@ public class BlogController {
         model.addAttribute("post", res);
         return "blog-edit";
     }
-    @PostMapping("/blog/{id}/edit")
-    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
-        Post post = postRepository.findById(id).orElseThrow();
-        post.setTitle(title);
-        post.setAnons(anons);
-        post.setFull_text(full_text);
-        postRepository.save(post);
-        return "redirect:/blog";
+
     }
     @PostMapping("/blog/{id}/remove")
     public String blogPostDelete(@PathVariable(value = "id") long id, Model model) {
