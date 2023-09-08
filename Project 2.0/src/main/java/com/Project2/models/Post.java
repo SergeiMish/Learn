a77e43b2bbe1cecb4d949f429ae71903f9cddfa5
SergeiMish <mishalevskii@yandex.ru>
2023-09-08 18:21:26 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/models/Post.java
+++ Project 2.0/src/main/java/com/Project2/models/Post.java
@@ -56,9 +56,6 @@ public class Post {
 
     private String title, anons, full_text;
     private int views;
-    public Post(String title, String anons, String full_text) {
-        this.title = title;
-        this.anons = anons;
-        this.full_text = full_text;
+
     }
 }
