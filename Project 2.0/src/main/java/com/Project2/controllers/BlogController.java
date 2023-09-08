6d9705ca8c4f9df0e1dec6ba38b1b42c8a56a3c5
SergeiMish <mishalevskii@yandex.ru>
2023-09-08 18:22:36 +0500

    Project2

--- Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
+++ Project 2.0/src/main/java/com/Project2/controllers/BlogController.java
@@ -42,7 +42,11 @@ public class BlogController {
         if (!postRepository.existsById(id)) {
             return "redirect:/blog";
         }
-
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
     }
 
     @GetMapping("/blog/{id}/edit")
