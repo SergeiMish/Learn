7df88eb63b760fa995e2464ec4c66f390ce80b12
SergeiMish <mishalevskii@yandex.ru>
2023-09-11 15:34:26 +0500

    TelegramBot

--- TelegramBot/src/main/java/CounterTelegramBot.java
+++ TelegramBot/src/main/java/CounterTelegramBot.java
@@ -3,17 +3,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
-@Component
 @Slf4j
-public class CounterTelegramBot implements TelegramLongPollingBot {
+@Component
+public class CounterTelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
-    @Override
-    public void onUpdateReceived(Update update) {
 
-    }
-
+    public CounterTelegramBot(BotConfig config) { this.config = config; }
     @Override
-    public String getBotUsername() {
-        return null;
-    }
+    public String getBotUsername() { return config.getBotName(); }
+    @Override
+    public String getBotToken() { return config.getToken(); }
+    @Override
+    public void onUpdateReceived(@NotNull Update update) {}
 }
