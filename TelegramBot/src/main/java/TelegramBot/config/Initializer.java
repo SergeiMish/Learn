f5162c57c271373d05edb9ce6e07ed2b72b9615b
SergeiMish <mishalevskii@yandex.ru>
2023-09-12 16:26:19 +0500

    TelegramBot

--- TelegramBot/src/main/java/TelegramBot/config/Initializer.java
+++ TelegramBot/src/main/java/TelegramBot/config/Initializer.java
@@ -1,5 +1,6 @@
 package TelegramBot.config;
 
+import TelegramBot.CounterTelegramBot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
