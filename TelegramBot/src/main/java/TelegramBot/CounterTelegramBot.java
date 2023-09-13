cc5d47c537eaef0782899a6c4d5f68e7f12e7ffc
SergeiMish <mishalevskii@yandex.ru>
2023-09-13 22:17:30 +0500

    TelegramBot

--- TelegramBot/src/main/java/TelegramBot/CounterTelegramBot.java
+++ TelegramBot/src/main/java/TelegramBot/CounterTelegramBot.java
@@ -35,15 +35,7 @@ public class CounterTelegramBot extends TelegramLongPollingBot {
             }
         }
     }
-    private void startBot(long chatId, String userName){
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-        message.setText("Привет человек," + userName + "! Я бот телега, добро пожаловать!");
-        try {
-            execute(message);
-            log.info("Попробуй заново");
-        }catch (TelegramApiException e){
-            log.error(e.getMessage());
+
         }
     }
 }
