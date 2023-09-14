bf3f58cfe731d755fecd56a4c49d185d0c364fdb
SergeiMish <mishalevskii@yandex.ru>
2023-09-14 21:05:31 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/controller/BotConfig.java
+++ PT1/src/main/java/com/telegramPT/PT1/controller/BotConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Data
-@PropertySource("config.properties")
+@PropertySource("application.properties")
 public class BotConfig {
     @Value() String botName;
     @Value String token;
