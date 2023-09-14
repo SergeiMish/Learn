05375367d077f5e9341059aee45cd1710327d1da
SergeiMish <mishalevskii@yandex.ru>
2023-09-14 21:01:21 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/controller/BotConfig.java
+++ PT1/src/main/java/com/telegramPT/PT1/controller/BotConfig.java
@@ -1,4 +1,11 @@
 package com.telegramPT.PT1.controller;
 
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("config.properties")
 public class BotConfig {
 }
