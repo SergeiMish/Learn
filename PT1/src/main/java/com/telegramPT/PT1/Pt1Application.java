ecf37eb81ce757075e551534dc430cf1348716a4
SergeiMish <mishalevskii@yandex.ru>
2023-09-14 20:53:14 +0500

    PT1

---
+++ PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
@@ -0,0 +1,13 @@
+package com.telegramPT.PT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pt1Application.class, args);
+	}
+
+}
