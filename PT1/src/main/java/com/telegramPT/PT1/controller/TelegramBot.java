e69d634a39f33cc87f738b830caf0a1624918a99
SergeiMish <mishalevskii@yandex.ru>
2023-09-14 21:28:52 +0500

    PT1

---
+++ PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
@@ -0,0 +1,17 @@
+package com.telegramPT.PT1.controller;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+}
