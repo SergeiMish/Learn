fa34cd58b7f1be72093b1b7bfdf307958b4e681f
SergeiMish <mishalevskii@yandex.ru>
2023-09-17 23:21:40 +0500

    TelegramBot

--- TelegramBot/src/main/java/TelegramBot/config/Initializer.java
+++ TelegramBot/src/main/java/TelegramBot/config/Initializer.java
@@ -20,7 +20,11 @@ public class Initializer {
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() {
-
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot((LongPollingBot) bot);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
         }
     }
 }
