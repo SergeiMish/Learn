fcc1d0063c796f7d2c2282dc340b25248f2076a2
SergeiMish <mishalevskii@yandex.ru>
2023-09-18 22:57:13 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
+++ PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
@@ -1,9 +1,11 @@
 package com.telegramPT.PT1.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 @Slf4j
+@Component
 public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
