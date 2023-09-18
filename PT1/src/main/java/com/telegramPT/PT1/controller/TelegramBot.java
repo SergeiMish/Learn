cd33a3496f88f3bc961d2492c02540e88101b1ef
SergeiMish <mishalevskii@yandex.ru>
2023-09-18 23:00:46 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
+++ PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
@@ -4,10 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -21,32 +18,6 @@ public class TelegramBot extends TelegramLongPollingBot {
         return null;
     }
     @Override
-    public void onUpdateReceived(@NotNull Update update){
+    public void onUpdateReceived(@NotNull Update update)
 
-        if (update.hasMessage()&& update.getMessage().hasText()){
-        String messageText = update.getMessage().getText();
-        long chatId = update.getMessage().getChatId();
-        String memberName = update.getMessage().getFrom().getFirstName();
-
-        switch (messageText){
-            case "/start":
-                startBot(chatId, memberName);
-                break;
-            default: log.info("Неправильно значение");
-        }
-    }
 }
-    private void startBot(long chatId, String userName){
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-        message.setText("Привет человек," + userName + "! Я бот телега, добро пожаловать!");
-        try {
-            execute(message);
-            log.info("Попробуй заново");
-        }catch (TelegramApiException e){
-            log.error(e.getMessage());
-        }
-    }
-}
-
-
