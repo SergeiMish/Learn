08aad7fe39896ace867032f6957208aa5848b633
SergeiMish <mishalevskii@yandex.ru>
2023-09-19 20:28:53 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
+++ PT1/src/main/java/com/telegramPT/PT1/controller/TelegramBot.java
@@ -1,5 +1,6 @@
 package com.telegramPT.PT1.controller;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
