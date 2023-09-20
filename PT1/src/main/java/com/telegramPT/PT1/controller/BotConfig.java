da799f78cd9460b2d0a4e4774a31789b8b890905
SergeiMish <mishalevskii@yandex.ru>
2023-09-20 21:35:12 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/controller/BotConfig.java
+++ PT1/src/main/java/com/telegramPT/PT1/controller/BotConfig.java
@@ -1,17 +1,17 @@
-package com.telegramPT.PT1.controller;
-
-import lombok.Data;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@Data
-@PropertySource("application.properties")
-public class BotConfig {
-    @Value("${bot.name}") String botName;
-    @Value("${bot.token}") String token;
-    @Value("${bot.chatId}") String chatId;
-
-}
+//package com.telegramPT.PT1.controller;
+//
+//import lombok.Data;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//
+//@Configuration
+//@Data
+//@PropertySource("application.properties")
+//public class BotConfig {
+//    @Value("${bot.name}") String botName;
+//    @Value("${bot.token}") String token;
+//    @Value("${bot.chatId}") String chatId;
+//
+//}
