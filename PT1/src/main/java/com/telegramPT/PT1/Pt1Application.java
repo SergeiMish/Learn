accc2ea2028c269b3bc9c2926abe2f8503a33fd6
SergeiMish <mishalevskii@yandex.ru>
2023-09-21 21:14:48 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
+++ PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
@@ -1,6 +1,5 @@
 package com.telegramPT.PT1;
 
-import com.telegramPT.PT1.controller.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
