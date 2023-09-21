6bcad9f55410e59fce189534c1ab78abafa245a7
SergeiMish <mishalevskii@yandex.ru>
2023-09-21 21:31:20 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
+++ PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
@@ -1,13 +1,22 @@
 package com.telegramPT.PT1;
-
+import com.telegramPT.PT1.controller.TelegramBot;
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 
 @SpringBootApplication
 public class Pt1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Pt1Application.class, args);
+		ApiContextInitializer.init();
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+		try {
+			telegramBotsApi.registerBot(TelegramBot.getBot());
+		} catch (TelegramApiRequestException e) {
+			e.printStackTrace();
+		}
 	}
 }
