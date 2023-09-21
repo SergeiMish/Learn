744de7507cb937b4be01a1d4c9a702a1950d4357
SergeiMish <mishalevskii@yandex.ru>
2023-09-21 21:58:23 +0500

    PT1

--- PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
+++ PT1/src/main/java/com/telegramPT/PT1/Pt1Application.java
@@ -3,9 +3,10 @@ package com.telegramPT.PT1;
 import com.telegramPT.PT1.controller.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
 public class Pt1Application {
 
 	public static void main(String[] args) {
