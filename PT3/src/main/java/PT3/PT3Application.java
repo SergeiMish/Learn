b51f14e2a0074a52c150c7a12a602ba70a1af910
SergeiMish <mishalevskii@yandex.ru>
2023-09-25 22:09:09 +0500

    PT3

--- PT3/src/main/java/PT3/PT3Application.java
+++ PT3/src/main/java/PT3/PT3Application.java
@@ -1,11 +1,12 @@
 package PT3;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PT3Application {
     public static void main(String[] args) {
-        Te
+        SpringApplication.run(PT3Application.class);
     }
 }
