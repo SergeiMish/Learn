bcc43f49b27596503dbfce40ecf71cd2375ea0fb
SergeiMish <mishalevskii@yandex.ru>
2023-09-25 22:04:27 +0500

    PT3

--- PT3/src/main/java/PT3/TelegramBot.java
+++ PT3/src/main/java/PT3/TelegramBot.java
@@ -1,4 +1,30 @@
 package PT3;
 
-public class TelegramBot {
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+import java.util.List;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
 }
