3962bb9b034a60ece1e71acec29074a14723d382
SergeiMish <mishalevskii@yandex.ru>
2023-09-26 21:02:30 +0500

    PT3

--- PT3/src/main/java/PT3/PT3Application.java
+++ PT3/src/main/java/PT3/PT3Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PT3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(PT3Application.class);
     }
