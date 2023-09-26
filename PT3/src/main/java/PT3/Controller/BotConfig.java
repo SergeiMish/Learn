097076606d7865d7552e2ae6722a244bce6120ee
SergeiMish <mishalevskii@yandex.ru>
2023-09-26 21:01:25 +0500

    PT3

--- PT3/src/main/java/PT3/Controller/BotConfig.java
+++ PT3/src/main/java/PT3/Controller/BotConfig.java
@@ -1,9 +1,11 @@
 package PT3.Controller;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Data
 public class BotConfig {
     @Value("${bot.name}") String botName;
     @Value("${bot.token}") String token;
