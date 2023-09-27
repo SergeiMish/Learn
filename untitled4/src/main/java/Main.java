63c6891dee9606f64107e0bb28dd34036a0b2c70
SergeiMish <mishalevskii@yandex.ru>
2023-09-27 09:04:59 +0500

    task HRlink

--- untitled4/src/main/java/Main.java
+++ untitled4/src/main/java/Main.java
@@ -1,2 +1,12 @@
+import java.awt.*;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<Word> words = List.of(
+                Word.of("large", -3)
+
+        )
+    }
 }
