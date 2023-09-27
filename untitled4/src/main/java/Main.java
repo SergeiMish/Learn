ec166ceefb00ac35d02708d5a10d494312013d8c
SergeiMish <mishalevskii@yandex.ru>
2023-09-27 10:16:02 +0500

    task HRlink

--- untitled4/src/main/java/Main.java
+++ untitled4/src/main/java/Main.java
@@ -1,39 +1,9 @@
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-
-        List<Word> words = List.of(
-                Word.of("large", -3),
-                Word.of("phone", -6),
-                Word.of("newspaper", 2),
-                Word.of("chocolate", -10),
-                Word.of("connection", 13),
-                Word.of("engineering", 10)
-        );
-        System.out.println(
-                words.stream()
-                        .filter(Predicate.not(Word::isBad))
-                        .max(Comparator.comparing(Word::getPriority))
-                        .map(Word::getValue)
-                        .orElse("Nothing found")
-        );
-    }
-    @AllArgsConstructor
-    @Getter
-    public static class Word {
-        private final String value;
-        private final int priority;
-
-        static Word of(String value, int priority){
-            return new Word(value, priority);
+        public int calculate(int n, int left, int right) {
+        
         }
-        public boolean isBad(){ return this.value.contains("La");}
 
     }
 }
