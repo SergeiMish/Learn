1d910aa2a2465c9199564ece542c5b65f5767d02
SergeiMish <mishalevskii@yandex.ru>
2023-09-27 10:21:23 +0500

    task HRlink

--- untitled4/src/main/java/Main.java
+++ untitled4/src/main/java/Main.java
@@ -2,7 +2,24 @@
 public class Main {
     public static void main(String[] args) {
         public int calculate(int n, int left, int right) {
-        
+            int highestLeft = 0;
+            int smallestRight = Integer.MAX_VALUE;
+            for (int i = 0; i <= n + 1; i++) {
+                if (i < left.length && left[i] > highestLeft) {
+                    highestLeft = left[i];
+                }
+                if (i < right.length && right[i] < smallestRight) {
+                    smallestRight = right[i];
+                }
+            }
+            if (left.length != 0 && right.length == 0) {
+                return highestLeft;
+            }
+            if (right.length != 0 && left.length == 0) {
+                return Math.abs(smallestRight - n);
+            }
+            return Math.max(highestLeft, Math.abs(smallestRight - n));
+        }
         }
 
     }
