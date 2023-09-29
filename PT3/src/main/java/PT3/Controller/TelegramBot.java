81527500219f396a1507a256431f258970b9a072
SergeiMish <mishalevskii@yandex.ru>
2023-09-29 21:54:37 +0500

    PT3

--- PT3/src/main/java/PT3/Controller/TelegramBot.java
+++ PT3/src/main/java/PT3/Controller/TelegramBot.java
@@ -16,10 +16,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         var originalMessage = update.getMessage();
         System.out.println(originalMessage.getText());
     }
-    @Override
-    public String getBotUsername() {
-        return null;
-    }
+
 
     @Override
     public String getBotToken() {
