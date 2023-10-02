46ebcbf3d0f9a083548d9b1f6c2c7488411d36ff
SergeiMish <mishalevskii@yandex.ru>
2023-10-02 21:13:02 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int moon = 9, star = 2 + 2 * 3;
-        float sun = star > 10 ? 1 : 3;
-        double jupiter = (sun + moon) - 1.0f;
-        int mars = --
-                moon
-                <= 8 ? 2 : 3;
-        System.out.println(sun + ", " + jupiter + ", " + mars);
+        final int score1 = 8, score2 = 3;
+        char myScore = 7;
+        var goal = switch (myScore) {
+            default ->
+            {if(10>score1) yield "unknown";}
+            case score1 -> "great";
+            case 2, 4, 6 -> "good";
+            case score2, 0 -> "bad";
+            };
+        System.out.println(goal);
     }
+
 }
