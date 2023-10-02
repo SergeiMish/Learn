06bb7953b78e3ba1511cf6f1c284f150c4d28fdc
SergeiMish <mishalevskii@yandex.ru>
2023-10-02 21:15:35 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        final int score1 = 8, score2 = 3;
-        char myScore = 7;
-        var goal = switch (myScore) {
-            default ->
-            {if(10>score1) yield "unknown";}
-            case score1 -> "great";
-            case 2, 4, 6 -> "good";
-            case score2, 0 -> {"bad";}
-            };
-        System.out.println(goal);
-    }
-
+        41: final int score1 = 8, score2 = 3;
+             42: char myScore = 7;
+             43: var goal = switch (myScore) {
+            44: default ->{if(10>score1) yield "unknown";}
+            45: case score1 -> "great";
+            46: case 2, 4, 6 ->"good";
+        47: case score2, 0 -> {"bad";}
+        48: };
+        49: System.out.println(goal);
 }
