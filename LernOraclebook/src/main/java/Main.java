fa9d2d5e54210776ac35760c808814f48e202874
SergeiMish <mishalevskii@yandex.ru>
2023-10-02 21:11:51 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -5,12 +5,9 @@ public class Main {
         var goal = switch (myScore) {
             default ->
             {if(10>score1) yield "unknown";}
-            case score1 ->
-                    "great";
-            case 2, 4, 6 ->
-                    "good";
-            case score2, 0 ->
-            {"bad";}
+            case score1 -> "great";
+            case 2, 4, 6 -> "good";
+            case score2, 0 -> {"bad";}
             };
         System.out.println(goal);
     }
