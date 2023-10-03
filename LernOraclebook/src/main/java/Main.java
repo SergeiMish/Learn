b6fd15ffff8a7be19aab139fdad2dc84c9b507cc
SergeiMish <mishalevskii@yandex.ru>
2023-10-03 21:47:32 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,12 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
-    public static void main(String[] args) {
-         final int score1 = 8, score2 = 3;
-         char myScore = 7;
-         var goal = switch (myScore) {
-         default ->{if(10>score1) yield "unknown";}
-         case score1 -> "great";
-         case 2, 4, 6 ->"good";
-         case score2, 0 -> {"bad";}
-         };
-         System.out.println(goal);
+     public static void main(String[] args) {
+          List<Integer> data = new ArrayList<>();
+          IntStream.range(0, 100).parallel().forEach(s ->
+                  data.add(s));
+          System.out.println(data.size());
+     }
 }
