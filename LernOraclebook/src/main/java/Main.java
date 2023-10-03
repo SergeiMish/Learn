14b8a4fcfe0ea61a0db7f3f15ab4fe0c3bb56363
SergeiMish <mishalevskii@yandex.ru>
2023-10-03 21:52:53 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
      public static void main(String[] args) {
