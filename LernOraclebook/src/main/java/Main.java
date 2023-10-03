35013b4cd3629d87b1aceefafcb0cc48abe622b0
SergeiMish <mishalevskii@yandex.ru>
2023-10-03 21:49:18 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -5,8 +5,7 @@ import java.util.stream.IntStream;
 public class Main {
      public static void main(String[] args) {
           List<Integer> data = new ArrayList<>();
-          IntStream.range(0, 100).parallel().forEach(s ->
-                  data.add(s));
+          IntStream.range(0, 100).parallel().forEach(data::add);
           System.out.println(data.size());
      }
 }
