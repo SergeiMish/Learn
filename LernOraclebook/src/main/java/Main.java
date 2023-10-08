c02b43048878a42a03b0495f0cf2c22945b67b4b
SergeiMish <mishalevskii@yandex.ru>
2023-10-08 19:28:02 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -13,8 +13,7 @@ public class Main {
 
      public class Cougar implements HasTail {
           public static void main(String[] args) {
-               var puma = new Puma() {
-               };
+               var puma = new Puma() { };
                System.out.println(puma.getTailLength());
           }
           public int getTailLength(int length) {
