f176ae6d9884f576eeda951974c21698c08479ad
SergeiMish <mishalevskii@yandex.ru>
2023-10-08 19:26:09 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -5,11 +5,11 @@ interface HasTail { private int getTailLength(); }
 abstract class Puma implements HasTail {
 String getTailLength() { return "4"; }
 }
-5: public class Cougar implements HasTail {
-6: public static void main(String[] args) {
-               7: var puma = new Puma() {};
+public class Cougar implements HasTail {
+public static void main(String[] args) {
+               var puma = new Puma() {};
                Assessment Test xlix
-               8: System.out.println(puma.getTailLength());
-               9: }
+               System.out.println(puma.getTailLength());
+               }
 10: public int getTailLength(int length) { return 2; }
 11: }
