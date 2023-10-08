b40cc691b5b9fa595dc85566c36e713cc0a01fff
SergeiMish <mishalevskii@yandex.ru>
2023-10-08 20:00:22 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,24 +1,7 @@
-import java.util.Arrays;
+
 
 public class Main {
-     interface HasTail {
-          private int getTailLength();
-     }
+     public static void main(String[] args) {
 
-
-     abstract class Puma implements HasTail {
-          String getTailLength() {
-               return "4";
-          }
-     }
-
-     public class Cougar implements HasTail {
-          public static void main(String[] args) {
-               var puma = new Puma() { };
-               System.out.println(puma.getTailLength());
-          }
-          public int getTailLength(int length) {
-               return 2;
-          }
      }
 }
