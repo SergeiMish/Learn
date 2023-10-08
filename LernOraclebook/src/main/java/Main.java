8e511260566972f803496b976f9b846ee0538dff
SergeiMish <mishalevskii@yandex.ru>
2023-10-08 19:22:03 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -5,5 +5,9 @@ public class Main {
  public static void addToInt(int x, int amountToAdd) {
                 x = x + amountToAdd;
                 }
-}
+} public static void main(String[] args) {
+          6: var a = 15;
+          7: var b = 10;
+          8: MathFunctions.addToInt(a, b);
+          9: System.out.println(a); } }
 
