b5db2e48bd4d00e26f7dc7fe42cb725ab427e44c
SergeiMish <mishalevskii@yandex.ru>
2023-10-08 19:21:06 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,26 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
 
 public class Main {
-     public static void main(String[] args) {
-          Predicate<String> empty = String::isEmpty;
-          Predicate<String> notEmpty = empty.negate();
 
-          var result = Stream.generate(() ->
-                  "")
-          .filter(notEmpty)
-          .collect(Collectors.groupingBy(k -> k))
-          .entrySet()
-          .stream()
-          .map(Map.Entry::getValue)
-          .flatMap(Collection::stream)
-          .collect(Collectors.partitioningBy(notEmpty));
-          System.out.println(result);
-     }
 }
