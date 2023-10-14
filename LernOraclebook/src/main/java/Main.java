4a39dc7685034cf64c5c7dbf0c210ba79a55362b
SergeiMish <mishalevskii@yandex.ru>
2023-10-14 16:19:56 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,6 +1,8 @@
 
  public class Main{
      public static void main(String[] args) {
-
+         String reference = "hello";
+         int len = reference.length();
+         System.out.println(len);
      }
  }
