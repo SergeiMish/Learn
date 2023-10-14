27282a5fdd918a5e233d389ad190ea7822b5a1c8
SergeiMish <mishalevskii@yandex.ru>
2023-10-14 12:36:17 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,7 +1,7 @@
-import java.util.Date;
+
 public class Main {
-     public class Conflicts {
-          Date date;
-// some more code
+     public static void main(String[] args) {
+          { System.out.println("Feathers"); }
+          }
+{ System.out.println("Snowy"); }
      }
-}
