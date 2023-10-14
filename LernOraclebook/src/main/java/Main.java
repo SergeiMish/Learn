085b182089f17aecc8cddd53311884913b602626
SergeiMish <mishalevskii@yandex.ru>
2023-10-14 12:32:51 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,12 +1,7 @@
 import java.util.Date;
 public class Main {
-     public class Swan {
-          int numberEggs; // instance variable
-
-          public void main(String[] args) {
-               Swan mother = new Swan();
-               mother.numberEggs = 1; // set variable
-               System.out.println(mother.numberEggs); // read variable
-          }
+     public class Conflicts {
+          Date date;
+// some more code
      }
 }
