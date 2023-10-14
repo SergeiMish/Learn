ef972cd33d0342738c570a36271e2461ee8ad55d
SergeiMish <mishalevskii@yandex.ru>
2023-10-14 16:38:08 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,9 +1,12 @@
 
  public class Main{
      public static void main(String[] args) {
-         final int x = 10;
-         int y = 0;
+         System.out.println(math());
+     }
+     public static int math(){
+         int x = 10;
+         int y = 5;
          x = y + 10;
-
+         return x;
      }
  }
