6c2be161fd2ecbd9bc8e01dd3bab3bbd871cd677
SergeiMish <mishalevskii@yandex.ru>
2023-10-17 22:17:05 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,5 +1,5 @@
 
- public class Main{
+ public class Main {
      public static void main(String[] args) {
          class Zoo {
              public void whatTypeAmI() {
@@ -8,4 +8,5 @@
              }
          }
      }
+ }
 
