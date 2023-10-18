c4bbd2ea7ca3b673ae5d0b4359f2e58aeba5758f
SergeiMish <mishalevskii@yandex.ru>
2023-10-18 22:14:14 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,7 +2,9 @@
  public class Main {
      public static void main(String[] args) {
          class SMT{
-
+             public void doesThisCompile ( boolean check){
+                 var question;
+                 question = 1;
                  var answer;
                  if (check) {
                      answer = 2;
