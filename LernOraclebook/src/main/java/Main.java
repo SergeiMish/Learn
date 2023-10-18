3976ebd0441f52307cf14df04d90f127d852583a
SergeiMish <mishalevskii@yandex.ru>
2023-10-18 22:13:51 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -5,13 +5,7 @@
              public void doesThisCompile ( boolean check){
                  var question;
                  question = 1;
-                 var answer;
-                 if (check) {
-                     answer = 2;
-                 } else {
-                     answer = 3;
-                 }
-                 System.out.println(answer);
+
              }
          }
 
