4bd277436aada88023630f3f518ffc48d833c672
SergeiMish <mishalevskii@yandex.ru>
2023-10-19 22:05:14 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -6,7 +6,12 @@
                  var question;
                  question = 1;
                  var answer;
-
+                 if (check) {
+                     answer = 2;
+                 } else {
+                     answer = 3;
+                 }
+                 System.out.println(answer);
              }
          }
 
