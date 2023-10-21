dd4ca6dae67f8d02fa18c010bcd4788c4f03d474
SergeiMish <mishalevskii@yandex.ru>
2023-10-21 09:41:58 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -6,11 +6,7 @@
                  var question;
                  question = 1;
                  var answer;
-                 if (check) {
-                     answer = 2;
-                 } else {
-                     answer = 3;
-                 }
+
                  System.out.println(answer);
              }
          }
