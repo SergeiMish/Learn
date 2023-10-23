6308d1e50c3b0187dd62970e02f4814906cb40d9
SergeiMish <mishalevskii@yandex.ru>
2023-10-23 22:17:08 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,12 +2,15 @@
  public class Main {
      public static void main(String[] args) {
          class eatIfHung {
-         public void eatIfHungry ( boolean hungry){
-             if (hungry) {
-                 int bitesOfCheese = 1;
-                 {
-                     var teenyBit = true;
-                     System.out.println(bitesOfCheese);
+             public void eatIfHungry(boolean hungry) {
+                 if (hungry) {
+                     int bitesOfCheese = 1;
+                     {
+                         var teenyBit = true;
+                         System.out.println(bitesOfCheese);
+                     }
                  }
              }
          }
+     }
+ }
