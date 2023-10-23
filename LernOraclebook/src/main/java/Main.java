fb2c22b4098892bd64049244737baf4b50d1a4a0
SergeiMish <mishalevskii@yandex.ru>
2023-10-23 22:14:42 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,18 +1,9 @@
 
  public class Main {
      public static void main(String[] args) {
-         class SMT{
-             public void doesThisCompile ( boolean check){
-                 var question;
-                 question = 1;
-                 var answer;
-                 if (check) {
-                     answer = 2;
-                 } else {
-                     answer = 3;
-                 }
-                 System.out.println(answer);
-             }
-         }
-     }
- }
+         16: public void eatIfHungry(boolean hungry) {
+             17: if (hungry) {
+                 18: int bitesOfCheese = 1;
+                 19: {
+                     20: var teenyBit = true;
+                     21: System.out.println(bitesOfCheese);
