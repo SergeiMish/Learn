ba9ebf82efb89112b0bf189c3ef5f47c9253eee8
SergeiMish <mishalevskii@yandex.ru>
2023-10-23 22:15:01 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,7 +3,7 @@
      public static void main(String[] args) {
          public void eatIfHungry(boolean hungry) {
              if (hungry) {
-                 18: int bitesOfCheese = 1;
+                 int bitesOfCheese = 1;
                  19: {
                      20: var teenyBit = true;
                      21: System.out.println(bitesOfCheese);
