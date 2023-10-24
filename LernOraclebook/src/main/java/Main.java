c77dc47cd8c8f7e278bc8cad96e08073dc565bac
SergeiMish <mishalevskii@yandex.ru>
2023-10-24 20:14:12 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -4,7 +4,7 @@
          12: int roomInBelly = 5;
          13: if (hungry) {
              14: var timeToEat = true;
-             15: while (amountOfFood > 0) {
+              while (amountOfFood > 0) {
                  int amountEaten = 2;
                   roomInBelly = roomInBelly -amountEaten;
                   amountOfFood = amountOfFood -amountEaten;
