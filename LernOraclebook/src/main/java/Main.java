6065a48e65d6f63ed39f7681030f6cb5b01353d2
SergeiMish <mishalevskii@yandex.ru>
2023-10-24 20:16:30 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,6 +1,9 @@
 
  public class Main {
-     public static void eatMore(boolean hungry, int amountOfFood) {
+     public static void main(String[] args) {
+
+     }
+     public void eatMore(boolean hungry, int amountOfFood) {
          int roomInBelly = 5;
          if (hungry) {
              var timeToEat = true;
