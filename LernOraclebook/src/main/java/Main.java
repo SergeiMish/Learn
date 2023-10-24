27c3aa776a2a89638f1b1ff46b24c205ea460872
SergeiMish <mishalevskii@yandex.ru>
2023-10-24 20:13:49 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -8,7 +8,7 @@
                  16: int amountEaten = 2;
                  17: roomInBelly = roomInBelly -amountEaten;
                  18: amountOfFood = amountOfFood -amountEaten;
-                 19: }
+                 }
              }
           System.out.println(amountOfFood);
          }
