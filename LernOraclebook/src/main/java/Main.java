2bdb43085ee19c3b6de5155fe31f088f605cbb20
SergeiMish <mishalevskii@yandex.ru>
2023-10-29 22:04:45 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -9,6 +9,10 @@
              var timeToEat = true;
              while (amountOfFood > 0) {
                  int amountEaten = 2;
-
+                 roomInBelly = roomInBelly - amountEaten;
+                 amountOfFood = amountOfFood - amountEaten;
+             }
+         }
+         System.out.println(amountOfFood);
      }
  }
