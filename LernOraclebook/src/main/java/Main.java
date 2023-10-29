2844c165eb2e7539352c695e83dbb240b1c1af40
SergeiMish <mishalevskii@yandex.ru>
2023-10-29 22:04:20 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,9 +3,6 @@
      public static void main(String[] args) {
 
      }
-     public void eatMore(boolean hungry, int amountOfFood) {
-         int roomInBelly = 5;
-         if (hungry) {
 
      }
  }
