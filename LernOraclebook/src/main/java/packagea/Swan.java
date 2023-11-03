291d21f15ab6e0587a3244b82caa7551626eab1e
SergeiMish <mishalevskii@yandex.ru>
2023-11-03 23:06:30 +0500

    LearnOracle

--- LernOraclebook/src/main/java/packagea/Swan.java
+++ LernOraclebook/src/main/java/packagea/Swan.java
@@ -3,5 +3,3 @@ package packagea;
 public class Swan {
     int numberEggs; // instance variable
     public static void main(String[] args) {
-        Swan mother = new Swan();
-        mother.numberEggs = 1; // set variable
