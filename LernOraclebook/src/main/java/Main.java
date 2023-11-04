34b78d2a7bb17e67dc1a5590faf047d5422263d3
SergeiMish <mishalevskii@yandex.ru>
2023-11-04 11:00:54 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,11 +3,10 @@
      public static void main(String[] args) {
 
      }
-     public void eatMore(boolean hungry, int amountOfFood) {
-         int roomInBelly = 5;
-         if (hungry) {
-             var timeToEat = true;
-             while (amountOfFood > 0) {
-                 int amountEaten = 2;
-                 roomInBelly = roomInBelly - amountEaten;
-                 amountOfFood = amountOfFood - amountEaten;
+
+     public class Bunny {
+         public void main(String[] x) {
+             Bunny bun = new Bunny();
+         }
+     }
+ }
