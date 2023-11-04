3ddc9bc35980750afb2a4b70c81641d9e77918fd
SergeiMish <mishalevskii@yandex.ru>
2023-11-04 11:07:36 +0500

    LearnOracle

--- LernOraclebook/src/main/java/packagea/Swan.java
+++ LernOraclebook/src/main/java/packagea/Swan.java
@@ -8,7 +8,7 @@ private void roar(Bear b) {
          }
  public static void main(String[] args) {
         Bear brownBear = new Bear();
-        10: Bear polarBear = new Bear();
+         Bear polarBear = new Bear();
         11: brownBear.roar(polarBear);
         12: polarBear = null;
         13: brownBear = null;
