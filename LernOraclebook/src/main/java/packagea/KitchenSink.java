725b54d5892042473078517073e8cf2b27d88f23
SergeiMish <mishalevskii@yandex.ru>
2023-11-07 22:22:25 +0500

    LearnOracle

--- LernOraclebook/src/main/java/packagea/KitchenSink.java
+++ LernOraclebook/src/main/java/packagea/KitchenSink.java
@@ -4,9 +4,7 @@ public class KitchenSink {
     private int numForks;
     public static void main(String[] args) {
         int numKnives;
-        System.out.print("""
-"# forks = " + numForks +
-" # knives = " + numKnives +
-# cups = 0""");
+        System.out.print(""" 
+                # forks = " + numForks + " # knives = " + numKnives + # cups = 0""");
     }
 }
