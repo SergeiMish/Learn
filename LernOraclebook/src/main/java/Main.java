5dde0b8d2d5b9cc5baec4c5b29ac583c02cf498c
SergeiMish <mishalevskii@yandex.ru>
2023-11-07 22:17:37 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -4,9 +4,14 @@
 
      }
 
-     public class Bunny {
-         public void main(String[] x) {
-             Bunny bun = new Bunny();
+     public class KitchenSink {
+         private int numForks;
+         public static void main(String[] args) {
+             int numKnives;
+             System.out.print("""
+"# forks = " + numForks +
+" # knives = " + numKnives +
+# cups = 0""");
          }
      }
  }
