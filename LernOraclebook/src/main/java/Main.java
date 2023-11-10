6bbc3613f1aaf45612a2771c40bc9c52fe282bef
SergeiMish <mishalevskii@yandex.ru>
2023-11-10 23:40:32 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -6,9 +6,6 @@
 
      public class KitchenSink {
          private int numForks;
-         public static void main(String[] args) {
-             int numKnives;
-             System.out.print("""
 
          }
      }
