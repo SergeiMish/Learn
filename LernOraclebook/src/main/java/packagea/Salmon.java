9c83c1fa662c5c662a5ca0d64c0c1fb9e98e69f7
SergeiMish <mishalevskii@yandex.ru>
2023-12-01 16:53:54 +0500

    LearnOracle

--- LernOraclebook/src/main/java/packagea/Salmon.java
+++ LernOraclebook/src/main/java/packagea/Salmon.java
@@ -8,10 +8,5 @@ int count;
  public Salmon() {
          count = 4;
          System.out.print(2+"-");
-         }
-public static void main(String[] args) {
-         System.out.print(7+"-");
-        var s = new Salmon();
-         System.out.print(s.count+"-");
-    }
+ 
 }
