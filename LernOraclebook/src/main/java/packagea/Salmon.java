fb4bb8caef727239e9882beb380b013e5500fda8
SergeiMish <mishalevskii@yandex.ru>
2023-12-07 20:56:17 +0500

    LearnOracle

--- LernOraclebook/src/main/java/packagea/Salmon.java
+++ LernOraclebook/src/main/java/packagea/Salmon.java
@@ -4,7 +4,7 @@ public class River {
 2: int Depth = 1;
 3: float temp = 50.0;
 4: public void flow() {
-        5: for (int i = 0; i < 1; i++) {
+         for (int i = 0; i < 1; i++) {
              int depth = 2;
              depth++;
              temp--
