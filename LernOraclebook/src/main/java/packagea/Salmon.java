e63cc102c286a0d96fb4a1a158fab14754b40cb6
SergeiMish <mishalevskii@yandex.ru>
2023-12-09 01:00:08 +0500

    LearnOracle

--- LernOraclebook/src/main/java/packagea/Salmon.java
+++ LernOraclebook/src/main/java/packagea/Salmon.java
@@ -8,4 +8,9 @@ public class River {
          for (int i = 0; i < 1; i++) {
              int depth = 2;
              depth++;
-     } }
+             temp--
+            ;
+             }
+  g... s) {
+         new River().flow();
+         } }
