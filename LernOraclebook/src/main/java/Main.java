b9d6e10d81a456244f6b94b7ef2ae70d3e13da53
SergeiMish <mishalevskii@yandex.ru>
2024-05-12 10:41:59 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,6 +3,6 @@
      public static void main(String[] args) {
     int cookies = 3;
     double reward = 3 + 2 * --cookies;
-         System.out.println("Звери получат по :" +reward+ " печенек");
+         System.out.println("Звери получат по :c" +reward+ " печенек");
      }
  }
