c777644ec62445f31d08bef55c06997ea043b8fb
SergeiMish <mishalevskii@yandex.ru>
2024-05-12 11:35:16 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,8 +1,11 @@
 
  public class Main {
      public static void main(String[] args) {
-    int cookies = 3;
-    double reward = 3 + 2 * --cookies;
-         System.out.println("Звери получат по :c" +reward+ " печенек");
+         double x = 7.5;
+         double y = 4.5;
+         y = y - 2;
+         x = y;
+         double result = (y - 0.5) * 2 + 3.5 + x * 2;
+         System.out.println(result);
      }
  }
