caa351b0b3f434d97fbdb80d2fba8b0de47add19
SergeiMish <mishalevskii@yandex.ru>
2024-05-13 18:29:52 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,10 +1,15 @@
 
  public class Main {
      public static void main(String[] args) {
-         String pixelCat = "Пиксель";
-         String badCat = "Плохой кот";
-         boolean isBadCat = pixelCat.equals(badCat);
+         String weather = "Дождь";
 
-         System.out.println(isBadCat);
+         System.out.println("Собираюсь на прогулку");
+         if (weather.equals("Дождь")) {weather.equals("Дождь");
+             System.out.println("Беру с собой зонт");}
+        else{
+            System.out.println("Иду на легке");
+         }
+         System.out.println("Люблю гулять!");
      }
  }
+
