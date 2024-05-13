6f95734af9c6f2e7031d6e0d4aacf6aca0bbd2c3
SergeiMish <mishalevskii@yandex.ru>
2024-05-13 11:34:10 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,9 +3,10 @@
      public static void main(String[] args) {
          int x = 5;
          int y = 7;
-        y = 15 - x;
-         x = x * y;
-         int result = y - x;
+//        y = 15 - x;
+//         x = x * y;
+         int result;
+         result = y - x;
          result = x * 2;
          System.out.println(result + "0");
      }
