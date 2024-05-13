c840f5912218cef0338919157d3d29cdbeb71606
SergeiMish <mishalevskii@yandex.ru>
2024-05-13 18:12:11 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,18 +1,10 @@
 
  public class Main {
      public static void main(String[] args) {
-         int answer1 = 3;
-         int answer2 = 4; // Ответ на 2 вопрос
-         int answer3 = 1; // Ответ на 3 вопрос
-         boolean questions1 = answer1 == 2;
-         boolean questions2 = answer2 != 4;
-         boolean questions3 = answer3 == 1;
-         System.out.println("Результат проверки вопроса 1:");
-         System.out.println(questions1);
-         System.out.println("Результат проверки вопроса 2:");
-         System.out.println(questions2);
-         System.out.println("Результат проверки вопроса 3:");
-         System.out.println(questions3);
-         // Повторите для вопросов 2 и 3
+         String pixelCat = "Пиксель";
+         String badCat = "Плохой кот";
+         boolean isBadCat = pixelCat.equals(badCat);
+
+         System.out.println(isBadCat);
      }
  }
