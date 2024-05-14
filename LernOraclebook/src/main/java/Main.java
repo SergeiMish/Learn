9a1c36a5d929b499af261e88e92bfe6035c31391
SergeiMish <mishalevskii@yandex.ru>
2024-05-14 19:31:16 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,39 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++){
-            System.out.println("Круг " + i);
-            if (i == 1){
-                System.out.println("Отжимаемся");
-            }
-            else if (i == 2){
-                System.out.println("Качаем пресс");
-            }
-            else if (i == 3){
-                System.out.println("Отжимаемся");
-            }
-            else if (i == 4){
-                System.out.println("Качаем пресс");
-            }
-            else if (i == 5){
-                System.out.println("Отжимаемся");
-            }
-            System.out.println("Бегаем в колесе");
+        double interestRate = 10;
+
+        System.out.println("Введите начальную сумму в выбранной валюте:");
+        double amount = NumberReader.getDouble();
+
+        // посчитайте начальную сумму и сохраните в переменную amount
+
+        System.out.println("Введите количество лет для расчёта:");
+        int yers = NumberReader.getInteger();
+        // посчитайте количество лет и сохраните в переменную years
+
+        // напишите условие выполнения цикла
+        for (int i = 1; i <= yers; i++) {
+            amount = amount + amount * (interestRate / 100);
+            // формула для суммы за один год на процентную ставку
         }
 
-        // в тренировке должно выполниться 5 кругов — напишите здесь цикл
-
-
-        // выполнение каждого круга зависит от условия — проверьте, является ли круг чётным
-        // если да, то нужно качать пресс
-
-
-        // иначе нужно отжаться
-
-
-        // независимо от номера круга бегаем в колесе
-
-
-        System.out.println("Отлично позанимались сегодня! Вы молодец!");
+        // выведите на экран сообщение с итоговой суммой
+        System.out.println("К окончанию срока сумма составит: " + amount + " " + "RUB");
     }
 }
