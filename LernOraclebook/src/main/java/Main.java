c85f7bd0fd37cc28cb010874350a70597d98dc6f
SergeiMish <mishalevskii@yandex.ru>
2024-05-14 09:25:49 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,18 +3,13 @@
      public static void main(String[] args) {
          String position = "seller";
 
-         if (position.equals("admin"))
-         {
+         if (position.equals("admin")) {
              System.out.println("Доступ разрешен на все ресурсы.");
-         }
-         else if (position.equals("seller"))
-         {
+         } else if (position.equals("seller")) {
              System.out.println("Доступ разрешен только на просмотр объявлений.");
          }
-         else {
-                 System.out.println("Проверка окончена.");
-             }
-         }
+         System.out.println("Проверка окончена.");
      }
+         }
 
 
