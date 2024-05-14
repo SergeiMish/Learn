ea1a8a3346c438906c75a8589aa9a5a2861135d6
SergeiMish <mishalevskii@yandex.ru>
2024-05-14 18:41:20 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,47 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double rateUSD = 95.0;
-        double rateEUR = 99.58;
-        double rateCNY = 13.05;
-
-
-        System.out.println("Введите сумму рублей для конвертации:");
-        double roubles = NumberReader.getDouble();
-
-        System.out.println("Введите номер валюты, в какую перевести рубли:");
-
-        System.out.println("1 – доллары;");
-        System.out.println("2 – евро;");
-        System.out.println("3 – юани;");
-        int command = NumberReader.getInteger();
-
-        if (command == 1) {
-            double USD = roubles / rateUSD;
-            if (USD <= 0) {
-                System.out.println("Ошибка: некорректные значения.");
-            }
-            else{
-                System.out.println("Было введено " + roubles + ", в долларах это " + USD);
-            }
-        } else if (command == 2) {
-            double EUR = roubles / rateEUR;
-            if (EUR <= 0) {
-                System.out.println("Ошибка: некорректные значения.");
-            }
-            else{
-                System.out.println("Было введено " + roubles + ", в долларах это " + EUR);
-            }
-
-        } else if (command == 3) {
-            double CNY = roubles / rateCNY;
-            if (CNY <= 0) {
-                System.out.println("Ошибка: некорректные значения.");
-            }
-            else{
-                System.out.println("Было введено " + roubles + ", в долларах это " + CNY);
-            }
-        } else System.out.println("Такой команды нет");
-        System.out.println("Работа с программой завершена.");
+        // вместо многоточий добавьте нужные значения
+        for (int i = 1; i <= 17; i++) {
+            System.out.println(i + "-я овечка перепрыгнула через забор."); // это тело цикла
+        }
+        // цикл завершён, далее выполнится эта строка:
+        System.out.println("Хомяки сыты, овцы целы. Можно отдохнуть!");
     }
 }
