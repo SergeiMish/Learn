74877a94e0e225093fef629bb7d1a1294417683b
SergeiMish <mishalevskii@yandex.ru>
2024-05-14 18:54:16 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        // вместо многоточий добавьте нужные значения
-        for (int i = 1; i <= 17; i++) {
-            System.out.println(i + "-я овечка перепрыгнула через забор."); // это тело цикла
+        int secondsBeforeStart = 70; // Секунды до старта, цикл должен начинаться с этого значения
+        for (int i = 7; i == secondsBeforeStart; i--){
+            System.out.println("До старта SpaceY осталось" + i);
         }
-        // цикл завершён, далее выполнится эта строка:
-        System.out.println("Хомяки сыты, овцы целы. Можно отдохнуть!");
+
+        System.out.println("Поехали! Узнаем, есть ли жизнь на Марсе!");
     }
 }
