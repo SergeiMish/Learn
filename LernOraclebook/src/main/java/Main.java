e2da31fde2d15cf34f2b86a713cb32b776d390b9
SergeiMish <mishalevskii@yandex.ru>
2024-05-14 09:55:54 +0500

    LearnOracle

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -1,15 +1,21 @@
 
  public class Main {
      public static void main(String[] args) {
-         String position = "seller";
+         String season = "spring";
+         int totalPeople = 5;
+         int dancers = 2;
+         int gamePlayers = 3;
+         int temperature = 20;
 
-         if (position.equals("admin")) {
-             System.out.println("Доступ разрешен на все ресурсы.");
-         } else if (position.equals("seller")) {
-             System.out.println("Доступ разрешен только на просмотр объявлений.");
+         if (season.equals("summer")) {
+             System.out.println("Пойдём гулять"); // Если это так – выведите сообщение с предложением погулять
+         } else if (temperature>=15) { // Проверьте, что на улице тепло
+             System.out.println("Пойдём в кафе на веранду!");   // Выведите соответствующее сообщение
+         } else if (dancers>0) { // Проверьте, есть ли желающие потанцевать
+             System.out.println("Пойдём в бар!"); // Выведите соответствующее сообщение
+         } else if (gamePlayers>=totalPeople) { // Проверьте, все ли хотят играть в настольные игры
+             System.out.println("Пойдём к кому-то в гости и поиграем в настольные игры!");  // Выведите соответствующее сообщение
          }
-         System.out.println("Проверка окончена.");
      }
-         }
-
+ }
 
