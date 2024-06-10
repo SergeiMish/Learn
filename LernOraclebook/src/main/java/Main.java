ef3097f151864e343c6a98e3f069412170041744
SergeiMish <mishalevskii@yandex.ru>
2024-06-10 19:23:05 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Круг " + i);
+            if (i % 2 == 0) {
+                for (int j = 1; j <= i; j++)
+                    System.out.println("  Приседаем " + j);
+            } else if (i % 3 == 0) {
+                for (int k = 1; k <= 3; k++) {
 
-        int flatsNumber = 5; // количество квартир на этаже
-        int floorsNumber = 10; // количество этажей
-
-        for(int i = 1; i <= floorsNumber; i++) {
-            for(int j = 1; j <=flatsNumber; j++) {
-                int currentFlat = (flatsNumber - 1) * i + j; // заготовка формулы для учёта квартир
-                System.out.println("Этаж " + i + " квартира " + j + " — доставлено.");
+                    System.out.println("  Отжимаемся " + k);
+                }
             }
+            for (int l = 1; l <= 7; l++) {
+                System.out.println("  Качаем пресс " + l);
+            }
+            System.out.println("Хороший темп, так держать!");
+
+            System.out.println("Отлично позанимались сегодня! Вы - молодец!");
         }
     }
 }
