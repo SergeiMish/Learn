ff059bc2cc58042d13220af4315fbe844d106f74
SergeiMish <mishalevskii@yandex.ru>
2024-06-10 14:13:19 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double price = 256;
-        int totalMonths = 12;
+        Scanner scanner = new Scanner(System.in); // создайте переменную Scanner
 
-        double totalCost = 0;
-        Scanner scanner = new Scanner(System.in);
-        double mea = scanner.nextDouble();
-        // опишите условие цикла.
-        // в качестве переменной итерирования можно использовать переменную month
-        for (int month = 1; month <= totalMonths; month++) {
-            if (month % 3 == 0){
-                price = price / 2;
-            }
-            totalCost = totalCost +price;
-            // Подсчитайте сумму с учётом изменения цены каждый третий месяц
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble(); // объявите переменную moneyBeforeSalary и используйте нужный метод класса Scanner
+
+        // сравнивайте значение  переменной moneyBeforeSalary со значениями в условии, чтобы получить необходимый результат
+        if (moneyBeforeSalary < 3000) {
+            System.out.println("Сегодня лучше поесть дома.");
+        } else if (moneyBeforeSalary >= 3000 && moneyBeforeSalary < 10000) {
+            System.out.println("Можно заказать пиццу!");
+        } else if (moneyBeforeSalary >= 10000 && moneyBeforeSalary < 30000) {
+            System.out.println("Неплохо! Сегодня можно поужинать в кафе.");
+        } else if (moneyBeforeSalary >= 30000) {
+            System.out.println("Отлично! Можно сходить в ресторан.");  // остальные варианты
         }
-
-        System.out.println("Общая стоимость за " + totalMonths + " месяцев: " + totalCost + " руб.");
     }
-    }
+}
