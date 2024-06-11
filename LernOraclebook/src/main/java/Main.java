4ae226882605d7cd2444ea0dc968d40a350172c9
SergeiMish <mishalevskii@yandex.ru>
2024-06-11 08:21:22 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -2,23 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Круг " + i);
-            if (i % 2 == 0) {
-                for (int j = 1; j <= i; j++)
-                    System.out.println("  Приседаем " + j);
-            } else if (i % 3 == 0) {
-                for (int k = 1; k <= 3; k++) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас сейчас: ");
+        double balance = scanner.nextDouble(); // ваши сбережения
+        int years = 0;
 
-                    System.out.println("  Отжимаемся " + k);
-                }
-            }
-            for (int l = 1; l <= 7; l++) {
-                System.out.println("  Качаем пресс " + l);
-            }
-            System.out.println("Хороший темп, так держать!");
-
-            System.out.println("Отлично позанимались сегодня! Вы - молодец!");
+        while (balance <= 1000000) {
+            balance = balance + balance * 0.05;
+            years = years + 1;
         }
+
+        System.out.println("Через " + years + " лет у вас будет миллион!");
     }
 }
