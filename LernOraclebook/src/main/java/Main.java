4853b76f67c5dc71ff2a7629e7f04c2788beb5fa
SergeiMish <mishalevskii@yandex.ru>
2024-06-11 11:24:11 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -15,16 +15,17 @@ public class Main {
 
             System.out.println("Траты за неделю:");
             // выведите с помощью цикла все траты за неделю в виде: "День ... . Потрачено рублей: ..."
-            if (i <= expenses.length) {
-                System.out.println("День " + expenses[i + 1] + " . Портачено рублей " + i);
+            for (int j = 0; j < expenses.length; j++) {
+                System.out.println("День " + (j+1) + " . Портачено рублей " + expenses[i]);
             }
 
                 int sum = 0;
                 // посчитайте и выведите сумму трат за неделю — используйте цикл и здесь
+            if (i == expenses[6]) {
                 sum = sum + expenses[i];
 
                 System.out.println("Траты в рублях за неделю: " + sum);
-
+            }
         }
     }
 }
