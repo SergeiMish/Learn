37eb78c5c2be5c208c09797b590b976dc63034d7
SergeiMish <mishalevskii@yandex.ru>
2024-06-11 12:08:15 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // объявите пустой массив трат за неделю (7 дней)
         int[] expenses = new int[7];
 
         Random random = new Random(); // Генерирует случайное число
@@ -11,21 +10,19 @@ public class Main {
         // допишите условие цикла for, чтобы заполнить массив случайными тратами
         for (int i = 0; i < expenses.length; i++) {
             expenses[i] = random.nextInt(1000);
-
-
-            System.out.println("Траты за неделю:");
-            // выведите с помощью цикла все траты за неделю в виде: "День ... . Потрачено рублей: ..."
-            for (int j = 0; j < expenses.length; j++) {
-                System.out.println("День " + (j+1) + " . Портачено рублей " + expenses[i]);
-            }
-
-                int sum = 0;
-                // посчитайте и выведите сумму трат за неделю — используйте цикл и здесь
-            if (i == expenses[6]) {
-                sum = sum + expenses[i];
-
-                System.out.println("Траты в рублях за неделю: " + sum);
-            }
         }
+
+        System.out.println("Траты за неделю:");
+        // выведите с помощью цикла все траты за неделю в виде: "День ... . Потрачено рублей: ..."
+        for (int i = 0; i < expenses.length; i++){
+            System.out.println("День " + (i+1) + " Потрачено рублей: " + expenses[i]);
+        }
+
+        int sum = 0;
+        // посчитайте и выведите сумму трат за неделю — используйте цикл и здесь
+        for (int i = 0; i < expenses.length; i++){
+            sum = sum + expenses[i];
+        }
+        System.out.println("Траты в рублях за неделю: " + sum);
     }
 }
