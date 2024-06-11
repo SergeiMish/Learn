cd7922d303004633cc5b763b33b205bd2a5c5ee7
SergeiMish <mishalevskii@yandex.ru>
2024-06-11 08:30:14 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
         // не создавайте объект класса Random
         int randomInt = new Random().nextInt(1000); // генерирует новое число от 0 до 1000
@@ -13,19 +14,18 @@ public class Main {
         System.out.println("Ваш ход:");
 
         // запускаем цикл игры
-
+        while (userInput == randomInt) {
         userInput = scanner.nextInt(); // в этой переменной должен сохраняться ввод пользователя
 
-        if (userInput < randomInt) { // условие проверяется в цикле
-            System.out.println("Меньше");
-        }
-        else if (userInput > randomInt){
-            // Второе условие
-            System.out.println("Больше");
-        }
-        else {
-            // печатаем, когда число угадано
-            System.out.println("Вы великолепны! Именно это я загадал.");
+            if (userInput < randomInt) { // условие проверяется в цикле
+                System.out.println("Меньше");
+            } else if (userInput > randomInt) {
+                // Второе условие
+                System.out.println("Больше");
+            } else {
+                // печатаем, когда число угадано
+                System.out.println("Вы великолепны! Именно это я загадал.");
+            }
         }
     }
 }
