b4031d7c81f7b378507356cf5f92f6bc0212194f
SergeiMish <mishalevskii@yandex.ru>
2024-06-12 10:03:27 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("На сколько кусков порезана пицца?");
-        int slicesInPizza = scanner.nextInt();
-        System.out.println("Сколько кусков съедает один участник встречи?");
-        int slicesToPerson = scanner.nextInt();
-        int numOfPizzas = 2;
-        int totalSlices = numOfPizzas * slicesInPizza;
-        int maximumPeopleToMeeting = totalSlices / slicesToPerson;
-        int leftSlices = totalSlices % maximumPeopleToMeeting;
-        System.out.println("Максимальное число участников идеальной встречи: " + maximumPeopleToMeeting);
-        System.out.println("Останется кусков пиццы: " + leftSlices);
-    }
+        double roubles = 5000; // сумма на счету
+        int yearRate = 4; // годовая процентная ставка
+        int numOfMonth = 36; // количество месяцев, на который открыт вклад
+        double monthRefill = 1000; // ежемесячное пополнение
+				double monthMaintenance = 0.1 * roubles; //плата за обслуживание счёта
+				double monthRate = monthRefill % yearRate; // вычислите месячную процентную ставку
 
+        for (int i = 1; i <=12; i++) { // дополните условие цикла
+        double depositInterest = roubles * monthRate / 100; // вычислите доход от процентов
+            roubles += depositInterest; // добавьте доход от процентов
+            roubles += monthRefill; // учтите ежемесячное пополнение
+        }
+
+        System.out.println("Через " + numOfMonth + " месяцев накопится " + roubles + " рублей");
+
+        if (roubles >= 41000) { // определите, удалось ли достичь цели
+            System.out.println("Ура! Можно идти в магазин за новым объективом!");
+        } else {
+            System.out.println("Нужно ещё немного подкопить.");
+        }
+    }
 }
