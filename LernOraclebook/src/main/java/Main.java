f868b7edeb21291fcd9cb378239ae5bd92fcc105
SergeiMish <mishalevskii@yandex.ru>
2024-06-12 09:36:56 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int b = 9;
-        int a = 11;
-        a += 1;
-        a *= 5;
-        b /= 3;
-        int result = a - b;
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На сколько кусков порезана пицца?");
+        int slicesInPizza = scanner.nextInt();
+        System.out.println("Сколько кусков съедает один участник встречи?");
+        int slicesToPerson = scanner.nextInt();
+        int numOfPizzas = 2;
+        int totalSlices = numOfPizzas * slicesInPizza;
+        int maximumPeopleToMeeting = totalSlices / slicesToPerson;
+        int leftSlices = maximumPeopleToMeeting % totalSlices;
+        System.out.println("Максимальное число участников идеальной встречи: " + maximumPeopleToMeeting);
+        System.out.println("Останется кусков пиццы: " + leftSlices);
     }
+
 }
