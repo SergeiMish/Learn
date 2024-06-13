9724f27f682758e1d9f18779f481922de1bc7c71
SergeiMish <mishalevskii@yandex.ru>
2024-06-13 09:55:26 +0500

    LearnYa

--- LernOraclebook/src/main/java/Main.java
+++ LernOraclebook/src/main/java/Main.java
@@ -3,60 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int monthNumber;
-
-        while (true) {
-            System.out.println("Когда планируется путешествие? Введите номер месяца от 1 до 12.");
-            monthNumber = scanner.nextInt();
-
-            if (monthNumber <= 12) { // Инвертируйте условие
-                break;
-            } else {
-                System.out.println("Некорректный номер месяца. Введите ещё раз.");
+        for (int i = 1; i < 21; i++) {
+            if (i % 3 == 0){
+                System.out.println("Яндекс");
             }
-        }
-        String season = "";
-        // Допишите условия ветвления в виде утверждений
-        if (monthNumber  < 3) {
-            season = "Зима";
-        } else if (monthNumber  < 6) {
-            season = "Весна";
-        } else if (monthNumber  < 9) {
-            season = "Лето";
-        } else if (monthNumber  < 12) {
-            season = "Осень";
-        } else {
-            season = "Зима";
-        }
-        if (season.equals("Лето")) {
-            System.out.println("Летом лучше остаться в Москве.");
-            /* Допишите ветвление, которое будет печатать строку:
-            System.out.println("Летом лучше остаться в Москве.");
-              и завершать выполнение программы */
-        }
-        else {
-            System.out.println("Укажите стоимость прямых билетов из Москвы в Париж:");
-            int ticketMoscowParis = scanner.nextInt();
-            System.out.println("Укажите стоимость билетов из Москвы в Париж с пересадкой в Лондоне:");
-            int ticketMoscowLondonParis = scanner.nextInt();
-            System.out.println("У вас есть британская виза?");
-            System.out.println("1 - да, виза есть");
-            System.out.println("0 - визы нет");
-            int britainVisa = scanner.nextInt();
-
-
-            // В этом ветвлении что-то напутано с условиями - исправьте это
-             // Перенесите проверку на "Лето" выше
-                if (ticketMoscowLondonParis < ticketMoscowParis) {
-                    if (britainVisa != 0) { // Упростите это условие
-                        System.out.println("Летим через Лондон!");
-                    } else {
-                        System.out.println("Летим напрямую в Париж!");
-                    }
-                } else {
-                    System.out.println("Летим напрямую в Париж!");
-                }
+            else if (i % 5 == 0){
+                System.out.println("Практикум");
             }
+            else  if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("Яндекс.Практикум");
+            }
+            else System.out.println(i);
         }
     }
+}
