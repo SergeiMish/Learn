8d9330f272fee7270eebd4f3f1f487a0ed74c4c1
SergeiMish <mishalevskii@yandex.ru>
2024-06-17 14:18:54 +0500

    Sprint 1

--- shopping-list/src/Shopping.java
+++ shopping-list/src/Shopping.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Shopping {
     public static void main(String[] args) {
 
         System.out.println("Вас приветствует список покупок!");
 
-        String [] shoppingList = new String[8];
+        String[] shoppingList = new String[8];
 
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Выберите одну из команд:");
+            System.out.println("1. Добавить товар в список");
+            System.out.println("2. Показать список");
+            System.out.println("3. Очистить список");
+            System.out.println("4. Завершить работу");
+
+            int actionNumber = scanner.nextInt();
+        }
     }
 }
