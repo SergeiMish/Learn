e41fe9f70f733bf78499df79dffac14e7c3ac278
SergeiMish <mishalevskii@yandex.ru>
2024-06-17 14:17:18 +0500

    Sprint 1

--- shopping-list/src/Shopping.java
+++ shopping-list/src/Shopping.java
@@ -1,5 +1,10 @@
 public class Shopping {
     public static void main(String[] args) {
 
+        System.out.println("Вас приветствует список покупок!");
+
+        String [] shoppingList = new String[8];
+
+
     }
 }
