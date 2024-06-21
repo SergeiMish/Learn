db272754959e7e621529522aa6ecdd4d2485bcfc
SergeiMish <mishalevskii@yandex.ru>
2024-06-21 16:43:45 +0500

    Sprint 1

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -2,50 +2,25 @@ import java.util.Scanner;
 
 public class Practicum {
 
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        System.out.println("Робот-помощник v2.0.");
-        // вызовите ниже методы в правильном порядке
-    sayHello();
-    sayHelloByTime();
-    welcomeUserByName();
-    printSuccess();
+        // ниже вызовите новый метод
+        String highestGrossingFilm = ...;
+        System.out.println("Самый кассовый фильм: " + highestGrossingFilm);
     }
 
-    public static void welcomeUserByName() {
-        System.out.println("Как вас зовут?");
-        String name = scanner.next();
-        printCity();
-        String city = scanner.next();// допишите чтение города
-        System.out.println("Рад познакомиться, " + name + " из " + city + "!");
-    }
+    public static String findHighestGrossingFilm() {
+        String film1 = "Титаник";
+        int income1 = 2194;
 
-    public static void printSuccess() {
-        System.out.println("У вас уже неплохо получается программировать!");
-    }
-    public static void sayHello(){
-        System.out.println("Привет");
-    }
-    // допишите метод sayHello(), который печатает: Привет!
-    public static void printCity(){
-        System.out.println("Из какого вы города?");
-    }
-    // допишите метод printCity(), который печатает: Из какого вы города?
+        String film2 = "Аватар";
+        int income2 = 2810;
 
-    public static void sayHelloByTime() {
-        System.out.println("Который час?");
-        int currentHour = scanner.nextInt();
-        if (currentHour < 6) {
-            System.out.println("Доброй ночи!");
-        } else if (currentHour > 22) {
-            System.out.println("Доброй ночи!");
-        } else if (currentHour < 12) {
-            System.out.println("Доброе утро!");
-        } else if (currentHour < 18) {
-            System.out.println("Добрый день!");
-        } else {
-            System.out.println("Добрый вечер!");
+        String film3 = "Тёмный рыцарь";
+        int income3 = 1084;
+
+        // допишите реализацию метода ниже
+        if ()
         }
     }
+
 }
