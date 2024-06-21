6ca546b889b681a727c5862c30efabc742db719a
SergeiMish <mishalevskii@yandex.ru>
2024-06-21 13:45:27 +0500

    Sprint 1

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -5,42 +5,43 @@ public class Practicum {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        welcomeUserByName();
-        sayHelloByTime();
-        printSuccess();
+        System.out.println("Робот-помощник v2.0.");
+        // вызовите ниже методы в правильном порядке
+    ...
+    ...
+    ...
     }
 
-    public static void welcomeUserByName() { // объявите метод welcomeUserByName
+    public static void welcomeUserByName() {
         System.out.println("Как вас зовут?");
         String name = scanner.next();
-         // сохраните введённое пользователем имя в переменную name
-        System.out.println("Рад познакомиться, " + name + "!");
+        String city = scanner.next();// допишите чтение города
+        System.out.println("Рад познакомиться, " + name + " из " + city + "!");
     }
 
-     public static void sayHelloByTime(){
-         System.out.println("Который час");
-         int currentHour = scanner.nextInt();
-         if (currentHour < 6){
-             System.out.println("Доброй ночи!");
-         }
-         else if (currentHour < 12){
-             System.out.println("Доброе утро!");
-         }
-         else if (currentHour < 18){
-             System.out.println("Добрый день!");
-         }
-         else if (currentHour < 22){
-             System.out.println("Добрый вечер!");
-         }
-        else System.out.println("Доброй ночи!");
-    }// объявите метод sayHelloByTime
-            // спросите у пользователя "Который час?" и сохраните ответ в переменную currentHour
-            // в зависимости от времени предусмотрите печать приветствий
-    public static void printSuccess(){
+    public static void printSuccess() {
         System.out.println("У вас уже неплохо получается программировать!");
     }
-            // объявите метод printSuccess
-             // метод printSuccess должен печатать строку "У вас уже неплохо получается программировать!"
+    public static void sayHello(){
+        System.out.println("Привет");
+    }
+    ... // допишите метод sayHello(), который печатает: Привет!
 
+            ... // допишите метод printCity(), который печатает: Из какого вы города?
 
+    public static void sayHelloByTime() {
+        System.out.println("Который час?");
+        int currentHour = scanner.nextInt();
+        if (currentHour < 6) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour > 22) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour < 12) {
+            System.out.println("Доброе утро!");
+        } else if (currentHour < 18) {
+            System.out.println("Добрый день!");
+        } else {
+            System.out.println("Добрый вечер!");
+        }
+    }
 }
