66fc6beec4391f02f9bc07943df8d4afb66d3bd0
SergeiMish <mishalevskii@yandex.ru>
2024-06-22 12:01:04 +0500

    Sprint 1

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -3,27 +3,19 @@ import java.util.Scanner;
 public class Practicum {
 
     public static void main(String[] args) {
-        System.out.println("Привет!");
-
-        doCommand();
+        System.out.println("Наибольшее из чисел 3 и 5 = " + findMax());
+        // напишите аналогичный вызов метода findMax для таких пар чисел: 16 и 5, -1 и -7
+        System.out.println("Наибольшее из чисел 16 и 5 = " + findMax());
+        System.out.println("Наибольшее из чисел -1 и -7 = " + findMax());
     }
 
-    public static void doCommand() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Что вы хотите сделать?");
-            System.out.println("1 - Получить совет о покупке евро");
-            System.out.println("2 - Получить совет о покупке долларов");
-            System.out.println("0 - Выход");
-            int command = scanner.nextInt();
-            if (command == 1) {
-                System.out.println("Если евро стоит меньше 105 рублей - покупайте!");
-            } else if (command == 2) {
-                System.out.println("Если доллар стоит меньше 95 рублей - покупайте!");
-            } else if (command == 0) {
-                System.out.println("Выход");
-                break;
-            }
+    public static int findMax() {
+        int a = 5;
+        int b = 3;
+        if (a > b) {
+            return a;
         }
+        return b;
     }
+
 }
