dcd6d2ffea6e554ab19f0714618f0a4423f51785
SergeiMish <mishalevskii@yandex.ru>
2024-06-22 12:21:10 +0500

    Sprint 1

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -2,7 +2,7 @@ public class Practicum {
 
     public static void main(String[] args) {
         double[] expenses = {1772.5, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
-        double maxExpense = findMaxExpense(double); // вызовите метод и присвойте maxExpense значение его результата
+        double maxExpense = findMaxExpense(expenses); // вызовите метод и присвойте maxExpense значение его результата
         System.out.println("Самая большая трата недели " + maxExpense);
     }
 	public static double findMaxExpense(double[] expenses){
