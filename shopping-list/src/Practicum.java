4b6e4777b65f6a4796f61fe49899810aca0468c1
SergeiMish <mishalevskii@yandex.ru>
2024-06-25 18:48:57 +0500

    Sprint 2

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -5,7 +5,6 @@ public class Practicum {
     public static void main(String[] args) {
         double[] expenses = new double[7];
 
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Сколько денег у вас осталось до зарплаты?");
@@ -15,39 +14,32 @@ public class Practicum {
         int daysBeforeSalary = scanner.nextInt();
 
         while (true) {
-            printMenu(); // вынесите печать меню в метод printMenu, здесь останется только его вызов
-
-
+            printMenu();
             int command = scanner.nextInt();
 
             if (command == 1) {
-                convert(scanner, moneyBeforeSalary); // вынесите обработку команды в метод convert, здесь вызовите его
-
+                convert(scanner, moneyBeforeSalary);
             } else if (command == 2) {
-                getAdvice(moneyBeforeSalary, daysBeforeSalary); // вынесите обработку команды в метод getAdvice, здесь вызовите его
-
+                getAdvice(moneyBeforeSalary, daysBeforeSalary);
             } else if (command == 3) {
-                System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
-                int day = scanner.nextInt();
-                System.out.println("Введите размер траты:");
-                double expense = scanner.nextDouble();
-                moneyBeforeSalary = moneyBeforeSalary - expense;
-                expenses[day - 1] = expenses[day - 1] + expense;
-                System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
-                if (moneyBeforeSalary < 1000) {
-                    System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
-                }
+                // перенесите код ниже в метод saveExpense
+                // вызовите метод с помощью такой строки:
+                moneyBeforeSalary = saveExpense(scanner, moneyBeforeSalary, expenses);
+
             } else if (command == 4) {
+                // перенесите обработку команды в метод printAllExpenses, здесь вызовите его
                 for (int i = 0; i < expenses.length; i++) {
                     System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
                 }
             } else if (command == 5) {
+                // перенесите поиск максимальной траты в метод findMaxExpense
                 double maxExpense = 0;
                 for (int i = 0; i < expenses.length; i++) {
                     if (expenses[i] > maxExpense) {
                         maxExpense = expenses[i];
                     }
                 }
+                // печать должна остаться здесь — для получения максимальной траты нужно вызвать метод
                 System.out.println("Самая большая сумма расходов на этой неделе составила " + maxExpense + " руб.");
             } else if (command == 0) {
                 System.out.println("Выход");
@@ -58,7 +50,25 @@ public class Practicum {
         }
     }
 
-    // объявите и реализуйте метод printMenu, который печатает меню
+    public static void saveExpense(Scanner scanner, double moneyBeforeSalary, double[] expenses){
+        System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
+        int day = scanner.nextInt();
+        System.out.println("Введите размер траты:");
+        double expense = scanner.nextDouble();
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+    } /* Добавьте здесь метод saveExpense, который будет сохранять значение расходов пользователя.
+           saveExpense также должен возвращать новое значение остатка средств. */
+
+            ... // объявите и реализуйте здесь метод printAllExpenses
+
+            ... // напишите метод findMaxExpense — он должен возвращать значение максимальной траты
+
+
     public static void printMenu() {
         System.out.println("Что вы хотите сделать? ");
         System.out.println("1 - Конвертировать валюту");
@@ -69,30 +79,10 @@ public class Practicum {
         System.out.println("0 - Выход");
     }
 
-    // объявите и реализуйте метод convert, который конвертирует валюты
-    public static void convert(Scanner scanner, double moneyBeforeSalary) {
-        double rateUSD = 94.8;
-        double rateEUR = 103.8;
-        double rateCNY = 13.1;
-        System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
-        System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - CNY.");
-        int currency = scanner.nextInt();
-        if (currency == 1) {
-            System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
-        } else if (currency == 2) {
-            System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
-        } else if (currency == 3) {
-            System.out.println("Ваши сбережения в юанях: " + moneyBeforeSalary / rateCNY);
-        } else {
-            System.out.println("Введена неизвестная валюта.");
-        }
-    }
-
-    // объявите и реализуйте метод getAdvice, который даёт совет
     public static void getAdvice(double moneyBeforeSalary, int daysBeforeSalary) {
         if (moneyBeforeSalary < 3000) {
             System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-        } else if (moneyBeforeSalary < 10000) {
+        } else if (moneyBeforeSalary < 10000){
             if (daysBeforeSalary < 10) {
                 System.out.println("Можно заказать пиццу!");
             } else {
@@ -113,5 +103,23 @@ public class Practicum {
         }
     }
 
+    public static void convert(Scanner scanner, double moneyBeforeSalary) {
+        double rateUSD = 94.8;
+        double rateEUR = 103.8;
+        double rateCNY = 13.1;
+
+        System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+        System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - CNY.");
+        int currency = scanner.nextInt();
+        if (currency == 1) {
+            System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
+        } else if (currency == 2) {
+            System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
+        } else if (currency == 3) {
+            System.out.println("Ваши сбережения в юанях: " + moneyBeforeSalary / rateCNY);
+        } else {
+            System.out.println("Введена неизвестная валюта.");
+        }
+    }
 
 }
