6b63ab3cbf6f5308dd8a583515f1314c3b46940e
SergeiMish <mishalevskii@yandex.ru>
2024-06-25 17:02:10 +0500

    Sprint 2

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Practicum {
 
     public static void main(String[] args) {
-        System.out.println("Привет, Пиксель!");
-        System.out.println("Привет, Байт!");
+
+
+
+            System.out.println("Привет, Пиксель!");
+            System.out.println("Привет, Байт!");
 
         double[] feedExpensesCat = {100.50, 236.0, 510.6, 150.20, 80.0, 172.0, 135.4};
         double[] feedExpensesHamster = {70.50, 146.0, 710.6, 250.20, 83.0, 19.0, 55.4};
