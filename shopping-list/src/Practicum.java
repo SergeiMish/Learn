fb85ac0dc685d7c7a7407bd841d7877266f0c3a4
SergeiMish <mishalevskii@yandex.ru>
2024-06-26 09:34:41 +0500

    Sprint 2

--- shopping-list/src/Practicum.java
+++ shopping-list/src/Practicum.java
@@ -6,7 +6,7 @@ public class Practicum {
         double[] expenses = new double[7];
 
         Scanner scanner = new Scanner(System.in);
-        Converter converter = new Converter; // создайте объект класса Converter
+        Converter converter = new Converter(); // создайте объект класса Converter
 
         System.out.println("Сколько денег у вас осталось до зарплаты?");
         double moneyBeforeSalary = scanner.nextDouble();
@@ -22,7 +22,7 @@ public class Practicum {
                 System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
                 System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - CNY.");
                 int currency = scanner.nextInt();
-                converter.convert // вызовите метод класса Converter
+                converter.convert(moneyBeforeSalary, daysBeforeSalary); // вызовите метод класса Converter
             } else if (command == 2) {
                 getAdvice(moneyBeforeSalary, daysBeforeSalary);
             } else if (command == 3) {
