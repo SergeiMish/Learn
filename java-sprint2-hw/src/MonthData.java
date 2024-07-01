0d36fc325c2e8e587305d5c81038ae903b5cfdc1
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 21:02:22 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/MonthData.java
+++ java-sprint2-hw/src/MonthData.java
@@ -26,9 +26,9 @@ public class MonthData {
         int currentSeries = 0;
         int finalSeries = 0;
         for (int i = 0; i < days.length; i++) {
-            if (days[i + 1] >= days[i]){
+
                 currentSeries++;
-            }
+
             if (currentSeries >= goalByStepsPerDay){
                 finalSeries = goalByStepsPerDay;
             }
