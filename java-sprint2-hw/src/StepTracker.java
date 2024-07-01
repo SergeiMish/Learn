5d52bc034c925a0ff4b7f88e96a8001913ec1cfd
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 12:25:17 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/StepTracker.java
+++ java-sprint2-hw/src/StepTracker.java
@@ -22,9 +22,18 @@ public class StepTracker {
 
             System.out.println("Введите день от 1 до 30 (включительно)");
             int day = scanner.nextInt();
+            if (day < 1 || day > 30){
+                System.out.println("Деньи должен быть от 1 до 30. Вы ввели " + day);
+                return;
+            }
             // ввод и проверка дня
 
             System.out.println("Введите количество шагов");
+            int steps = scanner.nextInt();
+            if (steps < 0){
+                System.out.println("Количество шагов не может быть отрицательным. Вы ввели" + steps);
+                return;
+            }
             // ввод и проверка количества шагов
 
             // получение соответствующего объекта MonthData из массива
