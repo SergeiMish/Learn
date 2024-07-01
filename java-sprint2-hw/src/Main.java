55ff415c08fe3a90c4491cd46ac7b947d95804ae
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 11:29:23 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/Main.java
+++ java-sprint2-hw/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printMenu();
 
+            int i = scanner.nextInt();
+            // обработка выбранной команды
+        }
     }
+
+    static void printMenu() {
+        // вывод доступных команд
+    }
+
 }
