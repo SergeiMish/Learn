bf0c5ff9744eede5cd96e01a216f6e2c7010286c
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 21:13:27 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/Converter.java
+++ java-sprint2-hw/src/Converter.java
@@ -2,7 +2,7 @@ public class Converter {
     final double STEP_LENGHT = 0.75;
 
     int convertToKm(int steps){
-        double step = 1000.0/(STEP_LENGHT * steps);
+        double step = (STEP_LENGHT * steps)/1000;
         steps = (int) step;
         return steps;
     }
