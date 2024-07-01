aaf4ba1ca703de8ccbb542b8ae59d1526af86dcf
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 12:22:06 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/StepTracker.java
+++ java-sprint2-hw/src/StepTracker.java
@@ -10,21 +10,27 @@ public class StepTracker {
         for (int i = 0; i < monthToData.length; i++) {
             monthToData[i] = new MonthData();
         }
-//
-//        void addNewNumberStepsPerDay(){
-//            System.out.println("Введите номер месяца");
-//            // ввод и проверка номера месяца
-//
-//            System.out.println("Введите день от 1 до 30 (включительно)");
-//            // ввод и проверка дня
-//
-//            System.out.println("Введите количество шагов");
-//            // ввод и проверка количества шагов
-//
-//            // получение соответствующего объекта MonthData из массива
-//            MonthData monthData = ...
-//            // сохранение полученных данных
-//            monthData.days[...] = ...
-//        }
+
+        void addNewNumberStepsPerDay(){
+            System.out.println("Введите номер месяца");
+            int mount = scanner.nextInt();
+            if (mount < 1 || mount > 12){
+                System.out.println("Месяц должен быть от 1 до 12. Вы ввели " + mount);
+                return;
+            }
+            // ввод и проверка номера месяца
+
+            System.out.println("Введите день от 1 до 30 (включительно)");
+            int day = scanner.nextInt();
+            // ввод и проверка дня
+
+            System.out.println("Введите количество шагов");
+            // ввод и проверка количества шагов
+
+            // получение соответствующего объекта MonthData из массива
+            MonthData monthData = ...
+            // сохранение полученных данных
+            monthData.days[...] = ...
+        }
     }
 }
