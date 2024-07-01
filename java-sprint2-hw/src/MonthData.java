6bdbf2464c632e4eb6588baca22bf1971f32beaa
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 19:47:36 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/MonthData.java
+++ java-sprint2-hw/src/MonthData.java
@@ -3,7 +3,7 @@ public class MonthData {
 
     void printDaysAndStepsFromMonth() {
         for (int i = 0; i < days.length; i++) {
-            System.out.println((days[i] + 1) +  " день: " + sumStepsFromMonth());
+            System.out.println((days[i++]) +  " день: " + sumStepsFromMonth());
         }
     }
     int sumStepsFromMonth(){
