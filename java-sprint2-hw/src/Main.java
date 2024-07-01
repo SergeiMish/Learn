8b1fb4eae9977fa6df380eef2974a9a2c252c09e
SergeiMish <mishalevskii@yandex.ru>
2024-07-01 11:39:31 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/Main.java
+++ java-sprint2-hw/src/Main.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            printMenu();
 
-            int i = scanner.nextInt();
-            // обработка выбранной команды
-        }
-    }
+        StepTracker stepTracker = new StepTracker(scanner);
 
-    static void printMenu() {
+//        while (true) {
+//            printMenu();
+//
+//            int i = scanner.nextInt();
+//            // обработка выбранной команды
+//        }
+//    }
+//
+//    static void printMenu() {
         // вывод доступных команд
     }
 
