f39d6898fbbf842102458a8cbdc5308d573b106d
SergeiMish <mishalevskii@yandex.ru>
2024-07-03 11:24:43 +0500

    Sprint 2 (FT)

--- java-sprint2-hw/src/Converter.java
+++ java-sprint2-hw/src/Converter.java
@@ -1,14 +1,17 @@
 public class Converter {
     final double STEP_LENGHT = 0.75;
+    final int KM = 1000;
+    final int KCAL = 1000;
+    final int CAL = 50;
 
     int convertToKm(int steps) {
-        double step = (STEP_LENGHT * steps) / 1000;
+        double step = (STEP_LENGHT * steps) / KM;
         steps = (int) step;
         return steps;
     }
 
     int convertStepsToKilocalories(int steps) {
-        steps = (steps * 50) / 1000;
+        steps = (steps * CAL) / KCAL;
         return steps;
     }
 }
