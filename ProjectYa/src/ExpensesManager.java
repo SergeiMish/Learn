ea52a85c5eb0389b686a23bc2125ebd73c892c0b
SergeiMish <mishalevskii@yandex.ru>
2024-07-09 20:34:07 +0500

    Sprint 3

---
+++ ProjectYa/src/ExpensesManager.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList; // импортируйте ArrayList
+
+public class ExpensesManager {
+    ArrayList<Double> expenses = new ArrayList(); // замените массив списком
+
+    ExpensesManager() {
+        expenses = new ArrayList<>(); // создайте список в конструкторе
+    }
+
+    // номер дня больше не нужен
+    double saveExpense(double moneyBeforeSalary, double expense) {
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        // эту строку нужно убрать
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    void printAllExpenses() {
+        for (int i = 0; i < expenses.size(); i++) {
+            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses.get(i) + " рублей");
+        }
+    }
+
+    double findMaxExpense() {
+        double maxExpense = 0;
+        for (Double exp : expenses) { // используйте сокращённую форму цикла
+            if (exp > maxExpense) {
+                maxExpense = exp;
+            }
+        }
+        return maxExpense;
+    }
+}
--- ProjectYa/src/Practicum.java
+++ ProjectYa/src/Practicum.java
@@ -1,54 +1,50 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Practicum {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> animals = new ArrayList<>();
-        animals.add("1212");
-        animals.add("��������");
-        animals.add("���");
-        animals.add("�������");
-        animals.add("����");
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
+
+        Converter converter = new Converter(94.8,103.8,13.1);
+        DinnerAdvisor dinnerAdvisor = new DinnerAdvisor();
+        ExpensesManager expensesManager = new ExpensesManager();
 
         while (true) {
             printMenu();
             int command = scanner.nextInt();
-            if (command == 1) {
-                System.out.println("������ � �������� ����� ������� " + animals.size() + " ��������:");
-                for (String i : animals){
-                    System.out.println(i);; // ����������� ��������
-                }
-            } else if (command == 2) {
-                System.out.println("����� �������� ������ ��������?");
-                String animal = scanner.next();
-                animals.add(animal);// �������� ��������
-            } else if (command == 3) {
-                System.out.println("����� �������� ����� �������?");
-                String animal = scanner.next();
-                if (!animals.isEmpty()){
-                    animals.remove(animal);
-                }// ������� ��������, ���� ������ �� ����
-            } else if (command == 4) {
-                animals.clear(); // �������� ������
-                System.out.println("��� �������� ���������� � ������ �������. ������ ����.");
-            } else if (command == 5) {
-                System.out.println("����� �������� �� ������ ������� � ��������?");
-                String animal = scanner.next();
 
-                if (animals.contains(animal)) {
-                    // �������� �������
-                    System.out.println(animal + " �� �����! ��������� ����������.");
-                }
-                else {
-                     System.out.println("������ ��������� ������ ��� � ����� ��������.");
-                }
-            } else {
+            if (command == 1) {
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - CNY.");
+                int currency = scanner.nextInt();
+                converter.convert(moneyBeforeSalary, currency);
+            } else if (command == 2) {
+                dinnerAdvisor.getAdvice(moneyBeforeSalary, daysBeforeSalary);
+            } else if (command == 3) {
+                // номер дня больше не нужен. Уберите вопрос и считывание номера дня
+
+                System.out.println("Введите размер траты:");
+                double expense = scanner.nextDouble();
+                expenses.add(expense);
+                // сигнатура метода изменится, учитывайте это
+                moneyBeforeSalary = expensesManager.saveExpense(moneyBeforeSalary, expense);
+            } else if (command == 4) {
+                expensesManager.printAllExpenses();
+            } else if (command == 5) {
+                System.out.println("Самая большая сумма расходов составила " + expensesManager.findMaxExpense() + " руб.");
+            } else if (command == 0) {
+                System.out.println("Выход");
                 break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
             }
         }
     }
+
     public static void printMenu() {
         System.out.println("Что вы хотите сделать? ");
         System.out.println("1 - Конвертировать валюту");
