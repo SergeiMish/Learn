ff70e619ca71cbccf717cc1438f74c55c72dde19
SergeiMish <mishalevskii@yandex.ru>
2024-07-09 09:40:20 +0500

    Sprint 3

--- ProjectYa/src/Practicum.java
+++ ProjectYa/src/Practicum.java
@@ -12,7 +12,7 @@ public class Practicum {
         System.out.println("2 - Нет");
         int command = scanner.nextInt();
         if (command == 1) {
-            user.moneyRUB = null;
+            user.moneyRUB = 0.0;
         }
 
         System.out.println("Хотите открыть счёт в USD?");
@@ -20,7 +20,7 @@ public class Practicum {
         System.out.println("2 - Нет");
         command = scanner.nextInt();
         if (command == 1) {
-            user.moneyUSD = null;
+            user.moneyUSD = 0.0;
         }
 
         System.out.println("Хотите открыть счёт в EUR?");
@@ -28,18 +28,18 @@ public class Practicum {
         System.out.println("2 - Нет");
         command = scanner.nextInt();
         if (command == 1) {
-            user.moneyEUR = null;
+            user.moneyEUR = 0.0;
         }
 
         System.out.println("Поздравляем, аккаунт для пользователя " + user.name + " создан.");
         System.out.println("Открытые счета:");
-        if (user.moneyRUB == null) {
+        if (user.moneyRUB != null) {
             System.out.println("- RUB");
         }
-        if (user.moneyUSD == null) {
+        if (user.moneyUSD != null) {
             System.out.println("- USD");
         }
-        if (user.moneyEUR == null) {
+        if (user.moneyEUR != null) {
             System.out.println("- EUR");
         }
 
