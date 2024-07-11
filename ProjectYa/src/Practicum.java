23e48982c8a293a2ed1153965994948a20b9a5a0
SergeiMish <mishalevskii@yandex.ru>
2024-07-11 17:45:56 +0500

    Sprint 3

--- ProjectYa/src/Practicum.java
+++ ProjectYa/src/Practicum.java
@@ -4,36 +4,54 @@ import java.util.HashMap;
 
 public class Practicum {
     public static void main(String[] args) {
-        HashMap<String, ArrayList<Integer>> stateHolidays;
-        stateHolidays = new HashMap<>();
+       ... // объявите и проиницализируйте хеш-таблицу musicGroups
+        musicGroups.put("The Beatles", 1960);
+        musicGroups.put("Rammstein", 1994);
+        musicGroups.put("Queen", 1970);
+        musicGroups.put("Led Zeppelin", 1968);
+        musicGroups.put("Black Sabbath", 1968);
+        musicGroups.put("AC/DC", 1973);
+        musicGroups.put("The Rolling Stones", 1962);
+        musicGroups.put("The Who", 1964);
+        musicGroups.put("Nightwish", 1996);
+        musicGroups.put("Nirvana", 1987);
+        musicGroups.put("Guns N’ Roses", 1985);
+        musicGroups.put("Scorpions", 1965);
+        musicGroups.put("Kiss", 1973);
+        musicGroups.put("The Doors", 1965);
 
-        ArrayList<Integer>january = new ArrayList<>();
-        january.add(1);
-        january.add(7);
+        System.out.println("Легендарные группы, год основания которых вы можете узнать:");
+        ... // распечайте список групп
+        ... // System.out.println(group)
 
-        ArrayList<Integer>february = new ArrayList<>();
-        february.add(23);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите опцию");
+            System.out.println("1 - Узнать год основания группы");
+            System.out.println("2 - Добавить любимую группу");
+            System.out.println("3 - Выйти из приложения");
+            int command = scanner.nextInt();
+            if (command == 1) {
+                ... // "Введите название группы"
+                ... // keyGroup
+                ... // year
+               ... // "Год основания " + keyGroup
+                System.out.println(year);
 
-        ArrayList<Integer>march = new ArrayList<>();
-        march.add(8);
+            } else if (command == 2) {
+                ... // "Добавьте свою любимую группу"
+                ... // "Введите её название"
+                ... // nameGroup
+                ... // "Введите год основания"
+                ... // bornYear
+                ...
+                System.out.println("Информация о группе " + nameGroup + " добавлена.");
 
-        ArrayList<Integer>may = new ArrayList<>();
-        may.add(1);
-        may.add(9);
-
-        ArrayList<Integer>june = new ArrayList<>();
-        june.add(12);
-
-        ArrayList<Integer>november = new ArrayList<>();
-        november.add(4);
-
-        stateHolidays.put("Январь", january);
-        stateHolidays.put("Февраль", february);
-        stateHolidays.put("Март", march);
-        stateHolidays.put("Май", may);
-        stateHolidays.put("Июнь", june);
-        stateHolidays.put("Ноябрь", november);
-
-        System.out.println(stateHolidays);
+            } else if (command == 3) {
+              ... // остановите цикл
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
     }
 }
