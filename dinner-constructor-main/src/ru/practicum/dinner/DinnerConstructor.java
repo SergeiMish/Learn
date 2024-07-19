44492c368a29a6ea4d4d7063a1cba4ce9738c2df
SergeiMish <mishalevskii@yandex.ru>
2024-07-19 15:58:23 +0500

    Split 3 (FT)

--- dinner-constructor-main/src/ru/practicum/dinner/DinnerConstructor.java
+++ dinner-constructor-main/src/ru/practicum/dinner/DinnerConstructor.java
@@ -6,10 +6,17 @@ import java.util.Map;
 import java.util.Random;
 
 public class DinnerConstructor {
-
+    /**
+     * Поле таблицы
+     */
     static Map<String, ArrayList<String>> dishes = new HashMap<>();
 
-
+    /**
+     * Private метод addNewDish внутри конструктора для добавления новых позиций
+     *
+     * @param dishType
+     * @param dishName
+     */
     private static void addNewDish(String dishType, String dishName) {
         if (dishes.containsKey(dishType)) {
             ArrayList<String> list = dishes.get(dishType);
@@ -27,11 +34,23 @@ public class DinnerConstructor {
         }
     }
 
+    /**
+     * Присвоение методу addNewDish публичного класса public
+     *
+     * @param dishType
+     * @param dishName
+     */
     public void addNewDishes(String dishType, String dishName) {
         addNewDish(dishType, dishName);
         System.out.println(dishes);
     }
 
+    /**
+     * Private метод generateDishCombo внутри конструктора для герерации КОМБО наборов
+     *
+     * @param numberOfCombos
+     * @param list
+     */
     private static void generateDishCombo(int numberOfCombos, ArrayList<String> list) {
         Random random = new Random();
         for (int i = 0; i < numberOfCombos; i++) {
@@ -44,11 +63,23 @@ public class DinnerConstructor {
         }
     }
 
+    /**
+     * Присвоение методу generateDishCombo публичного класса public
+     *
+     * @param numberOfCombos
+     * @param list
+     */
     // сгенерируйте комбинации блюд и выведите на экран
     public void generateDishCombos(int numberOfCombos, ArrayList<String> list) {
         generateDishCombo(numberOfCombos, list);
     }
 
+    /**
+     * Метод checkType проверки наличия категорий
+     *
+     * @param type
+     * @return
+     */
     public boolean checkType(String type) {
         return !dishes.containsKey(type);
     }
