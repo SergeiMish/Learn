2a6784c494927a604feaf042cf72d4cb4a2aa084
SergeiMish <mishalevskii@yandex.ru>
2024-07-25 18:44:02 +0500

    Sprint 4

--- ProjectYa/src/ru/yandex/practicum/фывпып.java
+++ ProjectYa/src/ru/yandex/practicum/фывпып.java
@@ -1,4 +1,20 @@
 package ru.yandex.practicum;
 
-public class фывпып {
+class Texla extends Automobile {
+    protected double autoPilotMaxSpeed;
+    protected double autoPilotAcceleration;
+
+    public Texla() {
+        autoPilotMaxSpeed = 60;
+        autoPilotAcceleration = 11;
+    }
+
+    public void accelerateByAutopilot() {
+        speed += autoPilotAcceleration;
+        // исправьте метод для ускорения при автопилоте
+    }
+
+    // переопределите метод для ускорения
+
+    // переопределите метод для торможения
 }
