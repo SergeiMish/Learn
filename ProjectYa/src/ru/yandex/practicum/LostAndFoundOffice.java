a9dbd37611d68a2784852d5d10ff3179558f34ee
SergeiMish <mishalevskii@yandex.ru>
2024-07-29 14:25:55 +0500

    Sprint 4

--- ProjectYa/src/ru/yandex/practicum/LostAndFoundOffice.java
+++ ProjectYa/src/ru/yandex/practicum/LostAndFoundOffice.java
@@ -2,8 +2,15 @@ package ru.yandex.practicum;
 import java.util.ArrayList;
 public class LostAndFoundOffice {
     // создайте список things
-
+    ArrayList<Object> things = new ArrayList<>();
     // реализуйте метод put()
+    public void put(Object element){
+        things.add(element);
+        }
+    }
+    public check(Object){
+
+    }
 
     // реализуйте метод check()
 
