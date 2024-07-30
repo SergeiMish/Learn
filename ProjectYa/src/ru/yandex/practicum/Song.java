672a0ba853bcf42ce93b08aa53b62dfe73a24abe
SergeiMish <mishalevskii@yandex.ru>
2024-07-30 12:15:30 +0500

    Sprint 4

--- ProjectYa/src/ru/yandex/practicum/Song.java
+++ ProjectYa/src/ru/yandex/practicum/Song.java
@@ -6,32 +6,31 @@ public class Song {
     public final String title;
     public final String artist;
     public final String songwriter;
-    private int value;
+
 
     public Song(String title, String artist, String songwriter) {
         this.title = title;
         this.artist = artist;
         this.songwriter = songwriter;
     }
-
-    // переопределите метод equals(Object)
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) { // проверяем рефлексивность
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) { // проверяем на неравенство объекту другого класса и на неравенство пустой ссылке
-            return false;
-        }
-
-        Song other = (Song) obj;
-
-        return value == other.value; // проверяем на правильное сравнение объектов одного класса
+@Override
+public boolean equals(Object obj) {
+    if (this == obj) {
+        return true;
     }
+    if (obj == null || getClass() != obj.getClass()) {
+        return false;
+    }
+    Song song = (Song) obj;
+    return title.equals(song.title) &&
+            artist.equals(song.artist) &&
+            songwriter.equals(song.songwriter);
+}
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return Objects.hash(title, artist, songwriter);
     }
+    // переопределите метод equals(Object)
+
 }
