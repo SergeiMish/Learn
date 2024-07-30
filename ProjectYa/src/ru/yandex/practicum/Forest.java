1decdad5ee6c705802ba5cc5f8c5187bf96e7960
SergeiMish <mishalevskii@yandex.ru>
2024-07-30 18:57:39 +0500

    Sprint 4

---
+++ ProjectYa/src/ru/yandex/practicum/Forest.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum;
+
+import java.util.ArrayList;
+
+public class Forest {
+    private ArrayList<MountainHare> hares;
+
+    // объявите недостающие переменные и добавьте конструктор
+
+    // добавьте метод setSeason(String newSeason)
+    // в этом методе реализуйте логику смены цвета шубок зайцев-беляков
+
+    // добавьте метод printHares()
+
+}
