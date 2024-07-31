64044bd79651b233f85f86c1aa4eeec0f63bf150
SergeiMish <mishalevskii@yandex.ru>
2024-07-31 17:43:34 +0500

    Sprint 4

--- ProjectYa/src/ru/yandex/practicum/TransactionValidator.java
+++ ProjectYa/src/ru/yandex/practicum/TransactionValidator.java
@@ -5,8 +5,14 @@ public class TransactionValidator {
     final static int MIN_AMOUNT = 1;
     final static int MAX_AMOUNT = 5000;
 
+    public void isValidAmount(){
+        if (MIN_AMOUNT < 1){
+            System.out.println("Минимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
+        }
+        else if ()
+    }
     // объявите метод isValidAmount()
     // внутри метода добавьте проверки на минимальную и максимальную сумму перевода
-    System.out.println("Минимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
+
     System.out.println("Максимальная сумма перевода: " + ... + " р. Попробуйте ещё раз!");
 }
