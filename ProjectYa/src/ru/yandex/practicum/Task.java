4fbfad3468b1d20228ce95f2c0f7ab70d27f07ca
SergeiMish <mishalevskii@yandex.ru>
2024-08-01 14:10:13 +0500

    Sprint 4

--- ProjectYa/src/ru/yandex/practicum/Task.java
+++ ProjectYa/src/ru/yandex/practicum/Task.java
@@ -2,25 +2,20 @@ package ru.yandex.practicum;
 
 public class Task {
 
-    static String priority; // добавьте переменную priority с приоритетом задачи
-
+    private String priority; // добавьте переменную priority с приоритетом задачи
     private String description;
-    public Task(String description) {
+    public Task(TaskPriority priority, String description) {
         this.description = description;
     }
     // добавьте конструктор класса
-
-    public static String getPriority() {
-        if (priority.equals(TaskPriority.HIGH)){
-            System.out.println("ываыа");
-        }
-
-        return priority;
-    }
 
     // добавьте метод get для приоритета
 
     public String getDescription() {
         return description;
+    }
+
+    public String getPriority() {
+        return priority;
     }
 }
