7fe2521ec868fd327a3693ac3222dcae70905f88
SergeiMish <mishalevskii@yandex.ru>
2024-08-12 14:35:17 +0500

    Sprint 5

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,62 +1,60 @@
 package ru.yandex.practicum;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Practicum {
-
     public static void main(String[] args) {
-        List<MediaItem> mediaItems = new ArrayList<>();
+        //создаём двухуровневую хеш-таблицу для хранения расписания уроков
+        AdvancedHashMap<String, Integer, String> timetable = new AdvancedHashMap<>();
+        //добавляем уроки, с указанием дня недели и номера урока
+        timetable.put("Понедельник", 1, "Русский язык");
+        timetable.put("Понедельник", 2, "Математика");
+        timetable.put("Вторник", 1, "Физкультура");
+        timetable.put("Вторник", 2, "Русский язык");
 
-        Scanner scanner = new Scanner(System.in);
+        //выводим первый урок во вторник
+        System.out.println(timetable.get("Вторник", 1));
 
-        while (true) {
-            printMenu();
-            // Считайте команду, введенную пользователем
-            int command = scanner.nextInt();
-
-            if (command == 1) {
-                System.out.println("Введите название фильма:");
-                String title = scanner.next();
-                System.out.println("Введите длительность фильма в минутах:");
-                int runtime = scanner.nextInt();
-
-                // На основе введенных пользователем значений создайте объект класса Movie
-                Movie movie = new Movie(title, runtime);
-                mediaItems.add(movie);
-            } else if (command == 2) {
-                System.out.println("Введите название сериала:");
-                String title = scanner.next();
-                System.out.println("Введите количество серий:");
-                int seriesCount = scanner.nextInt();
-                System.out.println("Введите среднюю длительность серии в минутах");
-                int runtime = scanner.nextInt();
-
-                // Создайте сериал и добавьте его в список просмотренных
-                Series series = new Series(title, runtime, seriesCount);
-                mediaItems.add(series);
-            } else if (command == 0) {
-                printMediaItemsList(mediaItems);
-
-                double totalRuntime = Calculator.calculate(mediaItems);
-                System.out.println("Всего вы потратили на просмотр фильмов и сериалов, в днях: " + totalRuntime);
-                break;
-            }
-        }
-    }
-
-    public static void printMenu() {
-        System.out.println("1 - Добавить фильм");
-        System.out.println("2 - Добавить сериал");
-        System.out.println("0 - Посчитать суммарное время и выйти");
-    }
-
-
-    public static void printMediaItemsList(List<MediaItem> mediaItems) {
-        System.out.println("Вы посмотрели фильмов и сериалов: " + mediaItems.size());
-        // Допишите вывод названий всех просмотренных фильмов и сериалов
-        for (MediaItem mediaItem : mediaItems) {
-            System.out.println(mediaItem.getTitle());
-        }
+        //получаем и выводим все уроки в понедельник
+        ArrayList<String> mondayLessons = timetable.getAll("Понедельник");
+        System.out.println(mondayLessons);
     }
 
 }
+
+class AdvancedHashMap <K1, K2, V>  {
+        //данные будем хранить в хеш-таблице из хеш-таблиц
+        HashMap<Integer, String > internalHashMap = new HashMap<>();
+
+public void put(...) {
+        //получаем хеш-таблицу по первому ключу
+        ... innerHashMap = ...;
+        if (innerHashMap == null) {
+        //вложенной хеш-таблицы по первому ключу пока нет — создаём её и добавляем в internalHashMap
+        innerHashMap = new HashMap<>();
+        ...
+        }
+        //добавляем элемент во вложенную хеш-таблицу
+        ...
+        }
+
+public ... get(...) {
+        //получаем хеш-таблицу по первому ключу
+        ... innerHashMap = ...;
+        if (innerHashMap == null) {
+        return null;
+        }
+        //получаем элемент из вложенной хеш-таблицы
+        return ...;
+        }
+
+public ... getAll(...) {
+        ... innerHashMap = ...;
+        if (innerHashMap == null) {
+        return new ArrayList<>();
+        }
+        return new ArrayList<>(innerHashMap.values());
+        }
+        }
