177e3edbdef4fb050d5427026267138a6e792daf
SergeiMish <mishalevskii@yandex.ru>
2024-08-14 20:34:36 +0500

    Sprint 5

---
+++ ProjectYa/src/ru/yandex/practicum/BankAccountTest.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum;
+
+
+// Дополните класс для проверки возраста пользователя
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class BankAccountTest {
+
+    @Test
+    public void shouldNotBeBlockedWhenCreated() {
+        BankAccount account = new BankAccount("a", "b");
+        assertFalse(account.isBlocked());
+    }
+
+    @Test
+    public void shouldReturnZeroAmountAfterActivation() {
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("RUB");
+        assertEquals(Integer.valueOf(0), account.getAmount());
+        assertEquals("RUB", account.getCurrency());
+    }
+    public void shouldBeBlockedAfterBlockIsCalled(){
+
+    }
+}
