1f09541fe87cb1420b0a672e8d1ca3c0d9acbcf4
SergeiMish <mishalevskii@yandex.ru>
2024-08-16 10:23:11 +0500

    Sprint 5

--- ProjectYa/src/ru/yandex/practicum/FortuneCookieControllerTest.java
+++ ProjectYa/src/ru/yandex/practicum/FortuneCookieControllerTest.java
@@ -1,12 +1,44 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FortuneCookieControllerTest {
 
-    public void shouldReturnPositiveFortune(){
+    private static FortuneCookieController goodFactoryController;
+    private static FortuneCookieController badFactoryController;
 
+    @BeforeAll
+    public static void setUp() {
+        goodFactoryController = create(true);
+        badFactoryController = create(false);
     }
-    public void shouldReturnNegativeFortune(){
 
+    private static FortuneCookieController create(boolean isPositive) {
+        FortuneConfig config = new FortuneConfig(isPositive);
+        ArrayList<String> positive = new ArrayList<>();
+        positive.add("positive");
+        ArrayList<String> negative = new ArrayList<>();
+        negative.add("negative");
+        FortuneCookieFactory factory = new FortuneCookieFactory(config, positive, negative);
+        return new FortuneCookieController(factory);
     }
 
+    @Test
+    public void shouldReturnPositiveFortune() {
+        String fortune = goodFactoryController.getFortune();
+        assertEquals("positive", fortune);
+    }
+
+    @Test
+    public void shouldReturnNegativeFortune() {
+        String fortune = badFactoryController.getFortune();
+        assertEquals("negative", fortune);
+    }
 }
+
+
