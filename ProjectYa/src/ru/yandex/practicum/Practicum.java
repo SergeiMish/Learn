e8ecec3e398a2eb82069b9d7475e49f7e8e766da
SergeiMish <mishalevskii@yandex.ru>
2024-09-02 12:48:08 +0500

    Sprint 6

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -12,15 +12,15 @@ public class Practicum {
         Scanner s = new Scanner(System.in);
         int userGuess = -1;
 
+
         // Считывайте числа от пользователя пока не найдёте число, равное target
         while (userGuess != target){
+            userGuess = s.nextInt();
             if (userGuess > target){
                 System.out.println("Ваше число больше");
-                s.nextInt();
             }
             else if (userGuess < target){
                 System.out.println("Ваше число меньше");
-                s.nextInt();
             }
             else {
                 System.out.println("Правильный ответ");
