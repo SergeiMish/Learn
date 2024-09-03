4370c2b83a9bbd67b8f5739e91d82b30d00e52e0
SergeiMish <mishalevskii@yandex.ru>
2024-09-03 18:45:08 +0500

    Sprint 6

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -5,27 +5,52 @@ import java.util.Scanner;
 
 public class Practicum {
     public static void main(String[] args) {
-        Random random = new Random();
-        int target = random.nextInt(1000) + 1;
-        System.out.println("Я загадал число. Попробуйте угадать!");
+        int[] array1 = new int[]{1, 3, 4, 6, 8};
+        int[] array2 = new int[]{1, 2, 3, 8, 9};
+        int[] resultArray = merge(array1, array2);
 
-        Scanner s = new Scanner(System.in);
-        int userGuess = -1;
-
-
-        // Считывайте числа от пользователя пока не найдёте число, равное target
-        while (userGuess != target){
-            userGuess = s.nextInt();
-            if (userGuess > target){
-                System.out.println("Ваше число больше");
-            }
-            else if (userGuess < target){
-                System.out.println("Ваше число меньше");
-            }
-            else {
-                System.out.println("Правильный ответ");
-                break;
-            }
+        for (int e : resultArray) {
+            System.out.print(e + ", ");
         }
+    }
+
+    private static int[] merge(int[] leftArray, int[] rightArray) {
+
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int[] resultArray = new int[leftSize + rightSize];
+
+        // Индексы, по которым идёт итерация
+        int left = 0;
+        int right = 0;
+        int index = 0;
+
+        while (index < resultArray.length) {
+
+            // Если левый индекс больше максимального левого индекса — добавляем элемент из правого подмассива.
+            if (left >= leftSize) {
+                resultArray[index] = rightArray[right];
+                right++;
+            }
+
+            // Если правый индекс больше максимального — добавляем элемент из левого подмассива.
+            else if (right >= rightSize) {
+                // !!! Добавьте ваш код
+            }
+
+            // Если оба текущих индекса внутри своих границ, нужно сравнивать элементы по этим индексам
+            // Если элемент в левом массиве меньше — добавляем его и увеличиваем левый индекс.
+            else if (leftArray[left] <= rightArray[right]) {
+                resultArray[index] = leftArray[left];
+                left++;
+            }
+            // Иначе — делаем тоже самое с правым индексом.
+            else {
+                // !!! Добавьте ваш код
+            }
+            index++;
+        }
+
+        return resultArray;
     }
 }
