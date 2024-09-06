8f9a68e2fce6b778d9f552c2d1e5da99d13bfb7e
SergeiMish <mishalevskii@yandex.ru>
2024-09-06 18:41:25 +0500

    Sprint 6

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -27,19 +27,26 @@ public class Practicum {
     }
 
     private void newOrder(String clientName) {
-        for (String s : orders.keySet()) {
-            
+        if (orders.containsKey(clientName)) {
+            orders.put(clientName, orders.get(clientName) + 1);
+        } else {
+            orders.put(clientName, 1);
         }
-        // Сохраните новый заказ в хеш-таблицу. При обновлении счётчика заказов
-        // не забудьте учесть заказы, которые уже были сделаны ранее.
     }
 
     private void printStatistics() {
+        int sum = 0;
+        for (Map.Entry<String, Integer> stringIntegerEntry : orders.entrySet()) {
+            System.out.println("Заказов от " + stringIntegerEntry.getKey() + ": " + stringIntegerEntry.getValue());
+            sum = stringIntegerEntry.getValue() + sum;
+        }
+        System.out.println("Всего заказов: " + sum);
+    }
+}
+
         // Выведите собранную статистику в консоль и посчитайте общее количество заказов.
         //
         // Формат для вывода данных в консоль:
         //     Заказов от Микеланджело: 15
         //     Заказов от Леонардо: 4
         //     Всего заказов: 19
-    }
-}
