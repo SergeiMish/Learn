143564bcdf1a7ec67d9e95274614d79487ee4732
SergeiMish <mishalevskii@yandex.ru>
2024-09-07 12:35:14 +0500

    Sprint 6

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,26 +1,49 @@
 package ru.yandex.practicum;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Practicum {
-    private static Map<String, String> films = new LinkedHashMap<>();
-
     public static void main(String[] args) {
-        films.put("Титаник", "Джеймс Кэмерон");
-        films.put("Гарри Поттер и философский камень", "Крис Коламбус");
-        films.put("Хакеры", "Иэн Софтли");
-        films.put("Терминатор", "Джеймс Кэмерон");
+        // ключ – автомобиль, значение – цена
+        Map<Car, Integer> cars = new HashMap<>();
 
-        for (String film : films.keySet()) {
-            System.out.println(film);
+        // хеш-таблица заполняется данными
+        cars.put(new Car("Audi A6", 3_760_000), 2);
+        cars.put(new Car("Honda CR-V ", 2_500_000), 3);
+        cars.put(new Car("KIA Cerato", 1_300_000), 8);
+        cars.put(new Car("Volkswagen Tiguan", 1_935_000), 5);
+
+        // проверяем порядок
+        for (Car car : cars.keySet()) {
+            System.out.println(car);
         }
     }
 }
-        // Выведите собранную статистику в консоль и посчитайте общее количество заказов.
-        //
-        // Формат для вывода данных в консоль:
-        //     Заказов от Микеланджело: 15
-        //     Заказов от Леонардо: 4
-        //     Всего заказов: 19
+
+class Car {
+    String model;
+    Integer priceInRubles;
+
+    public Car(String model, Integer priceInRubles) {
+        this.model = model;
+        this.priceInRubles = priceInRubles;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        return model.equals(car.model);
+    }
+
+    public int hashCode() {
+        return model.hashCode();
+    }
+
+    public String toString() {
+        return "Car{model=" + model + ", priceInRubles=" + priceInRubles + "}";
+    }
+}
