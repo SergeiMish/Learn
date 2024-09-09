537e8a3e2d1cd8afef451175577d857ff1e14920
SergeiMish <mishalevskii@yandex.ru>
2024-09-09 15:22:19 +0500

    Sprint 6

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,34 +1,72 @@
 package ru.yandex.practicum;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-class Practicum {
-    private static List<String> allPurchases = List.of(
-            "яблоки",
-            "молоко",
-            "колбаса",
-            "огурцы",
-            "сок",
-            "хлеб",
-            "виноград",
-            "молоко",
-            "йогурт",
-            "хлеб",
-            "пельмени"
-    );
-
+public class Practicum {
     public static void main(String[] args) {
-        // переменная uniquePurchases должна содержать множество уникальных товаров
-        Set<String> uniquePurchases = findUniquePurchases(allPurchases);
+        Comparator<Ticket> comparator = new Comparator<Ticket>();{
+            @Override
+            public int compare(Ticket o1, Ticket o2) {
+                return o1.priceInUsd - o2.priceInUsd;
+            }
+        }
+    }
+    Set<Ticket> tickets = new TreeSet<>();
+    fillTickets(tickets);
 
-        // допишите вывод количества уникальных товаров
-        System.out.println( "За месяц было куплено " + uniquePurchases.size() + " уникальных товаров.");
+        System.out.println("Доступные билеты: ");
+        for (Ticket ticket : tickets) {
+        System.out.println("  * " + ticket);
+    }
+}
+
+    private static void fillTickets(Set<Ticket> tickets) {
+        tickets.add(new Ticket("Лондон", "Париж", 376));
+        tickets.add(new Ticket("Милан", "Москва", 298));
+        tickets.add(new Ticket("Берлин", "Бостон", 1273));
+        tickets.add(new Ticket("Пекин", "Рим", 846));
+        tickets.add(new Ticket("Санкт-Петербург", "Афины", 284));
+        tickets.add(new Ticket("Сидней", "Токио", 1738));
+        tickets.add(new Ticket("Мюнхен", "Дубай", 974));
     }
 
-    // реализуйте этот метод
-    public static Set<String> findUniquePurchases(List<String> allPurchases) {
-        return new HashSet<>(allPurchases);
+public static class Ticket {
+    public String from;
+    public String to;
+    public int priceInUsd;
+
+    public Ticket(String from, String to, int priceInUsd) {
+        this.from = from;
+        this.to = to;
+        this.priceInUsd = priceInUsd;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ticket ticket = (Ticket) o;
+
+        if (priceInUsd != ticket.priceInUsd) return false;
+        if (!from.equals(ticket.from)) return false;
+        if (!to.equals(ticket.to)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = from.hashCode();
+        result = 31 * result + to.hashCode();
+        result = 31 * result + priceInUsd;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{from=" + from + ", to=" + to + ", priceInUsd=" + priceInUsd + '}';
+    }
+}
 }
