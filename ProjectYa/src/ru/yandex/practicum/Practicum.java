4b957728cb9eac79f57e6191b6856c6591ae606b
SergeiMish <mishalevskii@yandex.ru>
2024-09-16 17:52:39 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,11 +1,20 @@
 package ru.yandex.practicum;
 
 public class Practicum {
-    public String fixString(String str) {
-        if (str == null || str.trim().isBlank()) {
-            System.out.println("Вы ничего не ввели!");
-            return "";
-        } else {
-            return str;
+    private boolean isCapsLock = false;
+
+    public void capsLock() {
+        isCapsLock = !isCapsLock;
+        // Здесь нужно изменить значение флага isCapsLock на противоположное
+    }
+
+    public void print(String str) {
+        if (isCapsLock){
+            System.out.println(str.toUpperCase());
         }
+        else {
+            System.out.println(str);
+        }
+        // а здесь нужно распечатать строку или в верхнем регистре, или без изменений, учитывая флаг
+    }
 }
