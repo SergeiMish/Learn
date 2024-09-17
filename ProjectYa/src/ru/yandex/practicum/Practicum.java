d51d92d63129436dd4d25b6a5fc1dbd504c1f0f7
SergeiMish <mishalevskii@yandex.ru>
2024-09-17 21:08:34 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -2,12 +2,7 @@ package ru.yandex.practicum;
 
 public class Practicum {
     public static void main(String[] args) {
-        FindRepeats check = new FindRepeats();
-        int count = check.numberOfRepeats("раз, раз, раз", "раз");
-        int count2 = check.numberOfRepeats("Hello, world!", "goodbye"); // вернёт 0)
-        int count3 = check.numberOfRepeats("раз два три, раз два три", "раз"); // 2
-        System.out.println(count);
-        System.out.println(count2);
-        System.out.println(count3);
+        Capitalizator check = new Capitalizator();
+        System.out.println(check.capitalize("я не волшебник, я только учусь!"));
     }
 }
