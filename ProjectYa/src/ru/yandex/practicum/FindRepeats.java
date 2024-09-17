b4a817e7a571cf3cd8edd986805df4e20def86b3
SergeiMish <mishalevskii@yandex.ru>
2024-09-17 20:38:06 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/FindRepeats.java
+++ ProjectYa/src/ru/yandex/practicum/FindRepeats.java
@@ -2,6 +2,14 @@ package ru.yandex.practicum;
 
 public class FindRepeats {
     int numberOfRepeats(String text, String substring) {
-
+        int repeat = 0;
+            while (text.contains(substring)){
+                if (text.equals(substring)){
+                    repeat++;
+                    text = null;
+                }
+                int index = text.indexOf(substring) + substring.length();
+                return repeat + index;
+            } return repeat;
+        }
     }
-}
--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -4,6 +4,10 @@ public class Practicum {
     public static void main(String[] args) {
         FindRepeats check = new FindRepeats();
         int count = check.numberOfRepeats("раз, раз, раз", "раз");
+        int count2 = check.numberOfRepeats("Hello, world!", "goodbye"); // вернёт 0)
+        int count3 = check.numberOfRepeats("раз два три, раз два три", "раз"); // 2
         System.out.println(count);
+        System.out.println(count2);
+        System.out.println(count3);
     }
 }
