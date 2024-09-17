029a4b51e9c1bee87f65d75045b326e02ffe0027
SergeiMish <mishalevskii@yandex.ru>
2024-09-17 20:09:50 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,25 +1,9 @@
 package ru.yandex.practicum;
 
 public class Practicum {
-
-    private boolean isCapsLock = false;
-
-    public void capsLock() {
-        isCapsLock = !isCapsLock;
-        // Здесь нужно изменить значение флага isCapsLock на противоположное
+    public static void main(String[] args) {
+        FindRepeats check = new FindRepeats();
+        int count = check.numberOfRepeats("раз, раз, раз", "раз");
+        System.out.println(count);
     }
-
-    public void print(String str) {
-        if (isCapsLock){
-            System.out.println(str.toUpperCase());
-        }
-        else {
-            System.out.println(str);
-            System.out.println("Так много методов!".indexOf("м", 3) == 4);
-            System.out.println("Как их все запомнить?!".charAt(20)  == '?');
-            System.out.println("Шиншилла".lastIndexOf("ш")  == 4);
-        }
-        // а здесь нужно распечатать строку или в верхнем регистре, или без изменений, учитывая флаг
-    }
-
 }
