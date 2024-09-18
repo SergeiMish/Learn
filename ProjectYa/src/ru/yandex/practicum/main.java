bf31be846691d329b01d9f0204e834ebcd6e3b82
SergeiMish <mishalevskii@yandex.ru>
2024-09-18 22:36:23 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/main.java
+++ ProjectYa/src/ru/yandex/practicum/main.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum;
 
 public class main {
     public static void main(String[] args) {
-        FindRepeats findRepeats = new FindRepeats();
-        findRepeats.isPalindromeLine("300 спартанцев");
-        System.out.println();
+
     }
 }
