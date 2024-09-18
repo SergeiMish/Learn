b3965194d4cd5977a298211467a4b5bb60e2c14d
SergeiMish <mishalevskii@yandex.ru>
2024-09-18 23:17:43 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/Grades.java
+++ ProjectYa/src/ru/yandex/practicum/Grades.java
@@ -1,15 +1,58 @@
 package ru.yandex.practicum;
 
-public class FindRepeats {
-    public boolean isPalindromeLine(String str) {
+public class Grades {
 
-        String str2 = str.replaceAll("\\s+", "").toLowerCase();
-        StringBuilder sb = new StringBuilder(str2);
-        String str3 = sb.reverse().toString();
-        if (str2.equals(str3)) {
-            System.out.println(true);
-            return true;
-        }
-        return false;
+    private String capitalize(String str) {
+        return str.substring(0,1).toUpperCase() + str.substring(1);
     }
-}
+
+    private String gradeToString(String grade) {
+        switch (grade) {
+            case "5": {
+                return "Безупречно";
+            }
+            case "4": {
+                return "Потрясающе";
+            }
+            case "3": {
+                return "Восхитительно";
+            }
+            case "2": {
+                return "Прекрасно";
+            }
+            default:
+                return "Очаровательно";
+        }
+    }
+
+    // grades - строка вида "имя,фамилия,предмет,оценка;имя,фамилия,предмет,оценка;"
+    public void gradeBeautifier(String grades) {
+        StringBuilder sb = new StringBuilder();
+        String[] split = grades.split(";");
+        for (String s : split) {
+            String [] pureSplit = s.split(",");
+            for (String s1 : pureSplit) {
+                pureSplit[0] = capitalize(grades);
+                pureSplit[1] = capitalize(grades);
+                pureSplit[2] = grades.toLowerCase();
+                pureSplit[3] = gradeToString(grades);
+                System.out.println(s1);
+            }
+        }
+
+
+//            if (i == 0 ){
+//                capitalize(grades);
+//            }
+//            if (i == 1){
+//                capitalize(grades);
+//            }
+//            if (i == 2){
+//                grades.toLowerCase();
+//                System.out.println(" - ");
+//            }
+//            if (i == 3){
+//                gradeToString(grades);
+//            }
+        }
+    }
--- ProjectYa/src/ru/yandex/practicum/main.java
+++ ProjectYa/src/ru/yandex/practicum/main.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum;
 
 public class main {
     public static void main(String[] args) {
-
+    String grades = "вероника,чехова,ФИЗИКА,5;анна,строкова,МАТЕМАТИКА,4;иван,петров,ГЕОМЕТРИЯ,5";
+    Grades gd = new Grades();
+    gd.gradeBeautifier(grades);
     }
 }
