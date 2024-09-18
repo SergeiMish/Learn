f5ee9e1eb8a92f011c57a68d3baab7a2a96968a0
SergeiMish <mishalevskii@yandex.ru>
2024-09-18 20:31:28 +0500

    Sprint 7

---
+++ ProjectYa/src/ru/yandex/practicum/PoemFixer.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum;
+
+public class PoemFixer {
+
+    public String[] readPoem() {
+        return new String[]{
+                "   Это кто там ложку «ложит»?",
+                "",
+                "   ",
+                "Знай, такого быть не может!",
+                "  Ложку мы на стол кладём,  ",
+                "",
+                "А тебя – к обеду ждём."
+        };
+    }
+
+    public String fixPoem(String[] poem) {
+        StringBuilder sb = new StringBuilder();
+        for (String line : poem) {
+            if (!line.isBlank() && !line.isEmpty()) {
+                sb.append(line.trim() + "\n");
+            }
+            // допишите код
+        }return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        PoemFixer poemFixer = new PoemFixer();
+        String[] poem = poemFixer.readPoem();
+        String poemAsString = poemFixer.fixPoem(poem);
+        System.out.println(poemAsString);
+    }
+}
