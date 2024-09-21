442d4fb53f90b4a62d26c0fcdfb383ed08a8ef14
SergeiMish <mishalevskii@yandex.ru>
2024-09-21 10:20:39 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/CheckPrinterImproved.java
+++ ProjectYa/src/ru/yandex/practicum/CheckPrinterImproved.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CheckPrinterImproved {
@@ -15,7 +17,26 @@ public class CheckPrinterImproved {
     }
 
     public static void printCheck(String[] items) {
+        items = new String[]{"Пицца, 1 шт., 310.50", "Чай, 2 шт., 113.30", "Печенье, 1 уп., 75.75"};
+        List<String> names = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+        List<String> costs = new ArrayList<>();
+        for (String item : items) {
+            String[] split = item.split(", ");
+            String name = split[0];
+            String value = split[1];
+            String cost = split[2];
+            names.add(name);
+            values.add(value);
+            costs.add(cost);
+        }
+        int maxNameLength = findMaxLength(names.toArray(new String[0]));
+        int maxValueLength = findMaxLength(values.toArray(new String[0]));
+        String format = "%-" + (maxNameLength + 2) + "s%-" + (maxValueLength + 2) + "s%s%n";
 
+        for (int i = 0; i < names.size(); i++) {
+            System.out.printf(format, names.get(i), values.get(i), costs.get(i));
+        }
     }
 
     public static void main(String[] args) {
