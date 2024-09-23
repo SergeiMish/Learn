c5dfa629bd5f4f27703c0d8590ec6db454fcd7a8
SergeiMish <mishalevskii@yandex.ru>
2024-09-23 22:17:28 +0500

    Sprint 7

---
+++ ProjectYa/src/ru/yandex/practicum/IncorrectInputException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class IncorrectInputException extends Exception {
+    public IncorrectInputException(String message) {
+        super(message);
+    }
+}
--- ProjectYa/src/ru/yandex/practicum/InputException.java
+++
@@ -1,7 +0,0 @@
-//package ru.yandex.practicum;
-//
-//public class InputException extends Exception {
-//    public InputException(final String message){
-//        super(message);
-//    }
-//}
