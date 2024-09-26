cd822d54308d681cda0ef62cb869d71f2982a846
SergeiMish <mishalevskii@yandex.ru>
2024-09-26 21:41:59 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,97 +1,33 @@
 package ru.yandex.practicum;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Practicum {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> frequencyMap = new HashMap<>();
 
-        while (true) {
-            printMenu();
-            String command = scanner.nextLine();
+        FileReader reader = new FileReader("result.txt");
+        BufferedReader br = new BufferedReader(reader);
 
-            System.out.println("Введите путь к файлу/директории: ");
-            String enteredPath = scanner.nextLine();
-            Path path = Paths.get(enteredPath); // создайте переменную пути
-            if (!Files.isExecutable(path)) { // проверьте, не ошибся ли пользователь
-                System.out.println("Введённый путь не существует.");
-                break;
+        while (br.ready()) {
+            int count = 0;
+            String name = br.readLine();
+            if (name.equals(br.readLine())) {
+                count++;
+                frequencyMap.put(name, count);
             }
-            switch (command) {
-                case "exit":
-                    System.out.println("Выход.");
-                    System.exit(0); // пользователь хочет найти выход, выход есть всегда
-                    break;
-                case "ls":
-                    try {
-                        for (String element : path.toFile().list()) {
-                            System.out.println(element);
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Произошла ошибка при запросе содержимого директории.");
-                        e.printStackTrace();
-                    }
-                    break;
-
-                case "mkdir":
-                    try {
-                        Files.createDirectory(path);
-                    } catch (IOException e) {
-                        System.out.println("Произошла ошибка при создании директории.");
-                        e.printStackTrace();
-                    }
-                    break;
-                case "touch":
-                    try {
-                        Files.createFile(path);
-                    } catch (IOException e) {
-                        System.out.println("Произошла ошибка при создании файла.");
-                        e.printStackTrace();
-                    }
-                    break;
-                case "rename":
-                    System.out.println("Введите новое имя файла/директории: ");
-                    String newName = scanner.nextLine();
-
-                    try {
-                        Files.move(path, Path.of(newName), StandardCopyOption.REPLACE_EXISTING); // с помощью опции StandardCopyOption.REPLACE_EXISTING
-                    } catch (IOException e) {
-                        System.out.println("Произошла ошибка при переименовании файла/директории.");
-                        e.printStackTrace();
-                    }
-                    break;
-                case "rm_file":
-                    try {
-                        if (!Files.isDirectory(path)) {
-                            Files.deleteIfExists(path);
-                        } else {
-                            System.out.println("С помощью этой команды можно удалить только файл!");
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Произошла ошибка при удалении файла.");
-                        e.printStackTrace();
-                    }
-                default:
-                    System.out.println("Извините, такой команды пока нет.");
-            }
-
-        }
+            System.out.println(name + ": " + count);
+        } reader.close();
     }
 
-    public static void printMenu() {
-        System.out.println("Что вы хотите сделать? ");
-        System.out.println("ls - посмотреть содержимое директории.");
-        System.out.println("mkdir - создать директорию.");
-        System.out.println("touch - создать файл.");
-        System.out.println("rename - переименовать директорию/файл.");
-        System.out.println("rm_file - удалить файл.");
-        System.out.println("exit - выход.");
-    }
 
-}
+        // читайте файл построчно и сразу обновляйте frequencyMap.
+
+        // выведите результат в формате "<буква>: <количество>".
+
+    }
