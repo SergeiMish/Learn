a263a2f15c1a12ff675783e5285a030af8dfa6cd
SergeiMish <mishalevskii@yandex.ru>
2024-09-26 22:33:35 +0500

    Sprint 7

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,28 +1,29 @@
 package ru.yandex.practicum;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Practicum {
 
-    public static void main(String[] args) throws IOException {
-        Map<String, Integer> frequencyMap = new HashMap<>();
-
-        FileReader reader = new FileReader("result.txt");
-        BufferedReader br = new BufferedReader(reader);
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            frequencyMap.put(line, frequencyMap.getOrDefault(line, 0) + 1);
-        }
-        br.close();
-        // читайте файл построчно и сразу обновляйте frequencyMap.
-        // выведите результат в формате "<буква>: <количество>".
-        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+    public static void main(String[] args) {
+        BufferedReader fileReader = null;
+        try {
+            fileReader = new BufferedReader(new FileReader("input.txt"));
+            fileReader.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
