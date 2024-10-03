d4bcd2e23ac0b5cd99752e313740124ab1bc23b6
SergeiMish <mishalevskii@yandex.ru>
2024-10-03 21:33:03 +0500

    Sprint 8

--- ProjectYa/src/ru/yandex/practicum/PrimeNumbers.java
+++ ProjectYa/src/ru/yandex/practicum/PrimeNumbers.java
@@ -10,10 +10,10 @@ public class PrimeNumbers {
     public static void main(String[] args) {
         // Лямбда, возвращающая целое число в интервале от 1 до 100
         // Укажите правильный тип данных для переменной randomInt
-        Supplier <Integer> randomInt = () -> ThreadLocalRandom.current().nextInt(0,100);
+        Supplier<Integer> randomInt = () -> ThreadLocalRandom.current().nextInt(0, 100);
         // Лямбда, проверяющая, является ли число простым
         // Укажите правильный тип данных для переменной isPrime
-        Predicate <Integer> isPrime = number -> {
+        Predicate<Integer> isPrime = number -> {
             int count = 0;
             for (int i = 1; i <= number; i++) {
                 if (number % i == 0) {
@@ -24,7 +24,7 @@ public class PrimeNumbers {
         };
         // Лямбда, принимающая два аргумента (целое число и строку) и выводящая сообщение
         // укажите правильные параметры типа для BiConsumer
-        BiConsumer <Integer, String> outputConsumer =
+        BiConsumer<Integer, String> outputConsumer =
                 (num, str) -> System.out.println("Число " + num + " " + str);
 
         // Используем созданные лямбды
