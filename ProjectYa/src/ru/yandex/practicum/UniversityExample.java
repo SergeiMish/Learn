7dd22bbb93ef6c54b55a791543d4717ad5e1e733
SergeiMish <mishalevskii@yandex.ru>
2024-10-03 19:46:21 +0500

    Sprint 8

--- ProjectYa/src/ru/yandex/practicum/UniversityExample.java
+++ ProjectYa/src/ru/yandex/practicum/UniversityExample.java
@@ -1,31 +1,17 @@
 package ru.yandex.practicum;
 import java.util.function.BiFunction;
 
-public class ArithmeticOperationExample {
+interface Summator {
+    int sum(int n1, int n2);
+}
+
+public class Practicum {
 
     public static void main(String[] args) {
-        BiFunction<Integer, Integer, Integer> plusOperation = getOperation("+");
-        BiFunction<Integer, Integer, Integer> divideOperation = getOperation("/");
-        BiFunction<Integer, Integer, Integer> minOperation = getOperation("min");
-        BiFunction<Integer, Integer, Integer> maxOperation = getOperation("max");
-
-        System.out.println(plusOperation.apply(5, 11));
-        System.out.println(divideOperation.apply(12, 3));
-        System.out.println(minOperation.apply(15, 7));
-        System.out.println(maxOperation.apply(15, 7));
-    }
-
-    private static BiFunction<Integer, Integer, Integer> getOperation(String sign) {
-        return switch (sign) {
-            case "+" -> Integer::sum;
-            case "-" -> (value1, value2) -> value1 - value2;
-            case "*" -> (value1, value2) -> value1 * value2;
-            case "/" -> (value1, value2) -> value1 / value2;
-            case "max" -> Math::max;
-            case "min" -> Math::min;
-            //вставьте код здесь
-            default -> throw new IllegalArgumentException("Неизвестная операция");
-        };
-
+        // сохраните лямбду в переменную
+        // лямбда должна складывать два целых числа
+        ... summator = (n1, n2) -> ...;
+        // допишите код, использующий лямбду
+        System.out.println("Сумма 23 и 65 равна " + ...);
     }
 }
