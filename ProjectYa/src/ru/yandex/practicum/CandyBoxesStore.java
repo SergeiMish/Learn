4ef63400817c3d77a65c1e996cd7afc80ae7ee34
SergeiMish <mishalevskii@yandex.ru>
2024-10-07 10:37:09 +0500

    Sprint 8

--- ProjectYa/src/ru/yandex/practicum/CandyBoxesStore.java
+++ ProjectYa/src/ru/yandex/practicum/CandyBoxesStore.java
@@ -1,31 +1,13 @@
 package ru.yandex.practicum;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
-public class OptionalNameDemo {
+public class OptionalInStreams {
 
-    static Optional<String> requestUserName() {
-        //Здесь должен быть запрос к пользователю, но пока его не реализовали
-        //Поместите в переменную username имя пользователя или оставьте ее пустой, если имя скрыто
-        String username = "";
-
-        if (username.isBlank()) {
-
-            return Optional.empty();
-            //вставьте код здесь
-        } else {
-            return Optional.of(username);
-            //вставьте код здесь
-        }
-    }
     public static void main(String[] args) {
-        Optional<String> maybeName = requestUserName();
-        if (maybeName.isPresent()) {
-            String name = maybeName.get();
-            //вставьте код здесь
-            System.out.println("Привет, " + name + "!");
-        } else {
-            System.out.println("Ваше право не называть имя!");
-        }
+        Stream.of(1, 2, 3, 4, 5)
+                .filter(number -> number % 2 == 0)
+                .//выбрать первый отфильтрованный элемент
+                .//вывести на экран найденное число либо строку "Чётное число не найдено"
     }
 }
