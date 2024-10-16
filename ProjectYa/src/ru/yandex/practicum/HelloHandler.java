20fa36b7dee98b7c2d17504a0c598fa39e51dcd4
SergeiMish <mishalevskii@yandex.ru>
2024-10-16 10:18:08 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/HelloHandler.java
+++ ProjectYa/src/ru/yandex/practicum/HelloHandler.java
@@ -4,10 +4,16 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 class HelloHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         exchange.sendResponseHeaders(200, 0);
+
+        String response = "Привет на сервере";
+        try (OutputStream os = exchange.getResponseBody()){
+            os.write(response.getBytes());
+        }
     }
 }
