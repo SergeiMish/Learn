305325b5aa18511fa9d56de43f231d01dd69f157
SergeiMish <mishalevskii@yandex.ru>
2024-10-16 17:19:07 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -5,23 +5,22 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
 
 class HelloHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        // считываем тело запроса и преобразуем в строку
-        InputStream inputStream = httpExchange.getRequestBody();
-        String name = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-        System.out.println("Тело запроса:\n" + name);
-
+        // получаем путь, на который пришел запрос
+        String path = httpExchange.getRequestURI().getPath();
+        // разбиваем путь на компоненты и берём последний
+        String name = path.split("/")[2];
+        // выводим полученное имя в консоль и в качестве ответа
+        System.out.println("Имя: " + name);
         httpExchange.sendResponseHeaders(200, 0);
 
         try (OutputStream os = httpExchange.getResponseBody()) {
-            String response = "Привет " + name + "! Рады видеть на нашем сервере.";
+            String response = "Привет, " + name + "!";
             os.write(response.getBytes());
         }
     }
