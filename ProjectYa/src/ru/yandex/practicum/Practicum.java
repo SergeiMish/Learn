bf75f4aa220ff932246b646e680b45bab278e1d7
SergeiMish <mishalevskii@yandex.ru>
2024-10-16 17:22:44 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,28 +1,65 @@
 package ru.yandex.practicum;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 class HelloHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        // получаем путь, на который пришел запрос
-        String path = httpExchange.getRequestURI().getPath();
-        // разбиваем путь на компоненты и берём последний
-        String name = path.split("/")[2];
-        // выводим полученное имя в консоль и в качестве ответа
-        System.out.println("Имя: " + name);
-        httpExchange.sendResponseHeaders(200, 0);
+        String response;
 
+        // извлеките метод из запроса
+        String method = ...
+
+        switch(...) {
+            case ...:
+                response = handlePostRequest(httpExchange);
+            case ...:
+                response = handleGetRequest(httpExchange);
+                // не забудьте про ответ для остальных методов
+            ...
+        }
+
+        httpExchange.sendResponseHeaders(200, 0);
         try (OutputStream os = httpExchange.getResponseBody()) {
-            String response = "Привет, " + name + "!";
             os.write(response.getBytes());
         }
+    }
+
+    private static String handleGetRequest(HttpExchange httpExchange) {
+        // обработайте GET-запрос в соответствии с условиями задания
+        ...
+    }
+
+    private static String handlePostRequest(HttpExchange httpExchange) throws IOException {
+        // обработайте POST-запрос в соответствии с условиями задания
+
+        // извлеките path из запроса
+        String path = ...
+        // а из path — профессию и имя
+        String profession = ...
+        String name = ...
+
+        // извлеките тело запроса
+        ...
+        String body = ...
+
+        // объедините полученные данные из тела и пути запроса
+        String response = ...
+
+        // извлеките заголовок и в зависимости от условий дополните ответ
+        List<String> wishGoodDay = ...
+        // верните полученную строку ответа
+        ...
     }
 }
 
@@ -34,5 +71,6 @@ public class Practicum {
         httpServer.createContext("/hello", new HelloHandler());
         httpServer.start();
         System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
+        httpServer.stop(2);
     }
 }
