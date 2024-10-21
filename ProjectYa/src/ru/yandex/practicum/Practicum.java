c4d7f98dfe2ed7828eb4e90707873866b6003389
SergeiMish <mishalevskii@yandex.ru>
2024-10-21 11:46:30 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -2,99 +2,81 @@ package ru.yandex.practicum;
 
 import com.google.gson.Gson;
 
-import java.io.IOException;
+class Practicum {
+    public static void main(String[] args) {
+        UserPost post = new UserPost();
+        post.setPhotoUrl("https://new-social-network.site/images/928476864.jpg");
+        post.setUserId(97_748);
+        post.setDescription("Классное фото!");
+        post.setLikesQuantity(753);
 
-class LastLikeInfo {
-    // ваш код
-}
+        // создайте экземпляр Gson
+        Gson gson = new Gson();
+        // сериализуйте объект
+        String postSerialized = gson.toJson(post);
+        System.out.println("Serialized post: " + postSerialized);
 
-public class Practicum {
-
-    public static void main(String[] args) throws IOException {
-        String lastLikeInfoStr = "{ \"user\": \"Алексей\", \"hours\": 12, \"minutes\": 30}";
-
-        // код для десериализации
-				...
-        LastLikeInfo lastLikeInfo = ...
-
-        LikesInfo likesInfo = new LikesInfo();
-        likesInfo.setRepostsCount(10);
-        likesInfo.setHasOwnerLiked(true);
-        likesInfo.setLikes(new Like[]{
-                new Like("Алексей", "http://example.com/avatars/aleksey.jpg"),
-                new Like("Елена", "http://example.com/avatars/elena.jpg"),
-                new Like("Света", "http://example.com/avatars/sveta.jpg"),
-        });
-
-        likesInfo.setLastLikeInfo(...);
-
-        // код сериализации и вывода на экран
+        // десериализуйте объект
+        UserPost postDeserialized = gson.fromJson(postSerialized, UserPost.class);
+        System.out.println("Deserialized post: " + postDeserialized);
     }
 }
 
-class LikesInfo {
-    private boolean hasOwnerLiked;
-    private Like[] likes;
-    private int repostsCount;
-    private LastLikeInfo lastLikeInfo;
 
-    public boolean isHasOwnerLiked() {
-        return hasOwnerLiked;
+class UserPost {
+    // URL-адрес, по которому можно скачать фото
+    private String photoUrl;
+
+    // уникальный идентификатор автора поста
+    private int userId;
+
+    // текстовой комментарий к фото
+    private String description;
+
+    // сколько пользователей поставило лайк этому посту
+    private int likesQuantity;
+
+
+    public String getPhotoUrl() {
+        return photoUrl;
     }
 
-    public void setHasOwnerLiked(boolean hasOwnerLiked) {
-        this.hasOwnerLiked = hasOwnerLiked;
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
     }
 
-    public Like[] getLikes() {
-        return likes;
+    public int getUserId() {
+        return userId;
     }
 
-    public void setLikes(Like[] likes) {
-        this.likes = likes;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    public int getRepostsCount() {
-        return repostsCount;
+    public String getDescription() {
+        return description;
     }
 
-    public void setRepostsCount(int repostsCount) {
-        this.repostsCount = repostsCount;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public LastLikeInfo getLastLikeInfo() {
-        return lastLikeInfo;
+    public int getLikesQuantity() {
+        return likesQuantity;
     }
 
-    public void setLastLikeInfo(LastLikeInfo info) {
-        this.lastLikeInfo = info;
-    }
-}
-
-class Like {
-    private String name;
-    private String avatarUrl;
-
-    public Like() {}
-
-    public Like(String name, String avatarUrl) {
-        this.name = name;
-        this.avatarUrl = avatarUrl;
+    public void setLikesQuantity(int likesQuantity) {
+        this.likesQuantity = likesQuantity;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
+    @Override
+    public String toString() {
+        return "UserPost{" +
+                "photoUrl='" + photoUrl + '\'' +
+                ", userId=" + userId +
+                ", description='" + description + '\'' +
+                ", likesQuantity=" + likesQuantity +
+                '}';
     }
 }
