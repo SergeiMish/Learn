3badcd74502fdffcdf8bd761c0df26bd010c7cd7
SergeiMish <mishalevskii@yandex.ru>
2024-10-21 09:25:44 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,246 +1,100 @@
 package ru.yandex.practicum;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import com.google.gson.Gson;
 
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.io.IOException;
 
-class PostsHandler implements HttpHandler {
-    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-
-    private final List<Post> posts;
-
-    public PostsHandler(List<Post> posts) {
-        this.posts = posts;
-    }
-
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        Endpoint endpoint = getEndpoint(exchange.getRequestURI().getPath(), exchange.getRequestMethod());
-
-        switch (endpoint) {
-            case GET_POSTS: {
-                handleGetPosts(exchange);
-                break;
-            }
-            case GET_COMMENTS: {
-                handleGetComments(exchange);
-                break;
-            }
-            case POST_COMMENT: {
-                handlePostComments(exchange);
-                break;
-            }
-            default:
-                writeResponse(exchange, "Такого эндпоинта не существует", 404);
-        }
-    }
-
-    private void handlePostComments(HttpExchange exchange) throws IOException {
-        // реализуйте обработку запроса на добавление комментария
-
-        // извлеките идентификатор поста и обработайте исключительные ситуации
-        Optional<Integer> postIdOpt = getPostId(exchange);
-        if (postIdOpt.isEmpty()) {
-            writeResponse(exchange, "Некорректный идентификатор поста", 400);
-            return;
-        }
-        int postId = postIdOpt.get();
-
-        // Получаем комментарий из тела запроса
-        Optional<Comment> commentOpt = parseComment(exchange.getRequestBody());
-        if (commentOpt.isEmpty()) {
-            writeResponse(exchange, "Поля комментария не могут быть пустыми", 400);
-            return;
-        }
-        Comment comment = commentOpt.get();
-
-        // Ищем пост с указанным идентификатором и добавляем комментарий
-        for (Post post : posts) {
-            if (post.getId() == postId) {
-                post.addComment(comment);
-                writeResponse(exchange, "Комментарий добавлен", 201);
-                return;
-            }
-        }
-
-        // Если пост не найден
-        writeResponse(exchange, "Пост с идентификатором " + postId + " не найден", 404);
-    }
-//        ...
-//        Comment comment = ...;
-//
-//        // добавьте комментарий к указанном посту
-//        // не забудьте обработать ситуацию, когда пост не найден
-//        ...
-
-    private Optional<Comment> parseComment(InputStream bodyInputStream) throws IOException {
-        // реализуйте код, разбирающий тело запроса и конструирующий объект комментария
-        BufferedReader reader = new BufferedReader(new InputStreamReader(bodyInputStream, DEFAULT_CHARSET));
-        String username = reader.readLine();
-        StringBuilder textBuilder = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            textBuilder.append(line).append("\n");
-        }
-        String text = textBuilder.toString().trim();
-
-        // Проверяем, что имя пользователя и текст не пустые
-        if (username == null || username.isEmpty() || text.isEmpty()) {
-            return Optional.empty();
-        }
-
-        return Optional.of(new Comment(username, text));
-    }
-
-    private void handleGetPosts(HttpExchange exchange) throws IOException {
-        String response = posts.stream()
-                .map(Post::toString)
-                .collect(Collectors.joining("\n"));
-        writeResponse(exchange, response, 200);
-    }
-
-    private void handleGetComments(HttpExchange exchange) throws IOException {
-        Optional<Integer> postIdOpt = getPostId(exchange);
-        if(postIdOpt.isEmpty()) {
-            writeResponse(exchange, "Некорректный идентификатор поста", 400);
-            return;
-        }
-        int postId = postIdOpt.get();
-
-        for (Post post : posts) {
-            if (post.getId() == postId) {
-                String response = post.getComments().stream()
-                        .map(Comment::toString)
-                        .collect(Collectors.joining("\n"));
-                writeResponse(exchange, response, 200);
-                return;
-            }
-        }
-
-        writeResponse(exchange, "Пост с идентификатором " + postId + " не найден", 404);
-    }
-
-    private Optional<Integer> getPostId(HttpExchange exchange) {
-        String[] pathParts = exchange.getRequestURI().getPath().split("/");
-        try {
-            return Optional.of(Integer.parseInt(pathParts[2]));
-        } catch (NumberFormatException exception) {
-            return Optional.empty();
-        }
-    }
-
-    private Endpoint getEndpoint(String requestPath, String requestMethod) {
-        String[] pathParts = requestPath.split("/");
-
-        if (pathParts.length == 2 && pathParts[1].equals("posts")) {
-            return Endpoint.GET_POSTS;
-        }
-        if (pathParts.length == 4 && pathParts[1].equals("posts") && pathParts[3].equals("comments")) {
-            if (requestMethod.equals("GET")) {
-                return Endpoint.GET_COMMENTS;
-            }
-            if (requestMethod.equals("POST")) {
-                return Endpoint.POST_COMMENT;
-            }
-        }
-        return Endpoint.UNKNOWN;
-    }
-
-    private void writeResponse(HttpExchange exchange,
-                               String responseString,
-                               int responseCode) throws IOException {
-        try (OutputStream os = exchange.getResponseBody()) {
-            exchange.sendResponseHeaders(responseCode, 0);
-            os.write(responseString.getBytes(DEFAULT_CHARSET));
-        }
-        exchange.close();
-    }
-
-    enum Endpoint {GET_POSTS, GET_COMMENTS, POST_COMMENT, UNKNOWN}
+class LastLikeInfo {
+    // ваш код
 }
 
 public class Practicum {
-    private static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
-        // инициализация начальных данных
-        List<Post> posts = new ArrayList<>();
-        Post post1 = new Post(1, "Это первый пост, который я здесь написал.");
-        post1.addComment(new Comment("Пётр Первый", "Я успел откомментировать первым!"));
-        posts.add(post1);
+        String lastLikeInfoStr = "{ \"user\": \"Алексей\", \"hours\": 12, \"minutes\": 30}";
 
-        Post post2 = new Post(22, "Это будет второй пост. Тоже короткий.");
-        posts.add(post2);
+        // код для десериализации
+				...
+        LastLikeInfo lastLikeInfo = ...
 
-        Post post3 = new Post(333, "Это пока последний пост.");
-        posts.add(post3);
+        LikesInfo likesInfo = new LikesInfo();
+        likesInfo.setRepostsCount(10);
+        likesInfo.setHasOwnerLiked(true);
+        likesInfo.setLikes(new Like[]{
+                new Like("Алексей", "http://example.com/avatars/aleksey.jpg"),
+                new Like("Елена", "http://example.com/avatars/elena.jpg"),
+                new Like("Света", "http://example.com/avatars/sveta.jpg"),
+        });
 
-        // настройка и запуск HTTP-сервера
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
-        httpServer.createContext("/posts", new PostsHandler(posts));
-        httpServer.start(); // запускаем сервер
+        likesInfo.setLastLikeInfo(...);
 
-        System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
-        // завершаем работу сервера для корректной работы тренажёра
-//        httpServer.stop(1);
+        // код сериализации и вывода на экран
     }
 }
 
-class Post {
-    private int id;
-    private String text;
-    private List<Comment> comments = new ArrayList<>();
+class LikesInfo {
+    private boolean hasOwnerLiked;
+    private Like[] likes;
+    private int repostsCount;
+    private LastLikeInfo lastLikeInfo;
 
-    public Post(int id, String text) {
-        this.id = id;
-        this.text = text;
+    public boolean isHasOwnerLiked() {
+        return hasOwnerLiked;
     }
 
-    public int getId() {
-        return id;
+    public void setHasOwnerLiked(boolean hasOwnerLiked) {
+        this.hasOwnerLiked = hasOwnerLiked;
     }
 
-    public void addComment(Comment comment) {
-        comments.add(comment);
+    public Like[] getLikes() {
+        return likes;
     }
 
-    public List<Comment> getComments() {
-        return comments;
+    public void setLikes(Like[] likes) {
+        this.likes = likes;
     }
 
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", text='" + text + '\'' +
-                ", comments=" + comments +
-                '}';
+    public int getRepostsCount() {
+        return repostsCount;
+    }
+
+    public void setRepostsCount(int repostsCount) {
+        this.repostsCount = repostsCount;
+    }
+
+    public LastLikeInfo getLastLikeInfo() {
+        return lastLikeInfo;
+    }
+
+    public void setLastLikeInfo(LastLikeInfo info) {
+        this.lastLikeInfo = info;
     }
 }
 
-class Comment {
-    private String user;
-    private String text;
+class Like {
+    private String name;
+    private String avatarUrl;
 
-    public Comment(String user, String text) {
-        this.user = user;
-        this.text = text;
+    public Like() {}
+
+    public Like(String name, String avatarUrl) {
+        this.name = name;
+        this.avatarUrl = avatarUrl;
     }
 
-    @Override
-    public String toString() {
-        return "Comment{" +
-                "user='" + user + '\'' +
-                ", text='" + text + '\'' +
-                '}';
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
     }
 }
