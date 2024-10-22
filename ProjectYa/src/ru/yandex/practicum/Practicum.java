92fbb0f329d074961f6f289ba2d7bcdc3b7f5cd0
SergeiMish <mishalevskii@yandex.ru>
2024-10-22 20:23:38 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,48 +1,23 @@
 package ru.yandex.practicum;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Practicum {
     public static void main(String[] args) {
-        HttpClient client = HttpClient.newHttpClient();
+        Scanner scanner = new Scanner(System.in);
+        WeatherClient weatherClient = new WeatherClient();
+        WeatherApplication weatherApp = new WeatherApplication(weatherClient);
+        System.out.println("Приложение для получения информации о погоде.");
+        System.out.println("Доступные города: Москва (MOW), Санкт-Петербург (LED), Казань (KZN)");
+        while (true) {
+            System.out.println("Введите код города (или 'exit' для выхода).");
+            String command = scanner.nextLine();
 
-        URI url = URI.create("https://functions.yandexcloud.net/d4ed1i6t3f80hf0p7mer?base=RUB&symbols=USD,EUR");
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(url)
-                .GET()
-                .build();
-
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            // проверяем, успешно ли обработан запрос
-            if (response.statusCode() == 200) {
-                JsonElement jsonElement = JsonParser.parseString(response.body());
-                if(!jsonElement.isJsonObject()) { // проверяем, точно ли мы получили JSON-объект
-                    System.out.println("Ответ от сервера не соответствует ожидаемому.");
-                    return;
-                }
-                // получите курс доллара и евро и запишите в переменные rateUSD и rateEUR
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                JsonObject rates = jsonObject.getAsJsonObject("rates");
-                double rateUSD = rates.get("USD").getAsDouble();
-                double rateEUR = rates.get("EUR").getAsDouble();
-
-                System.out.println("Стоимость рубля в долларах: " + rateUSD + " USD");
-                System.out.println("Стоимость рубля в евро: " + rateEUR + " EUR");
-            } else {
-                System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
+            if (command.equalsIgnoreCase("exit")) {
+                break;
             }
-        } catch (IOException | InterruptedException e) { // обрабатываем ошибки отправки запроса
-            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
-                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+
+            weatherApp.displayWeather(command);
         }
     }
 }
---
+++ ProjectYa/src/ru/yandex/practicum/WeatherClient.java
@@ -0,0 +1,53 @@
+package ru.yandex.practicum;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+class WeatherClient {
+    private final HttpClient client;
+
+    // Конструктор по умолчанию
+    WeatherClient() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    // Метод для получения данных о погоде
+    String getWeatherData(String cityCode) {
+        String url = "https://functions.yandexcloud.net/d4eo3a1nvqedpic89160?scale=C&city=" + cityCode;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                JsonElement jsonElement = JsonParser.parseString(response.body());
+                if (!jsonElement.isJsonObject()) {
+                    return "Ответ от сервера не соответствует ожидаемому.";
+                }
+
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+                JsonObject cityData = jsonObject.getAsJsonObject("cities").getAsJsonObject(cityCode);
+
+                String city = cityData.get("city").getAsString();
+                String conditions = cityData.get("conditions").getAsString();
+                String temperature = cityData.get("temperature").getAsString();
+
+                return "Город: " + city + ". " + conditions + ", " + temperature;
+            } else {
+                return "Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode();
+            }
+        } catch (IOException | InterruptedException e) {
+            return "Во время выполнения запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, параметры запроса и повторите попытку.";
+        }
+    }
+}
