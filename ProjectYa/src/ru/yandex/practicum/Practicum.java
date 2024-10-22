e2cd141cbcfec96bd5e72890f87fbd3f6ad38e6a
SergeiMish <mishalevskii@yandex.ru>
2024-10-22 17:06:10 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -7,26 +11,37 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Practicum {
-
     public static void main(String[] args) {
         HttpClient client = HttpClient.newHttpClient();
 
-        // укажите URL запроса, включая его параметры
         URI url = URI.create("https://functions.yandexcloud.net/d4ed1i6t3f80hf0p7mer?base=RUB&symbols=USD,EUR");
-
-        // создайте объект, описывающий запрос с необходимой информацией
-        HttpRequest request =  HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(url)
-                .header("Accept", "application/json")
                 .GET()
                 .build();
 
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Код статуса: " + response.statusCode());
-            System.out.println("Ответ: " + response.body());
-        } catch (IOException | InterruptedException e) { // обработка ошибки отправки запроса
-            System.out.println("Во время выполнения запроса ресурса по URL-адресу: '" + url + "' возникла ошибка.\n" +
+            // проверяем, успешно ли обработан запрос
+            if (response.statusCode() == 200) {
+                JsonElement jsonElement = JsonParser.parseString(response.body());
+                if(!jsonElement.isJsonObject()) { // проверяем, точно ли мы получили JSON-объект
+                    System.out.println("Ответ от сервера не соответствует ожидаемому.");
+                    return;
+                }
+                // получите курс доллара и евро и запишите в переменные rateUSD и rateEUR
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+                JsonObject rates = jsonObject.getAsJsonObject("rates");
+                double rateUSD = rates.get("USD").getAsDouble();
+                double rateEUR = rates.get("EUR").getAsDouble();
+
+                System.out.println("Стоимость рубля в долларах: " + rateUSD + " USD");
+                System.out.println("Стоимость рубля в евро: " + rateEUR + " EUR");
+            } else {
+                System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) { // обрабатываем ошибки отправки запроса
+            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
                     "Проверьте, пожалуйста, адрес и повторите попытку.");
         }
     }
