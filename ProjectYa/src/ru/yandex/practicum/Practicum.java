864ab37443a1a50db9a9b9961caefdf01f089bcc
SergeiMish <mishalevskii@yandex.ru>
2024-10-22 08:29:24 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,139 +1,30 @@
 package ru.yandex.practicum;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-
 import java.io.IOException;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+class Practicum {
 
-enum SubtitleLanguage {
-    ru, en, cn
-        }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // укажите URL-адрес ресурса
+        URI uri = URI.create("https://ya.ru/white");
 
-class SubtitleItem {
-    HashMap<SubtitleLanguage, String> values = new HashMap<>();
-    LocalTime begin;
-    LocalTime end;
+        // создайте объект, описывающий HTTP-запрос
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
 
-    public SubtitleItem(Map<SubtitleLanguage, String> values, LocalTime begin, LocalTime end) {
-        this.values = new HashMap<>(values);
-        this.begin = begin;
-        this.end = end;
-    }
+        // создайте HTTP-клиент с настройками по умолчанию
+        HttpClient client = HttpClient.newHttpClient();
 
-    public HashMap<SubtitleLanguage, String> getValues() {
-        return values;
-    }
+        // получите стандартный обработчик тела запроса
+        // с конвертацией содержимого в строку
+        HttpResponse.BodyHandler<String> handler = ... ;
 
-    public void setValues(HashMap<SubtitleLanguage, String> values) {
-        this.values = values;
-    }
+        // отправьте запрос
+        HttpResponse<String> response = ... ;
 
-    public LocalTime getBegin() {
-        return begin;
-    }
-
-    public void setBegin(LocalTime begin) {
-        this.begin = begin;
-    }
-
-    public LocalTime getEnd() {
-        return end;
-    }
-
-    public void setEnd(LocalTime end) {
-        this.end = end;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SubtitleItem that = (SubtitleItem) o;
-        return Objects.equals(values, that.values) && Objects.equals(begin, that.begin) && Objects.equals(end, that.end);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(values, begin, end);
-    }
-}
-
-
-class SubtitleListTypeToken extends TypeToken<List<SubtitleItem>> {
-
-        }
-
-class LocalTimeTypeAdapter extends TypeAdapter<LocalTime> {
-    private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-
-    @Override
-    public void write(JsonWriter jsonWriter, LocalTime time) throws IOException {
-        jsonWriter.value(time.format(timeFormatter));
-    }
-
-    @Override
-    public LocalTime read(JsonReader jsonReader) throws IOException {
-        return LocalTime.parse(jsonReader.nextString(), timeFormatter);
-    }
-}
-
-
-public class Practicum {
-
-    public static void main(String[] args) {
-        List<SubtitleItem> subtitles = Arrays.asList(
-                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Здравствуйте!",
-                        SubtitleLanguage.en, "Hello!",
-                        SubtitleLanguage.cn, "Ni hao"),
-                        LocalTime.of(0, 0, 15),
-                        LocalTime.of(0, 0, 17)
-                ),
-                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Привет!",
-                        SubtitleLanguage.en, "Hi!",
-                        SubtitleLanguage.cn, "Ni hao"),
-                        LocalTime.of(0, 0, 21),
-                        LocalTime.of(0, 0, 24)
-                ),
-                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Как дела?",
-                        SubtitleLanguage.en, "How are you?",
-                        SubtitleLanguage.cn, "Ni hao ma"),
-                        LocalTime.of(0, 0, 28),
-                        LocalTime.of(0, 0, 31)
-                ),
-                new SubtitleItem(Map.of(SubtitleLanguage.ru, "Всё хорошо, спасибо!",
-                        SubtitleLanguage.en, "I'm fine, thank you!",
-                        SubtitleLanguage.cn, "Wo hen hao, xie xie"),
-                        LocalTime.of(0, 0, 34),
-                        LocalTime.of(0, 0, 37)
-                )
-        );
-
-        // Адаптер для преобразования типа LocalTime в String в формате субтитров
-        LocalTimeTypeAdapter localTimeTypeAdapter = new LocalTimeTypeAdapter();
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(LocalTime.class, localTimeTypeAdapter);
-        gsonBuilder.setPrettyPrinting();
-        Gson gson = gsonBuilder.create();
-
-        // Сериализация
-        String subtitlesJson = gson.toJson(subtitles);
-        System.out.println(subtitlesJson);
-
-        // Десериализация
-        List<SubtitleItem> parsed = gson.fromJson(subtitlesJson,  new SubtitleListTypeToken().getType());
-        if (parsed.equals(subtitles)) {
-            System.out.println("Субтитры десериализованы корректно.");
-        } else {
-            System.out.println("Произошла ошибка при десериализации.");
-        }
+        // выведите код состояния и тело ответа
     }
 }
