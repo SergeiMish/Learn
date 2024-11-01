94a4618d8fb54df3e3707a384f20c3aab7591853
SergeiMish <mishalevskii@yandex.ru>
2024-11-01 20:44:15 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,23 +1,38 @@
 package ru.yandex.practicum;
 
-import java.util.Scanner;
+import java.time.LocalTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Practicum {
+
+    private static final Logger log = LoggerFactory.getLogger(Practicum.class);
+
+    public static final LocalTime CHECK_IN_TIME = LocalTime.of(14, 0);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        WeatherClient weatherClient = new WeatherClient();
-        WeatherApplication weatherApp = new WeatherApplication(weatherClient);
-        System.out.println("Приложение для получения информации о погоде.");
-        System.out.println("Доступные города: Москва (MOW), Санкт-Петербург (LED), Казань (KZN)");
-        while (true) {
-            System.out.println("Введите код города (или 'exit' для выхода).");
-            String command = scanner.nextLine();
+        LocalTime start = LocalTime.of(14, 0);
+        LocalTime finish = LocalTime.of(12, 0);
 
-            if (command.equalsIgnoreCase("exit")) {
-                break;
-            }
+        log.info("Пользователь выбрал интервал заселения {}-{}.", start, finish);
 
-            weatherApp.displayWeather(command);
+        try {
+            confirmCheckInTime(start, finish);
+        } catch (IllegalArgumentException e) {
+            log.error("Выбран неправильный интервал.", e);
+        }
+    }
+
+    public static void confirmCheckInTime(LocalTime start, LocalTime finish) {
+        if (start.isAfter(finish)) {
+            throw new IllegalArgumentException("Начало интервала должно быть раньше завершения интервала.");
+        }
+
+        if (CHECK_IN_TIME.isBefore(start) || CHECK_IN_TIME.equals(start)) {
+            System.out.println("Время заселения подтверждено.");
+        } else {
+            System.out.println("Заселение возможно после " + CHECK_IN_TIME + ".");
         }
     }
 }
