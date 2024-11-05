265d639d0415a1cfc210fc65092a8748f6802458
SergeiMish <mishalevskii@yandex.ru>
2024-11-05 09:21:19 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/WeatherApplication.java
+++ ProjectYa/src/ru/yandex/practicum/WeatherApplication.java
@@ -1,16 +1,16 @@
 package ru.yandex.practicum;
 
-public class WeatherApplication {
-    private final WeatherClient weatherClient;
-
-    public WeatherApplication(WeatherClient weatherClient) {
-        this.weatherClient = weatherClient;
-    }
-
-    // метод для отображения погоды
-    public void displayWeather(String city) {
-        // замените данные на динамические, полученные через weatherClient
-        String weatherData = weatherClient.getWeatherData(city);
-        System.out.println(weatherData);
-    }
-}
+//public class WeatherApplication {
+//    private final WeatherClient weatherClient;
+//
+//    public WeatherApplication(WeatherClient weatherClient) {
+//        this.weatherClient = weatherClient;
+//    }
+//
+//    // метод для отображения погоды
+//    public void displayWeather(String city) {
+//        // замените данные на динамические, полученные через weatherClient
+//        String weatherData = weatherClient.getWeatherData(city);
+//        System.out.println(weatherData);
+//    }
+//}
