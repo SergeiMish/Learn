dbc5552a9da26f6ac5f9bc93e967310f1ab13dad
SergeiMish <mishalevskii@yandex.ru>
2024-11-05 09:29:37 +0500

    Sprint 9

--- ProjectYa/src/ru/yandex/practicum/Practicum.java
+++ ProjectYa/src/ru/yandex/practicum/Practicum.java
@@ -1,38 +1,28 @@
 package ru.yandex.practicum;
 
-import java.time.LocalTime;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class Practicum {
-
-    private static final Logger log = LoggerFactory.getLogger(Practicum.class);
-
-    public static final LocalTime CHECK_IN_TIME = LocalTime.of(14, 0);
-
-    public static void main(String[] args) {
-        LocalTime start = LocalTime.of(14, 0);
-        LocalTime finish = LocalTime.of(12, 0);
-
-        log.info("Пользователь выбрал интервал заселения {}-{}.", start, finish);
-
-        try {
-            confirmCheckInTime(start, finish);
-        } catch (IllegalArgumentException e) {
-            log.error("Выбран неправильный интервал.", e);
+@interface ObjectInfo {
+        Class<?> type() ... Object....;
+        Class<?>[] args() default  {};
+        ... ... ... ...;
         }
+
+// Класс для аннотирования
+        ......(... = ....class)
+public class MyClass {
+
+    // Элементы класса
+    ....(type = String. ...)
+    private String myField;
+
+    // Конструктор
+    @....(type = ..., args = {...}, ... = true)
+    public MyClass(int initialValue) {
+        // Код конструктора
     }
 
-    public static void confirmCheckInTime(LocalTime start, LocalTime finish) {
-        if (start.isAfter(finish)) {
-            throw new IllegalArgumentException("Начало интервала должно быть раньше завершения интервала.");
-        }
-
-        if (CHECK_IN_TIME.isBefore(start) || CHECK_IN_TIME.equals(start)) {
-            System.out.println("Время заселения подтверждено.");
-        } else {
-            System.out.println("Заселение возможно после " + CHECK_IN_TIME + ".");
-        }
+    // Метод
+    ...(type = ...сlass, args = {int.class, ....}, ... = true)
+    public void myMethod(int number, String text) {
+        // Код метода
     }
 }
