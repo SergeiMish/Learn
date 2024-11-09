d306e4fe776a2ac9c873fbf4b4e86baa966ea13b
SergeiMish <mishalevskii@yandex.ru>
2024-11-09 11:59:21 +0500

    Catsgramm

---
+++ Catsgram/src/main/java/Practicum.java
@@ -0,0 +1,24 @@
+
+import lombok.*;
+
+
+@Value
+@Builder(toBuilder = true)
+class Point {
+    int x;
+    int y;
+}
+
+public class Practicum {
+    public static void main(String[] args) {
+
+        final Point point1 = Point.builder().x(0).y(0).build();
+        System.out.println(point1);
+
+        // используем метод toBuilder на уже созданном объекте
+        // point1, чтобы на его основе создать новый объект
+        // с другим значением в поле y
+        final Point point2 = point1.toBuilder().y(3).build();
+        System.out.println(point2);
+    }
+}
